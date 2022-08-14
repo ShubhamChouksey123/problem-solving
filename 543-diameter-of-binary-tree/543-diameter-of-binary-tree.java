852a1e805af24c294556207bde73464705bca88c
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2022-08-14 23:49:13 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (86.76%) - LeetHub

---
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    private Integer maxVal = 0;
+    
+    public Integer diameterOfBinaryTreeUtil(TreeNode root) {
+        
+        if(root == null)
+            return 0;
+        
+        
+        if(root.left == null && root.right == null)
+            return 0;
+        
+        Integer leftDia = 0;
+        Integer rightDia = 0;
+        
+        if(root.left != null){
+             leftDia = diameterOfBinaryTreeUtil(root.left) + 1;
+        }
+        if(root.right != null){
+             rightDia = diameterOfBinaryTreeUtil(root.right) + 1;
+        }
+        
+        maxVal = Math.max(maxVal, leftDia + rightDia);
+        
+        Integer dia = Math.max(leftDia , rightDia);
+        return Math.max(leftDia , rightDia);
+        
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        Integer dia = diameterOfBinaryTreeUtil(root);
+        return maxVal.intValue();
+     }
+}
+
+/**
+
+[1]
+[1, 2]
+[1, 2, 3]
+
+*/
