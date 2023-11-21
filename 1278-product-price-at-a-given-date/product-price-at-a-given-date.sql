59fac3954315c9a2bde169886361e7ea7bef1fd9
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2023-11-21 19:43:06 +0530

    Time: 961 ms (36.88%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,14 +1,3 @@
-# Write your MySQL query statement below
-# SELECT product_id, new_price, change_date, DATEDIFF('2019-08-16', change_date)
-# FROM Products 
-# WHERE change_date <= '2019-08-16'
-# GROUP BY product_id
-# HAVING MIN(DATEDIFF('2019-08-16', change_date));
-
-# SELECT MIN(DATEDIFF('2019-08-16', change_date))
-# FROM Products
-# WHERE product_id = 2 AND change_date <= '2019-08-16';
-
 SELECT product_id, new_price AS price
 FROM Products 
 WHERE(product_id, change_date) IN (
