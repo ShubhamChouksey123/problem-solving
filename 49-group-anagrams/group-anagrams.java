9b4798701a1e6c802beceec21146c7afafde6c6d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-04-20 11:20:07 +0530

    Time: 8 ms (38.51%) | Memory: 48.4 MB (14.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,5 +1,12 @@
 class Solution {
-    
+
+    /**
+     * String "ate" will return a1e1t1
+     * String "tan" will return a1n1t1
+     *
+     * @param word
+     * @return the string consisting of count of each character
+     */
     public String generateKeysCountString(String word) {
         StringBuilder keysCountString = new StringBuilder();
         int[] count = new int[26];
@@ -21,6 +28,16 @@ class Solution {
     }
 
 
+        /**
+     * <ul>
+     *     <li>will get the keyCountString of each word.</li>
+     *     <li>map a map of key as the keyCountString with the mapped value as the word</li>
+     * </ul>
+     *
+     *
+     * @param
+     * @return the string consisting of count of each character
+     */
     public List<List<String>> groupAnagrams(String[] strs) {
 
         int n = strs.length;
