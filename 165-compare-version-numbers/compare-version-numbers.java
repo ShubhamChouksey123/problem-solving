388d1da4a284788fcfb998324488958cdf830236
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-05-04 16:55:03 +0530

    Time: 1 ms (78.70%) | Memory: 41.3 MB (70.31%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -2,11 +2,11 @@ class Solution {
     public int compareVersion(String version1, String version2) {
 
         String[] str1 = version1.split("\\.");
-        System.out.println("str1 : " + Arrays.toString(str1));
+        // System.out.println("str1 : " + Arrays.toString(str1));
 
 
         String[] str2 = version2.split("\\.");
-        System.out.println("str2 : " + Arrays.toString(str2));
+        // System.out.println("str2 : " + Arrays.toString(str2));
 
         for (int i = 0; i < Math.max(str1.length, str2.length); i++) {
 
@@ -19,7 +19,7 @@ class Solution {
                 num2 = Integer.valueOf(str2[i]);
             }
 
-            System.out.println("num1 : " + num1 + " and num2 : " + num2);
+            // System.out.println("num1 : " + num1 + " and num2 : " + num2);
 
             if (num1.compareTo(num2) < 0) {
                 return -1;
