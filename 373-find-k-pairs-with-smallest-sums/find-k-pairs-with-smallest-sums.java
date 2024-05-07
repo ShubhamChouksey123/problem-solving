577be33f8734583439fe75db9134face761543a7
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-05-07 10:41:35 +0530

    Time: 1945 ms (5.07%) | Memory: 59.2 MB (59.42%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -6,24 +6,25 @@ class Solution {
         int m = nums1.length;
         int n = nums2.length;
 
-        PriorityQueue<SmallPair> pq = new PriorityQueue<>(new SmallPairMaxComparator());
+        PriorityQueue<int[]> pq = new PriorityQueue<>(((int[] a, int[] b) -> ((b[0] + b[1]) - (a[0] + a[1]))));
 
         for (int i = 0; i < Math.min(m, k); i++) {
             for (int j = 0; j < Math.min(n, k); j++) {
 
                 if (pq.size() < k) {
-                    pq.add(new SmallPair(nums1[i], nums2[j]));
+                    int[] pair = new int[]{nums1[i], nums2[j]};
+                    pq.add(pair);
                     continue;
                 }
 
-                SmallPair topSmallPair = pq.peek();
+                int[] topSmallPair = pq.peek();
 
-                if (topSmallPair.getSum() > nums1[i] + nums2[j]) {
+                if (topSmallPair[0] + topSmallPair[1] > nums1[i] + nums2[j]) {
                     pq.poll();
-                    pq.add(new SmallPair(nums1[i], nums2[j]));
+                    int[] pair = new int[]{nums1[i], nums2[j]};
+                    pq.add(pair);
                 }
 
-                // System.out.println("after adding " + nums1[i] + " and " + nums2[j] + " pq.peek : " + pq.peek());
             }
         }
 
@@ -31,66 +32,12 @@ class Solution {
 
         while (!pq.isEmpty()) {
             List<Integer> pair = new ArrayList<>();
-            SmallPair topSmallPair = pq.poll();
-            pair.add(topSmallPair.getA());
-            pair.add(topSmallPair.getB());
+            int[] topSmallPair = pq.poll();
+            pair.add(topSmallPair[0]);
+            pair.add(topSmallPair[1]);
             ans.add(pair);
         }
 
         return ans;
-    }
-
-    public class SmallPairMinComparator implements Comparator<SmallPair> {
-
-        @Override
-        public int compare(SmallPair o1, SmallPair o2) {
-            return (o1.getA() + o1.getB()) - (o2.getA() + o2.getB());
-        }
-    }
-
-    public class SmallPairMaxComparator implements Comparator<SmallPair> {
-
-        @Override
-        public int compare(SmallPair o1, SmallPair o2) {
-            return (o2.getA() + o2.getB()) - (o1.getA() + o1.getB());
-        }
-    }
-
-    public class SmallPair {
-        public int a;
-        public int b;
-
-        public SmallPair(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
-
-        public int getA() {
-            return a;
-        }
-
-        public void setA(int a) {
-            this.a = a;
-        }
-
-        public int getB() {
-            return b;
-        }
-
-        public void setB(int b) {
-            this.b = b;
-        }
-
-        public int getSum() {
-            return this.getA() + this.getB();
-        }
-
-        @Override
-        public String toString() {
-            return "SmallPair{" +
-                    "a=" + a +
-                    ", b=" + b +
-                    '}';
-        }
     }
 }
