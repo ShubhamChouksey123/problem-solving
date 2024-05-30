74d18117dfcc7e5d97cb700f39553fd3652dd00a
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-05-30 16:38:08 +0530

    Time: 6 ms (64.96%) | Memory: 49.2 MB (30.40%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,37 +1,27 @@
 class Solution {
-    
     public int maxSubarraySumCircular(int[] nums) {
         int n = nums.length;
-        int curMax = nums[0];
-        int curMin = nums[0];
-        int maxSubArraySum = nums[0];
-        int minSubArraySum = nums[0];
-        int totalSum = nums[0];
-        for (int end = 1; end < n; end++) {
-            if (curMax > 0) {
-                curMax = curMax + nums[end];
-            } else {
-                curMax = nums[end];
-            }
+        if(n <= 1){
+            return nums[0];
+        }
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        int currentMaxSum = nums[0];
+        int currentMinSum = nums[0];
+        int total = nums[0];
+        for(int i = 1 ; i < n ; i++){
+            int number = nums[i];
+            currentMaxSum = Math.max(currentMaxSum + number, number);
+            currentMinSum = Math.min(currentMinSum + number , number);
 
-            if (curMin < 0) {
-                curMin = curMin + nums[end];
-            } else {
-                curMin = nums[end];
-            }
-
-            totalSum += nums[end];
-            maxSubArraySum = Math.max(maxSubArraySum, curMax);
-            minSubArraySum = Math.min(minSubArraySum, curMin);
+            maxSum = Math.max(currentMaxSum, maxSum);
+            minSum = Math.min(currentMinSum, minSum);
+            total += number;
         }
 
-        if (maxSubArraySum < 0) {
-            return maxSubArraySum;
+        if(total - minSum> 0){
+            return Math.max(maxSum, total - minSum);   
         }
-
-
-        return Math.max(maxSubArraySum, totalSum - minSubArraySum);
-
+        return maxSum;
     }
-
 }
