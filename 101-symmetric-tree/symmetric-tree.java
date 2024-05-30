0b6e75e7f334020cba41ccd25b12086fe4f3315a
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-05-30 10:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (73.96%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -14,26 +14,26 @@
  * }
  */
 class Solution {
-     public boolean isMirrorImage(TreeNode root1, TreeNode root2) {
-
-        if (root1 == null && root2 == null)
+    
+    private boolean isSymmetricUtil(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null){
             return true;
-
-        if (root1 == null || root2 == null)
+        }
+        if(root1 == null && root2 != null || root1 != null && root2 == null){
             return false;
-
-        if (root1.val != root2.val)
+        }
+        if(root1.val != root2.val){
             return false;
+        }
 
-        return isMirrorImage(root1.left, root2.right) && isMirrorImage(root1.right, root2.left);
-
+        return isSymmetricUtil(root1.left, root2.right) && isSymmetricUtil(root1.right, root2.left);
     }
-
+    
     public boolean isSymmetric(TreeNode root) {
-
-        if (root == null)
+        if(root == null){
             return true;
-
-        return isMirrorImage(root.left, root.right);
+        }
+        return isSymmetricUtil(root.left, root.right);
+        
     }
 }
