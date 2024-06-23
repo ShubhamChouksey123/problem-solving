8e8f7b3bb6547623ebbd6001d7198172b492d27b
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-06-23 11:12:48 +0530

    Time: 10 ms (24.88%) | Memory: 47.1 MB (68.68%) - LeetSync

---
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    private int maxSumValueLevel = Integer.MIN_VALUE;
+    private int maxSumNumberLevel = 0;
+
+    public void maxLevelSumUtil(TreeNode root) {
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        int level = 1, currentLevelSum = 0;
+
+        queue.add(root);
+        queue.add(null);
+    
+        while(!queue.isEmpty()){
+
+            TreeNode top = queue.poll();
+           
+            if(top == null){
+            // System.out.println("top is null, level : " + level + " and currentLevelSum : "+ currentLevelSum +" and queue : " + queue);
+
+                if(currentLevelSum > maxSumValueLevel){
+                    maxSumValueLevel = currentLevelSum;
+                    maxSumNumberLevel = level;
+                }
+                currentLevelSum = 0;
+                level++;
+                if(!queue.isEmpty()){
+                    queue.add(null);
+                }
+                continue;
+            }
+
+            currentLevelSum += top.val;
+            // System.out.println("top : " + top.val + ", level : " + level + " and currentLevelSum : "+ currentLevelSum +" and queue : " + queue);
+
+            
+            if(top.left != null){
+                queue.add(top.left);
+            }
+            if(top.right != null){
+                queue.add(top.right);
+            }
+        }
+    }
+
+    public int maxLevelSum(TreeNode root) {
+        this.maxSumValueLevel = 1;
+        this.maxSumValueLevel = Integer.MIN_VALUE;
+        maxLevelSumUtil(root);
+        return maxSumNumberLevel;
+
+    }
+}
