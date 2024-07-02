8a94826233e1c3dc67f873349ffce680a4f3408d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-02 16:07:06 +0530

    Time: 1 ms (70.08%) | Memory: 45.8 MB (74.84%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,43 +1,56 @@
 class Solution {
     public int trap(int[] height) {
+
         int n = height.length;
-
-        if (n == 1 || n == 2)
+        if(n < 2){
             return 0;
+        }
+        
+        int[] maxElementRight = new int[n];
+        int[] maxElementLeft = new int[n];
 
-
-        int totalWaterStored = 0;
-        int currentWaterStored = 0;
-
-        int maxElementLeftValue = height[0];
-        int maxElementRightValue = height[n - 1];
-
-        int leftPointer = 0;
-        int rightPointer = n - 1;
-
-        while (leftPointer < rightPointer) {
-
-            if (maxElementLeftValue <= maxElementRightValue) {
-                leftPointer++;
-                currentWaterStored = Math.max(maxElementLeftValue - height[leftPointer], 0);
-                maxElementLeftValue = Math.max(maxElementLeftValue, height[leftPointer]);
-                
-
-            } else {
-                rightPointer--;
-                currentWaterStored = Math.max(maxElementRightValue - height[rightPointer], 0);
-                maxElementRightValue = Math.max(maxElementRightValue, height[rightPointer]);
-               
-            }
-
-            totalWaterStored += currentWaterStored;
+        int maxValue = height[0];
+        for(int i = 1; i < n ; i++){
+            maxElementLeft[i] = maxValue;
+            maxValue = Math.max(maxValue, height[i]);
         }
 
+        maxValue = height[n-1];
+        for(int i = n-2 ; i >= 0 ; i--){
+            maxElementRight[i] = maxValue;
+            maxValue = Math.max(maxValue, height[i]);
+        }
 
-        // System.out.println("totalWaterStored : " + totalWaterStored);
+        int totalWater = 0;
+        for(int i = 1 ; i < n-1 ; i++){
+            int water = Math.min(maxElementLeft[i], maxElementRight[i]) - height[i];
+            if(water > 0){
+                totalWater += water;
+            }
+        }
 
-        return totalWaterStored;
-
-        
+        return totalWater;
     }
 }
+
+
+/**
+
+maximum height on its left
+0, 1,0,2,1,0,1,3,2,1,2,1
+-1 0,1,1,2,2,2,2,3,3,3,3
+
+
+maximum height on its right
+0, 1,0,2,1,0,1,3,2,1,2,1
+..            ..  ,2,1,-1
+
+
+O(n)
+maxHeight Right = 2
+maxHeight left = 3
+min(2, 3) => 2
+water 2 -1 = 1
+
+
+ */
