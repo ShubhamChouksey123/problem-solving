bfd87ec6424ab451f4bf41a367a8917779bae766
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-04 11:47:24 +0530

    Time: 3 ms (80.87%) | Memory: 44.4 MB (84.46%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,33 +1,23 @@
 class Solution {
-
-    /**
-     * 'I', 'V', 'X', 'L', 'C', 'D', 'M'
-     */
-    static Map<Character, Integer> mp = Map.ofEntries(
-            Map.entry('I', 1),
-            Map.entry('V', 5),
-            Map.entry('X', 10),
-            Map.entry('L', 50),
-            Map.entry('C', 100),
-            Map.entry('D', 500),
-            Map.entry('M', 1000)
+    private static final Map<Character, Integer> values = new HashMap<>(
+        Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000)
     );
+    
     public int romanToInt(String s) {
-        int n = s.length();
-        int sum = 0;
-        int previousValue = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            char c = s.charAt(i);
-            int currentValue = mp.get(c);
-
-            if (currentValue < previousValue) {
-                currentValue = -currentValue;
-            }
-            sum += currentValue;
-            previousValue = currentValue;
-        }
-        System.out.println("sum : " + sum);
-        return sum;
         
+        int n = s.length();
+        int sum = values.get(s.charAt(0));
+        int previousValue = sum;
+
+        for(int i = 1; i < n ; i++){
+            char c = s.charAt(i);
+            int value = values.get(c);
+            sum += value;
+            if(previousValue < value){
+                sum -= (previousValue * 2);
+            }
+            previousValue = value;
+        }
+        return sum;
     }
 }
