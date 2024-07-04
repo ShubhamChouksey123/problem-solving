927e0f1a8e6d0de2539d6a1963ab302d0d6e5193
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-04 21:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (97.11%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,37 +1,21 @@
 class Solution {
 
-    private int robUtil(int[] nums, int n, int index, int[] dp){
-
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        
-        if(index == 0){
-            dp[index] = nums[0];
-            return dp[index];
-        }
-        if(index == 1){
-            dp[index] = Math.max(nums[0], nums[1]);;
-            return dp[index];
-        }
     
-        int a = 0;
-        if(index >= 2){
-            a = nums[index] + robUtil(nums, n, index -2, dp);
-        }
-        int b = robUtil(nums, n, index -1, dp);
-        
-        dp[index] = Math.max(a, b);
-        return dp[index];
-    }
-
     public int rob(int[] nums) {
         
         int n = nums.length; 
-        int[] dp = new int[n];
 
+        int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        robUtil(nums, n, n-1, dp);
+
+        dp[0] = nums[0];
+        if(n >= 2){
+            dp[1] = Math.max(nums[0], nums[1]);
+        }
+    
+        for(int i = 2 ; i < n ; i++){
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i -1]);
+        }
         return dp[n-1];
     }
 }
