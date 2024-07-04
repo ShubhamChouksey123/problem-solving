106493e507c5433d4e773bc9d85ef36f9b5ef04e
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-04 22:48:27 +0530

    Time: 40 ms (67.32%) | Memory: 44.2 MB (46.94%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,24 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-
         int n = nums.length;
-        /**
-         * where longestSubSequence[i] represent longest subsequence start from index i
-         */
-        int[] longestSubSequence = new int[n];
-        longestSubSequence[n - 1] = 1;
-        int ans = 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
 
-        for (int i = n - 2; i >= 0; i--) {
-            longestSubSequence[i] = 1;
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] < nums[j]) {
-                    longestSubSequence[i] = Math.max(longestSubSequence[j] + 1, longestSubSequence[i]);
+        int max = 1;
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int j = i+1 ; j < n ; j++){
+                if(nums[j] > nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            ans = Math.max(ans, longestSubSequence[i]);
+            max = Math.max(max, dp[i]);
         }
-        // System.out.println("longestSubSequence : " + Arrays.toString(longestSubSequence));
-        return ans;
+        return max;
     }
 }
