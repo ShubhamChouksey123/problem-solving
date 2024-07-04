8f4e3b6577657e0b1eb5f8823cea5be262cf95ad
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-04 10:14:35 +0530

    Time: 3 ms (80.18%) | Memory: 45.1 MB (90.11%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,29 +1,26 @@
 class Solution {
     public int candy(int[] ratings) {
-
-
         int n = ratings.length;
-        int[] candy = new int[n];
-        Arrays.fill(candy, 1);
+        int[] candys = new int[n];
+        Arrays.fill(candys, 1);
 
-        for (int i = 1; i < n; i++) {
-            if (ratings[i - 1] < ratings[i] && candy[i - 1] >= candy[i]) {
-                candy[i] = candy[i - 1] + 1;
+        for(int i = 1 ; i < n ; i++){
+            if(ratings[i-1] < ratings[i] && candys[i-1] >= candys[i]){
+                candys[i] = candys[i-1] + 1;
             }
         }
 
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
-                candy[i] = candy[i + 1] + 1;
+        for(int i = n-2 ; i >= 0 ; i--){
+            if(ratings[i] > ratings[i+1] && candys[i] <= candys[i+1]){
+                candys[i] = candys[i+1] + 1;
             }
         }
-        
-        int totalCandies =  0;
-        for(int num:  candy){
-            totalCandies += num;
-        }
-        return totalCandies;
 
+        int total =0; 
+        for(int candy : candys){
+            total += candy; 
+        }
+
+        return total;
     }
 }
