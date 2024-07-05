874920662cf3ec9196c739c274844d9a551aecb7
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-05 22:57:57 +0530

    Time: 4 ms (31.12%) | Memory: 45.2 MB (66.41%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,38 +1,28 @@
 class Solution {
-
-    public boolean liesInRange(int m, int n, int x, int y) {
-        if (x >= 0 && x < m && y >= 0 && y < n)
-            return true;
-        return false;
-    }
-
+    
+    
     public int minPathSum(int[][] grid) {
 
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
 
-        int[][] pathSum = new int[m][n];
-        pathSum[0][0] = grid[0][0];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0)
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 && j == 0){
                     continue;
-                int val = grid[i][j];
-                int a = Integer.MAX_VALUE;
-                int b = Integer.MAX_VALUE;
-                if (liesInRange(m, n, i - 1, j)) {
-                    a = pathSum[i - 1][j] + val;
                 }
-                if (liesInRange(m, n, i, j - 1)) {
-                    b = pathSum[i][j - 1] + +val;
+                int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE; 
+                if(i >= 1){
+                    a = dp[i-1][j] + grid[i][j];
                 }
-                pathSum[i][j] = Math.min(a, b);
+                if(j >= 1){
+                    b = dp[i][j-1] + grid[i][j];  
+                }
+                dp[i][j] = Math.min(a, b);
             }
         }
-
-        System.out.println("pathSum  : " + Arrays.deepToString(pathSum));
-        return pathSum[m - 1][n - 1];
-
+        return dp[m-1][n-1];
     }
 }
