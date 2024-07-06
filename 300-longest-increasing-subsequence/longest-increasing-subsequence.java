c6dfb00397864911172ed688f59a355d20349060
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-06 09:56:02 +0530

    Time: 83 ms (22.58%) | Memory: 43.5 MB (98.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,70 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
+    
+    private int maxValue = 1;
 
-        int max = 1;
-        for(int i = n-1 ; i >= 0 ; i--){
-            for(int j = i+1 ; j < n ; j++){
-                if(nums[j] > nums[i]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            max = Math.max(max, dp[i]);
+    public int lengthOfLIS1(int[] nums, int index) {
+        
+        System.out.println("LIS1 for index : " + index );
+        if(index == 0){
+            return 1;
         }
-        return max;
+
+        int maxLength = 1;
+        for(int i = index - 1 ; i >= 0 ; i--){
+            if(nums[i] < nums[index]){
+                int length = lengthOfLIS1(nums, i) + 1; 
+                maxLength = Math.max(length, maxLength);
+                maxValue = Math.max(maxValue, maxLength);
+            }
+        }
+        System.out.println("LIS1 for index : " + index + " maxLength : " + maxLength);
+        return maxLength;
+    }
+
+    public int lengthOfLIS(int[] nums, int index, int[] longestSeqLength) {
+        
+        // System.out.println("LIS for index : " + index );
+        if(index == 0){
+            return 1;
+        }
+
+        if(longestSeqLength[index] != -1){
+            return longestSeqLength[index];
+        }
+
+        int maxLength = 1;
+        for(int i = index - 1 ; i >= 0 ; i--){
+            if(nums[i] < nums[index]){
+                int length = lengthOfLIS(nums, i, longestSeqLength) + 1; 
+                maxLength = Math.max(length, maxLength);
+                maxValue = Math.max(maxValue, maxLength);
+            }
+        }
+
+        longestSeqLength[index] = maxLength;
+        // System.out.println("longestSeqLength[" + index + "] : " + longestSeqLength[index]);
+        return maxLength;
+    }
+    
+    public int lengthOfLIS(int[] nums) {
+
+        int n = nums.length;
+        // recursive
+        // for(int i = 1 ; i < n ; i++){
+        //     lengthOfLIS1(nums, i);
+        // }
+        // return maxValue; 
+
+
+
+        int[] longestSeqLength = new int[n];
+        Arrays.fill(longestSeqLength, -1);
+
+
+        for(int i = 1; i < n ; i++){
+            lengthOfLIS(nums, i, longestSeqLength);
+        }
+        
+        return maxValue;
     }
 }
