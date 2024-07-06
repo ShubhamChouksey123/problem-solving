24ccd54b1731be85b0c54bb1f0b38b865b14428d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-06 11:53:48 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (32.79%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,21 +1,50 @@
 class Solution {
-
     
-    public int rob(int[] nums) {
+    private int robUtil1(int[] nums, int index) {
+
+        if(index < 0){
+            return 0;
+        }
+
+        int a = nums[index] + robUtil1(nums, index-2);
+        int b = robUtil1(nums, index-1);
+        return Math.max(a,b);
         
-        int n = nums.length; 
+    }
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
 
-        dp[0] = nums[0];
-        if(n >= 2){
-            dp[1] = Math.max(nums[0], nums[1]);
+    private int robUtil(int[] nums, int index, int[] maxAmount){
+        // System.out.println("RU index : " + index);
+        if(index < 0){
+            return 0;
         }
-    
-        for(int i = 2 ; i < n ; i++){
-            dp[i] = Math.max(nums[i] + dp[i-2], dp[i -1]);
+        if(maxAmount[index] != -1){
+            return maxAmount[index];
         }
-        return dp[n-1];
+
+        int a = nums[index] + robUtil(nums, index-2, maxAmount);
+        int b = robUtil(nums, index-1, maxAmount);
+        maxAmount[index] = Math.max(a, b);
+
+        // System.out.println("maxAmount [" + index + "] : " + maxAmount[index] );
+        return maxAmount[index];
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        // recursive
+        // return robUtil1(nums, n-1);
+
+        if(nums.length == 1){
+            return nums[0];
+        }
+
+        int[] maxAmount = new int[n];
+        Arrays.fill(maxAmount, -1);
+        maxAmount[0] = nums[0];
+
+        robUtil(nums, n-1, maxAmount);
+        return maxAmount[n-1];
+
     }
 }
