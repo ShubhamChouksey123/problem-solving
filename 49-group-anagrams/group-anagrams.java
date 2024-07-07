89dc5a544316d094367841cf69d978767b9e3a09
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-07 23:31:19 +0530

    Time: 15 ms (24.42%) | Memory: 48.2 MB (16.72%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,64 +1,51 @@
 class Solution {
 
-    /**
-     * String "ate" will return a1e1t1
-     * String "tan" will return a1n1t1
-     *
-     * @param word
-     * @return the string consisting of count of each character
-     */
-    public String generateKeysCountString(String word) {
-        StringBuilder keysCountString = new StringBuilder();
-        int[] count = new int[26];
 
-        for (int i = 0; i < word.length(); i++) {
-            count[word.charAt(i) - 'a']++;
+    private String getKey(String s){
+        int[] counts = new int[26];
+        int n = s.length();
+        for(int i = 0; i < n ; i++){
+            counts[s.charAt(i) - 'a']++;
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (count[i] > 0) {
-
-                char c = (char) ('a' + i);
-
-                keysCountString.append(c);
-                keysCountString.append(count[i]);
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < 26 ; i++){
+            if(counts[i] > 0){
+                char c = (char)('a' + i);
+                stringBuilder.append(c);
+                stringBuilder.append(String.valueOf(counts[i]));
             }
         }
-        return keysCountString.toString();
+
+        return stringBuilder.toString();
     }
-
-
-        /**
-     * <ul>
-     *     <li>will get the keyCountString of each word.</li>
-     *     <li>map a map of key as the keyCountString with the mapped value as the word</li>
-     * </ul>
-     *
-     *
-     * @param
-     * @return the string consisting of count of each character
-     */
+    
     public List<List<String>> groupAnagrams(String[] strs) {
 
         int n = strs.length;
+
         Map<String, List<String>> mp = new HashMap<>();
 
-
-        for (int i = 0; i < n; i++) {
-            String keysCountString = generateKeysCountString(strs[i]);
-
-            if (mp.get(keysCountString) == null) {
-                List<String> group = new ArrayList<>();
-                group.add(strs[i]);
-                mp.put(keysCountString, group);
-            } else {
-                List<String> group = mp.get(keysCountString);
-                group.add(strs[i]);
-                mp.replace(keysCountString, group);
+        for(String str : strs){
+            String key = getKey(str);
+            List<String> currentList = mp.get(key);
+            if(currentList == null){
+                List<String> newList = new ArrayList<>();
+                newList.add(str);
+                mp.put(key, newList);
+            }else{
+                currentList.add(str);
             }
         }
 
-        return new ArrayList<>(mp.values());
+
+        List<List<String>> ans = new ArrayList<>();
+        mp.forEach(
+            (k, v) -> {
+                ans.add(v);
+            }
+        );
         
+        return ans;
     }
 }
