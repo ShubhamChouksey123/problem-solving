946a0071b9782d8fb1c6ad340b1c8b0973fc7d12
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-09 17:43:42 +0530

    Time: 5 ms (6.09%) | Memory: 41.4 MB (53.27%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,35 +1,31 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
 
-        String[] str1 = version1.split("\\.");
-        // System.out.println("str1 : " + Arrays.toString(str1));
+        String[] ver1 = version1.split("\\.");
+        String[] ver2 = version2.split("\\.");
 
+        System.out.println("ver1 : " + Arrays.toString(ver1));
+        System.out.println("ver2 : " + Arrays.toString(ver2));
 
-        String[] str2 = version2.split("\\.");
-        // System.out.println("str2 : " + Arrays.toString(str2));
-
-        for (int i = 0; i < Math.max(str1.length, str2.length); i++) {
-
-            Integer num1 = 0;
-            Integer num2 = 0;
-            if (i < str1.length) {
-                num1 = Integer.valueOf(str1[i]);
+        Integer first, second;
+        for(int i = 0; i < Math.max(ver1.length, ver2.length) ; i++){
+            first = 0; second = 0;
+            if(i < ver1.length){
+                first = Integer.valueOf(ver1[i]);
             }
-            if (i < str2.length) {
-                num2 = Integer.valueOf(str2[i]);
+            if(i < ver2.length){
+                second = Integer.valueOf(ver2[i]);
             }
 
-            // System.out.println("num1 : " + num1 + " and num2 : " + num2);
-
-            if (num1.compareTo(num2) < 0) {
+            if(first < second){
                 return -1;
-            } else if (num1.compareTo(num2) > 0) {
+            }
+            else if(first > second){
                 return 1;
             }
         }
 
-
         return 0;
-    }
 
+    }
 }
