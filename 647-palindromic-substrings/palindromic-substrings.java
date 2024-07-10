d15a8ad05f5333989ddb408427fee75e2c7f2eca
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-10 13:18:48 +0530

    Time: 5 ms (96.04%) | Memory: 41.5 MB (56.87%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,72 +1,52 @@
 class Solution {
-    
-    private int countPal = 0;
 
-    public boolean isPalindrome(String s, int start, int end) {
-        
-        for(int i = start ; i < end/2 ; i++){
-            if(s.charAt(i) != s.charAt(end-i-1)){
-                return false;
+    private int count = 0;
+
+    private void countOddLengthAtIndex(String s, int n, int index) {
+
+        int length = 0;
+        while(index + length < n && index - length >= 0){
+            if(s.charAt(index + length) != s.charAt(index - length)){
+                break;
             }
+            count++;
+            length++;
         }
-        return true;
 
     }
 
-    public boolean isPalindrome(String s, int start, int end, int[][] isPal) {
-        
-        if(isPal[start][end] != -1){
-            if(isPal[start][end] == 1){return true;}else{return false;} 
+    private void countEvenLengthAtIndex(String s, int n, int index) {
+
+        int start = index, end = index +1;
+        while(end < n && start >= 0){
+            if(s.charAt(end) != s.charAt(start)){
+                break;
+            }
+            count ++;
+            start--;
+            end++;
         }
 
-        if(start == end){
-            isPal[start][end] = 1;
-            return true;
-        }
-
-        if(s.charAt(start) != s.charAt(end)){
-            isPal[start][end] = 0;
-            return false;
-        }
-
-        if(start + 1 == end){
-            isPal[start][end] = 1;
-            return true;
-        }
-
-        boolean isInner = isPalindrome(s, start+1, end-1, isPal);
-        isPal[start][end] = (isInner == true) ? 1 : 0;
-        
-        if(isPal[start][end] == 1){return true;}else{return false;} 
     }
 
-
-    public void countSubstringsUtil(String s, int start, int end, int[][] isPal) {
-
-        // System.out.println("start : " + start + " and end : " + end);
-
-        if(isPalindrome(s, start, end, isPal)){
-            // System.out.println("pal for start : " + start + " and end : " + end);
-            countPal++;
+    private void countOddLength(String s, int n) {
+        for(int i = 0 ; i < n ; i++){
+            countOddLengthAtIndex(s, n, i);
         }
     }
-    
+
+    private void countEvenLength(String s, int n) {
+        for(int i = 0 ; i < n - 1 ; i++){
+            countEvenLengthAtIndex(s, n, i);
+        }
+    }
+
     public int countSubstrings(String s) {
-
+        this.count = 0;
         int n = s.length();
-        countPal = 0;
+        countEvenLength(s, n);
+        countOddLength(s, n);
 
-        int[][] isPal = new int[n][n];
-        for(int[] inner : isPal){
-            Arrays.fill(inner, -1);
-        }
-
-        for(int i = 0; i < n ; i++){
-            for(int j = i; j < n ; j++){
-                countSubstringsUtil(s, i, j, isPal);
-            }
-        }
-
-        return countPal;
+        return count ;
     }
 }
