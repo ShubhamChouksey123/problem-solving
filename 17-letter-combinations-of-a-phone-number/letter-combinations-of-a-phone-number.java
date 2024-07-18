29e8e0b3e8eaeb3ef61422cb92aacddba3508be7
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2024-07-18 10:55:17 +0530

    Time: 1 ms (66.39%) | Memory: 42.1 MB (48.31%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,39 +1,48 @@
-class Solution {
-    public void letterCombinationsUtil(String digits, int n, int index, String s, char[][] mappedChar, List<String> ans) {
 
-        if (index == n) {
-            ans.add(s);
+import static java.util.Map.entry;
+
+class Solution {
+    
+    Map<Integer, List<Character>> mp =  Map.ofEntries(
+        entry(2, Arrays.asList('a', 'b', 'c')), 
+        entry(3, Arrays.asList('d', 'e', 'f')), 
+        entry(4, Arrays.asList('g', 'h', 'i')),
+        entry(5, Arrays.asList('j', 'k', 'l')),
+        entry(6, Arrays.asList('m', 'n', 'o')),
+        entry(7, Arrays.asList('p', 'q', 'r', 's')),
+        entry(8, Arrays.asList('t', 'u', 'v')),
+        entry(9, Arrays.asList('w', 'x', 'y', 'z'))
+    );
+
+    
+
+    public void letterCombinations(String digits, List<Character> word, List<String> ans) {
+
+        if(word.size() == digits.length()){
+            if(word.size() == 0)
+                return ;
+            StringBuilder stringBuilder = new StringBuilder();
+            for(Character c : word){
+                stringBuilder.append(c);
+            } 
+            ans.add(stringBuilder.toString());
             return;
         }
-
-        int digit = digits.charAt(index) - '2';
-
-        for (int i = 0; i < mappedChar[digit].length; i++) {
-            letterCombinationsUtil(digits, n, index + 1, s + mappedChar[digit][i], mappedChar, ans);
+        
+        int digit = digits.charAt(word.size()) - '0';
+        for(Character c : mp.get(digit)){
+            word.add(c);
+            letterCombinations(digits, word, ans);
+            word.remove(word.size() - 1);
         }
     }
 
+    
     public List<String> letterCombinations(String digits) {
 
+        // System.out.println("mp : " + mp);
         List<String> ans = new ArrayList<>();
-        int n = digits.length();
-        if (n == 0) {
-            return ans;
-        }
-
-
-        char[][] mappedChar = new char[][]{
-                {'a', 'b', 'c'},
-                {'d', 'e', 'f'},
-                {'g', 'h', 'i'},
-                {'j', 'k', 'l'},
-                {'m', 'n', 'o'},
-                {'p', 'q', 'r', 's'},
-                {'t', 'u', 'v'},
-                {'w', 'x', 'y', 'z'},
-        };
-
-        letterCombinationsUtil(digits, n, 0, "", mappedChar, ans);
+        letterCombinations(digits, new ArrayList<>(), ans);
 
         return ans;
     }
