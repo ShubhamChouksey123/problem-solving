5616408236132580838d9239f18fc5db9cf7c399
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-01-27 22:43:49 +0530

    [152] [Time Beats: 66.74%] [Memory Beats: 43.72%] - LeetPush

--- Maximum-Product-Subarray.java
+++ Maximum-Product-Subarray.java
@@ -2,34 +2,19 @@
 2
 3    public int maxProduct(int[] nums) {
 4        int n = nums.length, index = 0;
-5        long[] minProd = new long[n];
-6        long[] maxProd = new long[n];
-7        minProd[0] = nums[0];
-8        maxProd[0] = nums[0];
-9        if(nums[0] == 0){
-10            minProd[0] = 1; maxProd[0] = 1; 
-11        }
-12
-13        for(int i = 1 ; i < n ; i++){
-14            if(nums[i] == 0){
-15                minProd[i] = 1; maxProd[i] = 1; 
-16                continue;
-17            }
-18            long a = nums[i]*minProd[i-1];
-19            long b = nums[i]*maxProd[i-1];
-20            minProd[i] = Math.min(Math.min(a, b), (long)nums[i]);
-21            maxProd[i] = Math.max(Math.max(a, b), (long)nums[i]);
-22        }
-23
-24        long max = Long.MIN_VALUE;
-25        for(int i = 0 ; i < n ; i++){
-26            if(nums[i] != 0){
-27                max = Math.max(max, maxProd[i]);
-28            }else{
-29                max = Math.max(max, 0);
-30            } 
-31        }
-32
-33        return (int)max;
-34    }
-35}
+5        int minSoFar = nums[0] , maxSoFar = nums[0], result = nums[0];
+6        
+7
+8        for(int i = 1 ; i < n ; i++){
+9            int cur = nums[i]; 
+10            int tmp = minSoFar;
+11
+12            minSoFar = Math.min(cur, Math.min(minSoFar * cur , maxSoFar * cur) );
+13            maxSoFar = Math.max(cur, Math.max(tmp * cur , maxSoFar * cur) );
+14            result = Math.max(result, maxSoFar);
+15        
+16        } 
+17
+18        return result;
+19    }
+20}
