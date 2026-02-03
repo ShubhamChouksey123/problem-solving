21a9b663b0796cee68c9dfe9669c9a9fa7358eaf
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-02-03 09:51:55 +0530

    [42] [Time Beats: 100.00%] [Memory Beats: 89.04%] - LeetPush

--- Trapping-Rain-Water.java
+++ Trapping-Rain-Water.java
@@ -1,37 +1,31 @@
 1class Solution {
-2
-3    private void populate(int[] height, int[] maxHeightLeft, int[] maxHeightRight) {
-4
-5        int n = height.length ;
-6        int maxLeft = height[0];
-7        for(int i = 1 ; i <= n - 1 ; i++){
-8            maxHeightLeft[i] = maxLeft;
-9            maxLeft = Math.max(maxLeft, height[i]);
-10        }
-11
-12        int maxRight = height[n-1];
-13        for(int i = n-2 ; i >= 0 ; i--){
-14            maxHeightRight[i] = maxRight;
-15            maxRight = Math.max(maxRight, height[i]);
-16        }
-17    
-18    }
-19
-20    public int trap(int[] height) {
-21
-22        int n = height.length ;
-23        int[] maxHeightLeft = new int[n];
-24        int[] maxHeightRight = new int[n];
-25
-26        populate(height, maxHeightLeft, maxHeightRight) ;
-27        int water = 0;
-28
-29        for(int i = 1 ; i <= n-2 ; i++){
-30            int curWater = Math.min(maxHeightLeft[i], maxHeightRight[i]) - height[i];
-31            water += Math.max(0,  curWater);
-32        }
-33
-34        return water;
-35        
-36    }
-37}
+2    public int trap(int[] height) {
+3
+4        int n = height.length, start = 0;
+5        int end = n-1;
+6
+7        int maxLeft = 0, maxRight = 0, totalWater = 0;
+8
+9        while(start <= end){
+10            if(height[start] < height[end]){
+11                if(height[start] >= maxLeft){
+12                    maxLeft = height[start];
+13                }else{
+14                    int cur = maxLeft - height[start];
+15                    totalWater += cur;
+16                }
+17                start++;
+18            }else{
+19                if(height[end] >= maxRight){
+20                    maxRight = height[end];
+21                }else{
+22                    int cur = maxRight - height[end];
+23                    totalWater += cur;
+24                }
+25                end--;
+26            }
+27        } 
+28        return totalWater;
+29        
+30    }
+31}
