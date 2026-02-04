2d2abeaa996251a6657b7d52e0e63596115fe53e
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-02-04 08:47:42 +0530

    [448] [Time Beats: 78.77%] [Memory Beats: 82.21%] - LeetPush

--- Find-All-Numbers-Disappeared-in-an-Array.java
+++ Find-All-Numbers-Disappeared-in-an-Array.java
@@ -1,20 +1,44 @@
 1class Solution {
-2    public List<Integer> findDisappearedNumbers(int[] nums) {
-3        int n = nums.length;
-4        List<Integer> ans = new ArrayList<>();
-5        for(int i = 0 ; i < n ; i++){
-6            int index = Math.abs(nums[i]) - 1;
-7            if(nums[index] > 0){
-8                nums[index] = -nums[index];
-9            }
-10            
-11        }
-12
-13        for(int i = 0 ; i < n ; i++){
-14            if(nums[i] > 0){
-15                ans.add(i+1);
-16            }
-17        }
-18        return ans;
-19    }
-20}
+2
+3    private void swap(int[] nums, int indexA, int indexB){
+4        int tmp = nums[indexA];
+5        nums[indexA] = nums[indexB];
+6        nums[indexB] = tmp;
+7    }
+8
+9    private void print(int[] nums){
+10        for(int i : nums){
+11            System.out.print(i + ",");
+12        }
+13        System.out.println("");
+14    }
+15
+16
+17    public List<Integer> findDisappearedNumbers(int[] nums) {
+18
+19        int n = nums.length;
+20        for(int i = 0 ; i < n ; i++){
+21            nums[i]--;
+22        }
+23
+24        int i = 0;
+25        while(i < n){
+26            int correctIndex = nums[i];
+27            if(nums[i] == nums[correctIndex]){
+28                i++;
+29            }else if(nums[i] != i){
+30                swap(nums, i, correctIndex);
+31            }else{
+32                i++;
+33            }
+34        }
+35        
+36        List<Integer> result = new ArrayList<>();
+37        for(i = 0 ; i < n ; i++){
+38            if(nums[i] != i){
+39                result.add(i+1);
+40            }
+41        }
+42        return result;
+43    }
+44}
