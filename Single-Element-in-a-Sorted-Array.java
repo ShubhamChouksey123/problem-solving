ead87a0b647d97cadd86abe25d001d54de015b19
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-01 19:57:49 +0530

    [540] [Time Beats: 4.51%] [Memory Beats: 19.15%] - LeetPush

--- Single-Element-in-a-Sorted-Array.java
+++ Single-Element-in-a-Sorted-Array.java
@@ -1,9 +1,24 @@
 1class Solution {
-2    public int singleNonDuplicate(int[] nums) {
-3        int result = 0;
-4        for(int num : nums){
-5            result = result ^ num;
-6        }    
-7        return result;
-8    }
-9}
+2    public int singleNonDuplicateUtil(int[] nums, int start, int end) {
+3        
+4        System.out.println("start : " + start + ", and end : " + end);
+5        System.out.println("start value : " + nums[start] + ", and end value : " + nums[end]);
+6        if(start == end){
+7            return nums[start];
+8        }
+9
+10        int mid = start + (end - start) / 2;
+11        if(mid %2 == 1) mid--;
+12
+13        if(nums[mid + 1] == nums[mid + 2]){
+14            return singleNonDuplicateUtil(nums, start, mid);
+15        }
+16        
+17        return singleNonDuplicateUtil(nums, mid + 2, end);
+18
+19    }
+20
+21    public int singleNonDuplicate(int[] nums) {
+22        return singleNonDuplicateUtil(nums, 0, nums.length - 1);
+23    }
+24}
