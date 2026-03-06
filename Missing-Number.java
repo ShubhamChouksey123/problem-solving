e90b8ecd4aca1ce45c5a3b4fafbbab20b1fd0c95
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-06 19:21:19 +0530

    [268] [Time Beats: 28.88%] [Memory Beats: 65.57%] - LeetPush

--- Missing-Number.java
+++ Missing-Number.java
@@ -1,17 +1,29 @@
 1class Solution {
-2    public int missingNumber(int[] nums) {
-3
-4        int n = nums.length;
-5        long totalSum = 0, curSum = 0;
-6
-7        for(int i = 0 ; i <= n ; i++){
-8            totalSum += i; 
-9        }
+2
+3    private void swap(int[] nums, int index1, int index2){
+4        int tmp = nums[index1];
+5        nums[index1] = nums[index2];
+6        nums[index2] = tmp;
+7    }
+8    
+9    private void swapStartingFromIndex(int[] nums, int n, int index) {
 10
-11        for(int num : nums){
-12            curSum += num;
+11        while(nums[index] < n && nums[index] != index){
+12            swap(nums, nums[index], index);
 13        }
-14        return (int)(totalSum- curSum);
-15        
-16    }
-17}
+14    }
+15
+16    public int missingNumber(int[] nums) {
+17        int n = nums.length;
+18        for(int i = 0 ; i < n ; i++){
+19            swapStartingFromIndex(nums, n, i);
+20        } 
+21
+22        for(int i = 0 ; i < n ; i++){
+23            if(nums[i] != i){
+24                return i;
+25            }
+26        } 
+27        return n;
+28    }
+29}
