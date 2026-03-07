9e725370be73a19e9acd841e8cd05a75c86d6570
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-07 12:20:55 +0530

    [39] [Time Beats: 9.56%] [Memory Beats: 18.61%] - LeetPush

--- Combination-Sum.java
+++ Combination-Sum.java
@@ -1,32 +1,32 @@
 1class Solution {
-2
-3    private void combinationSumUtil(int[] candidates, List<List<Integer>> result, int target, int sum, int start, List<Integer> com){
-4    
-5        if(sum > target){
-6            return;
-7        }
-8
-9        if(sum == target){
-10            result.add(new ArrayList<>(com));
-11            return;
-12        }
+2    public void combinationSum(int[] candidates, int target, int sum, int start, List<Integer> cur, List<List<Integer>> result) {
+3        if(sum > target){
+4            return;
+5        }
+6
+7        if(sum == target){
+8            result.add(new ArrayList<>(cur)); return;
+9        }
+10
+11        for(int i = start ; i < candidates.length ; i++){
+12            int candidate = candidates[i];
 13
-14        for(int i = start ; i < candidates.length ; i++){
-15
-16            if(sum + candidates[i] <= target){
-17                com.add(candidates[i]);
-18                combinationSumUtil(candidates, result, target, sum + candidates[i], i, com);
-19
-20                com.remove(com.size() - 1);
-21            }
-22        }        
-23    }
+14            cur.add(candidate);
+15            combinationSum(candidates, target, sum + candidate, i, cur, result);
+16
+17            cur.remove(cur.size() - 1);
+18        }
+19        
+20
+21    }
+22    
+23    public List<List<Integer>> combinationSum(int[] candidates, int target) {
 24
-25    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+25        Arrays.sort(candidates);
 26
 27        List<List<Integer>> result = new ArrayList<>();
-28
-29        combinationSumUtil(candidates, result, target, 0, 0, new ArrayList<>());
+28        combinationSum(candidates, target, 0, 0, new ArrayList<>(), result);
+29        
 30        return result;
 31    }
 32}
