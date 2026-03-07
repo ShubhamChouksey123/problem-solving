df6e2e103a3c6fe19f3d9d7c429203fd15f29613
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-07 23:31:02 +0530

    [2433] [Time Beats: 88.47%] [Memory Beats: 88.93%] - LeetPush

---
+++ Find-The-Original-Array-of-Prefix-Xor.java
@@ -0,0 +1,14 @@
+1class Solution {
+2    
+3    public int[] findArray(int[] pref) {
+4
+5        int n = pref.length;
+6        int[] ans = new int[n];
+7        ans[0] = pref[0];
+8
+9        for(int i = 1 ; i < n ; i++){
+10            ans[i] = pref[i] ^ pref[i-1];
+11        }
+12        return ans;
+13    }
+14}
