ea9725aeedf30ed8e4c3acd12de35d7773f19b1a
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-07 15:35:35 +0530

    [131] [Time Beats: 6.15%] [Memory Beats: 5.11%] - LeetPush

--- Palindrome-Partitioning.java
+++ Palindrome-Partitioning.java
@@ -1,35 +1,37 @@
 1class Solution {
-2    
-3    private boolean isPalindrome(String s, int startIndex, int endIndex){
-4        for(int i = 0 ; i <= (endIndex - startIndex)/ 2 ; i++){
-5            if(s.charAt(startIndex + i) != s.charAt(endIndex-i)){
-6                return false;
-7            }
-8        }
-9        return true;
-10    }
-11
-12    private void partitionUtil(List<List<String>> result, String s, int n, int startIndex, List<String> cur) {
-13
-14        if(startIndex == n){
-15            result.add(new ArrayList<>(cur));
-16            return;
-17        }
-18        
-19        for(int endIndex = startIndex ; endIndex < n ; endIndex++){
-20            if(isPalindrome(s, startIndex, endIndex)){
-21                cur.add(s.substring(startIndex, endIndex + 1));
-22                partitionUtil(result, s, n, endIndex + 1, cur);
-23                cur.remove(cur.size() - 1);
-24            }
-25        }
-26
-27    }
-28
-29    public List<List<String>> partition(String s) {
-30        List<List<String>> result = new ArrayList<>();
-31        partitionUtil(result, s, s.length(), 0, new ArrayList<>());
-32
-33        return result; 
-34    }
-35}
+2
+3    private boolean isPalindrom(String s, int start, int end){
+4        System.out.println("start : " + start + ", end : " + end);
+5        while(start < end){
+6            if(s.charAt(start) != s.charAt(end)){
+7                return false;
+8            }
+9            start++; end--;
+10        }
+11        return true;
+12    }
+13    private void partitionUtils(String s, int start, List<String> cur, List<List<String>> result) {
+14
+15        if(start == s.length()){
+16            result.add(new ArrayList<>(cur));
+17            return;
+18        }
+19
+20        for(int end = start ; end < s.length() ; end++){
+21            if(isPalindrom(s, start, end)){
+22                String substring = s.substring(start, end + 1);
+23                cur.add(substring);
+24                partitionUtils(s, end + 1, cur, result);
+25                cur.remove(cur.size() - 1);    
+26            }   
+27        }
+28    }
+29    
+30    public List<List<String>> partition(String s) {
+31        int n = s.length();
+32        List<List<String>> result = new ArrayList<>();
+33        partitionUtils(s, 0, new ArrayList<>(), result);
+34
+35        return result;     
+36    }
+37}
