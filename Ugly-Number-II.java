b744fc7587f3fbac6570df446f5d024a46bb0ccc
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-09 23:56:37 +0530

    [264] [Time Beats: 99.48%] [Memory Beats: 37.52%] - LeetPush

--- Ugly-Number-II.java
+++ Ugly-Number-II.java
@@ -1,21 +1,24 @@
 1class Solution {
 2    public int nthUglyNumber(int n) {
-3
-4        Queue<Long> minHeap = new PriorityQueue<>();
-5        minHeap.add(1L);
-6        Set<Long> visited = new HashSet<>();
+3        
+4        int p2 = 0, p3 = 0, p5 = 0;
+5        long[] ugly = new long[n];
+6        ugly[0] = 1L;
 7
-8        Long num = 1L;
-9        while(n > 0){
-10            num = minHeap.poll();
-11            if(visited.contains(num)){
-12                continue;
-13            }
-14            visited.add(num);
-15            minHeap.add(num*2);
-16            minHeap.add(num*3);
-17            minHeap.add(num*5); n--;
-18        }
-19        return num.intValue();
-20    }
-21}
+8        for(int i = 1 ; i < n ; i++){
+9            long p2Value = ugly[p2] * 2;
+10            long p3Value = ugly[p3] * 3;
+11            long p5Value = ugly[p5] * 5;
+12
+13            long nextUglyNumber = Math.min(p2Value, Math.min(p3Value, p5Value));
+14            ugly[i] = nextUglyNumber;
+15
+16            if(nextUglyNumber == p2Value) p2++;
+17            if(nextUglyNumber == p3Value) p3++;
+18            if(nextUglyNumber == p5Value) p5++;
+19
+20        }
+21        return (int) ugly[n-1];
+22            
+23    }
+24}
