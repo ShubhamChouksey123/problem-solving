2f442ed29331c3345db1ec5c102d3b96946d8b65
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-28 17:37:54 +0530

    [901] [Time Beats: 96.14%] [Memory Beats: 92.90%] - LeetPush

--- Online-Stock-Span.java
+++ Online-Stock-Span.java
@@ -1,26 +1,31 @@
 1class StockSpanner {
 2
-3    private List<Integer> list;
-4
-5    public StockSpanner() {
-6        list = new ArrayList<>();
-7    }
-8    
-9    public int next(int price) {
-10
-11        int span = 1;
-12        int n = list.size(); int index = n-1;
-13        while(index >= 0 && list.get(index) <= price){
-14            span++; index--;
-15        }
-16
-17        list.add(price); 
-18        return span; 
-19    }
-20}
-21
-22/**
-23 * Your StockSpanner object will be instantiated and called as such:
-24 * StockSpanner obj = new StockSpanner();
-25 * int param_1 = obj.next(price);
-26 */
+3    private List<Integer> prices;
+4    private List<Integer> spans;
+5
+6    public StockSpanner() {
+7        prices = new ArrayList<>();
+8        spans = new ArrayList<>();
+9    }
+10    
+11    public int next(int price) {
+12
+13        int span = 1;
+14        int n = prices.size(); int index = n - 1;
+15        
+16        while(index >= 0 && prices.get(index) <= price){
+17            span = span + spans.get(index);
+18            index = index - spans.get(index);
+19        }
+20
+21        prices.add(price);
+22        spans.add(span); 
+23        return span; 
+24    }
+25}
+26
+27/**
+28 * Your StockSpanner object will be instantiated and called as such:
+29 * StockSpanner obj = new StockSpanner();
+30 * int param_1 = obj.next(price);
+31 */
