90cdf94eabcd76736af604f23916748b9d479cb8
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-28 12:01:28 +0530

    [503] [Time Beats: 99.45%] [Memory Beats: 77.71%] - LeetPush

--- Next-Greater-Element-II.java
+++ Next-Greater-Element-II.java
@@ -1,23 +1,22 @@
 1class Solution {
 2    public int[] nextGreaterElements(int[] nums) {
-3        int n = nums.length;
-4        /**
-5        * stack containing num[i] and i => value and index
-6         */
-7        Stack<int[]> st = new Stack<>();
-8        int[] nextGreaterOnRight = new int[n];
-9        Arrays.fill(nextGreaterOnRight, -1);
-10        
-11        for(int i = 0 ; i < 2*n ; i++){
-12            while(!st.isEmpty() && st.peek()[0] < nums[i % n]){
-13                int[] topElement = st.pop();
-14                nextGreaterOnRight[topElement[1]] = nums[i % n];
-15            }
-16            if(i < n){
-17                st.push(new int[]{nums[i], i});
-18            }
-19            
-20        }
-21        return nextGreaterOnRight;
-22    }
-23}
+3        
+4        Deque<Integer> st = new ArrayDeque<Integer>();
+5        int n = nums.length;
+6        int[] nextGreaterElement = new int[n];
+7        Arrays.fill(nextGreaterElement, -1);
+8
+9        for(int i = 0 ; i < (2 * n) ; i++){
+10
+11            while(!st.isEmpty() && nums[st.peek()] < nums[i % n]){
+12                nextGreaterElement[st.pop()] = nums[i % n];
+13            }
+14
+15            if(i < n){
+16                st.push(i);
+17            }    
+18            
+19        }
+20        return nextGreaterElement;
+21    }
+22}
