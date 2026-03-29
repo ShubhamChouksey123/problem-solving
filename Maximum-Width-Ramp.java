6ae131e85497a6a7a51481d215196b03c401b401
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-29 11:53:01 +0530

    [962] [Time Beats: 89.67%] [Memory Beats: 82.27%] - LeetPush

--- Maximum-Width-Ramp.java
+++ Maximum-Width-Ramp.java
@@ -1,26 +1,26 @@
 1class Solution {
 2    public int maxWidthRamp(int[] nums) {
 3
-4        int n = nums.length;
-5        int[] greatestOnRight = new int[n];
-6        greatestOnRight[n-1] = nums[n-1];
-7
-8        int maxvalue = Integer.MIN_VALUE;
-9        for(int i = n - 2 ; i >= 0 ; i--){
-10            greatestOnRight[i] = Math.max(greatestOnRight[i+1], nums[i]);
-11        }
-12
-13        int start = 0;
-14        int maxRamp = 0;
-15        for(int end = 1 ; end < n ; end++){
-16            if(nums[start] <= nums[end]){
-17                maxRamp = Math.max(maxRamp, end - start);
+4        int n = nums.length; 
+5        int start = 0;
+6        int maxRamp = 0, ramp = 0;
+7        int[] greatestOnRight = new int[n];
+8        greatestOnRight[n-1] = nums[n-1];
+9
+10        for(int j = n - 2 ; j >= 0 ; j--){
+11            greatestOnRight[j] = Math.max(greatestOnRight[j+1], nums[j]);
+12        }
+13
+14        for(int end = 1 ; end < n ; end++){
+15            while(end < n && nums[start] <= greatestOnRight[end]){
+16                maxRamp = Math.max(maxRamp, end - start);
+17                end++;
 18            }
-19            else if(nums[start] > greatestOnRight[end]){
+19            if(end < n && nums[start] > greatestOnRight[end]){
 20                start++;
 21            }
-22        }
-23        
-24        return maxRamp;
+22        }  
+23        return maxRamp; 
+24        
 25    }
 26}
