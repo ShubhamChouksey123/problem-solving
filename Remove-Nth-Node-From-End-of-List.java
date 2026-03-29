564d069952fc7ea0bed344b74038dc8d849a6a16
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-29 22:16:27 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 84.07%] - LeetPush

--- Remove-Nth-Node-From-End-of-List.java
+++ Remove-Nth-Node-From-End-of-List.java
@@ -10,39 +10,25 @@
 10 */
 11class Solution {
 12    public ListNode removeNthFromEnd(ListNode head, int n) {
-13
-14        if (head == null || head.next == null) {
-15            return null;
-16        }
-17
-18        ListNode dummy = new ListNode(0);
-19        dummy.next = head;
-20
+13        ListNode dummy = new ListNode(-1);
+14        dummy.next = head;
+15        ListNode slow = dummy, fast = dummy;
+16
+17        while(n > 0){
+18            fast = fast.next;
+19            n --;
+20        }
 21
-22        ListNode current = head;
-23
-24        int times = n;
-25        while (times > 0) {
-26            times--;
-27            current = current.next;
-28        }
-29
+22        while(fast != null && fast.next != null){
+23            slow = slow.next;
+24            fast = fast.next;
+25        }
+26
+27        if(slow != null && slow.next != null){
+28            slow.next = slow.next.next;
+29        }
 30
-31        ListNode leading = current;
-32        ListNode trailing = head;
-33
-34        if(leading == null){
-35            return head.next;
-36        }
-37
-38        while (leading != null &&  leading.next != null) {
-39            leading = leading.next;
-40            trailing = trailing.next;
-41        }
-42
-43
-44        trailing.next = trailing.next.next;
-45        return head;
-46    }
-47
-48}
+31        return dummy.next;
+32        
+33    }
+34}
