fce793941a6826168ac1c2dd37b566f98c474c92
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-29 22:30:48 +0530

    [287] [Time Beats: 92.31%] [Memory Beats: 32.48%] - LeetPush

--- Find-the-Duplicate-Number.java
+++ Find-the-Duplicate-Number.java
@@ -1,21 +1,23 @@
 1class Solution {
 2    public int findDuplicate(int[] nums) {
 3
-4        int n = nums.length, slow = 0, fast = 0;
-5        fast = nums[fast];
+4        int slow = 0, fast = 0;
+5        slow = nums[slow];
 6        fast = nums[fast];
-7        slow = nums[slow];
-8        while(fast != slow){
-9            fast = nums[fast];
-10            fast = nums[fast];
-11            slow = nums[slow];
-12        }
-13
-14        fast = 0;
-15        while(slow != fast){
-16            slow = nums[slow];
+7        fast = nums[fast];
+8
+9        while(slow != fast){
+10            slow = nums[slow];
+11            fast = nums[fast];
+12            fast = nums[fast];
+13        }
+14
+15        slow = 0;
+16        while(slow != fast){
 17            fast = nums[fast];
-18        } 
-19        return slow;
-20    }
-21}
+18            slow = nums[slow];
+19        }
+20
+21        return slow;
+22    }
+23}
