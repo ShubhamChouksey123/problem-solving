c7fe82cb7fb479798b2bb4db76e76148f114d7ab
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-31 20:47:52 +0530

    [187] [Time Beats: 55.41%] [Memory Beats: 50.85%] - LeetPush

--- Repeated-DNA-Sequences.java
+++ Repeated-DNA-Sequences.java
@@ -1,30 +1,23 @@
-1class Solution {
-2    public List<String> findRepeatedDnaSequences(String s) {
-3
-4        int n = s.length();
-5        List<String> result = new ArrayList<>();
-6        if(n <= 10)
-7            return result;
-8        
-9        Set<String> set = new HashSet<>();
-10        Set<String> resultSet = new HashSet<>();
-11        StringBuilder stringBuilder = new StringBuilder();
-12        for(int i = 0 ; i < 10; i++){
-13            stringBuilder.append(s.charAt(i));
-14        }
-15        set.add(stringBuilder.toString());
-16        
-17        for(int i = 10 ; i < n ; i++){
-18            stringBuilder.append(s.charAt(i));;
-19            stringBuilder.delete(0, 1);
-20            if(set.contains(stringBuilder.toString()) && !resultSet.contains(stringBuilder.toString()) ){
-21                result.add(stringBuilder.toString());
-22                resultSet.add(stringBuilder.toString());
-23            }else{
-24                set.add(stringBuilder.toString());
-25            }
-26        }
-27        return result;
-28        
-29    }
-30}
+1class Solution {    
+2
+3    private static final int K = 10;
+4
+5    public List<String> findRepeatedDnaSequences(String s) {
+6
+7        int n = s.length();
+8        Set<String> seen = new HashSet<>();
+9        Set<String> repeated = new HashSet<>();
+10
+11        for(int i = 0 ; i <= n - K ; i++){
+12            String substring = s.substring(i, i + K);
+13
+14            if(seen.contains(substring) && !repeated.contains(substring)){
+15                repeated.add(substring);
+16            }
+17
+18            seen.add(substring);
+19        }
+20        
+21        return new ArrayList<>(repeated);
+22    }
+23}
