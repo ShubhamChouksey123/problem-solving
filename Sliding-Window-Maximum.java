54fbcc8b75665f45e71e5e1165faf7d520de4ccc
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-03-31 22:33:49 +0530

    [239] [Time Beats: 85.64%] [Memory Beats: 41.34%] - LeetPush

--- Sliding-Window-Maximum.java
+++ Sliding-Window-Maximum.java
@@ -1,28 +1,29 @@
 1class Solution {
 2    public int[] maxSlidingWindow(int[] nums, int k) {
-3        Deque<Integer> dq = new ArrayDeque<>();
-4        int n = nums.length;
-5        int[] result = new int[n - k + 1];
-6
-7        
-8        for(int i = 0 ; i < n ; i++){
-9
-10            if(!dq.isEmpty() && dq.peekFirst() <= i - k){
-11                dq.pollFirst();
-12            }
-13            int cur = nums[i];
-14            
-15            while(!dq.isEmpty() && cur > nums[dq.peekLast()]){
-16                dq.pollLast();
-17            }
-18
-19            dq.offerLast(i);
-20            
-21            if(i + 1 >= k){
-22                result[i-k+1] = nums[dq.peekFirst()];
-23            }
-24        }
-25
-26        return result;
-27    }
-28}
+3
+4        /**
+5            Deque containg value and its index {value, index}
+6         */
+7        Deque<int[]> elements = new ArrayDeque<>();
+8        int n = nums.length;
+9        int[] maxSlidingValue = new int[n-k+1];
+10
+11        for(int i = 0 ; i < n ; i++){
+12
+13            if(!elements.isEmpty() && elements.peekFirst()[1] <= i - k){
+14                elements.pollFirst();
+15            }
+16
+17            int cur = nums[i];
+18            while(!elements.isEmpty() && elements.peekLast()[0] <= cur){
+19                elements.pollLast();
+20            }
+21            elements.offerLast(new int[]{cur, i});
+22            if(i >= k - 1){
+23                maxSlidingValue[i-k+1] = elements.peekFirst()[0];
+24            }
+25        }
+26
+27        return maxSlidingValue;
+28    }
+29}
