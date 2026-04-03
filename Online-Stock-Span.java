4dfcffb3b9e9d69846696053e5da6cf04a1164a2
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-03 23:11:00 +0530

    [901] [Time Beats: 88.02%] [Memory Beats: 8.88%] - LeetPush

--- Online-Stock-Span.java
+++ Online-Stock-Span.java
@@ -1,30 +1,31 @@
 1class StockSpanner {
 2
-3    /**
-4        stack containt {price, span}
-5     */
-6    private Deque<int[]> stack;
-7
-8    public StockSpanner() {
-9        stack = new ArrayDeque<>();
-10    }
-11    
-12    public int next(int price) {
-13        
+3    private Deque<List<Integer>> stockSpanStack;
+4
+5    public StockSpanner() {
+6
+7        /**
+8            stockSpan stack containing list of {value of stock, span}
+9         */
+10        stockSpanStack = new ArrayDeque<>();
+11    }
+12    
+13    public int next(int price) {
 14        int span = 1;
 15
-16        while(!stack.isEmpty() && stack.peek()[0] <= price){
-17            span += stack.pop()[1];
-18        }
-19
-20        stack.push(new int[]{price, span});
-21        return span;
-22
-23    }
-24}
-25
-26/**
-27 * Your StockSpanner object will be instantiated and called as such:
-28 * StockSpanner obj = new StockSpanner();
-29 * int param_1 = obj.next(price);
-30 */
+16        while(!stockSpanStack.isEmpty() && stockSpanStack.peek().get(0) <= price){
+17            List<Integer> topStockSpan = stockSpanStack.poll();
+18            span += topStockSpan.get(1);
+19        }
+20
+21        stockSpanStack.push(List.of(price, span));
+22        return span;
+23        
+24    }
+25}
+26
+27/**
+28 * Your StockSpanner object will be instantiated and called as such:
+29 * StockSpanner obj = new StockSpanner();
+30 * int param_1 = obj.next(price);
+31 */
