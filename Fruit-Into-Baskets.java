40b765485bf722cb6fb2c1693f5788d25215ebf9
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-04 15:53:43 +0530

    [904] [Time Beats: 90.23%] [Memory Beats: 93.45%] - LeetPush

--- Fruit-Into-Baskets.java
+++ Fruit-Into-Baskets.java
@@ -1,33 +1,30 @@
 1class Solution {
 2    public int totalFruit(int[] fruits) {
-3        
-4        int start = 0, maxLen = 0;
-5        Map<Integer, Integer> mp = new HashMap<>();
+3
+4        int n = fruits.length, len = 0, maxLen = 0, start = 0;
+5        Map<Integer, Integer> countOfFruits = new HashMap<>();
 6
-7        for(int end = 0 ; end < fruits.length ; end++){
-8            int fruit = fruits[end];
-9            
-10            if(mp.size() == 2 && !mp.containsKey(fruit)){
-11                while(mp.size() > 1){
-12                    int key = fruits[start];
-13                    int count = mp.get(key);  
-14                    --count;
-15                    if(count == 0){
-16                        mp.remove(key);
-17                    }else{
-18                        mp.put(key, count);
-19                    }
-20                    start++;
-21                }
-22                mp.put(fruit, 1);
-23            }
-24            else{
-25                mp.put(fruit , mp.getOrDefault(fruit, 0) + 1);
-26            }
-27
-28            maxLen = Math.max(maxLen, end - start + 1);
-29        }
-30        return maxLen;
-31        
-32    }
-33}
+7        for(int end = 0 ; end < n ; end++){
+8            countOfFruits.put(fruits[end], countOfFruits.getOrDefault(fruits[end], 0) + 1);
+9           
+10            while(countOfFruits.size() > 2){
+11                int curCount = countOfFruits.get(fruits[start]);
+12                if(curCount == 1){
+13                    countOfFruits.remove(fruits[start]);
+14                }else{
+15                    countOfFruits.put(fruits[start] , --curCount);
+16                }
+17                start++;
+18            }
+19            maxLen = Math.max(maxLen, end - start + 1);
+20        } 
+21        return maxLen;   
+22    }
+23}
+24
+25/**
+26[1,2,3,2,2]
+27[1,2,1,1,2,1,2,1,3,3,1,3,4,1,3,4,5,3,4,4,1,4,5,1,12,13,4,1,3,12,3,15]
+28[1,2,1,1,2,1,2,1,3,3,1,3,4]
+29[3,3,3,1,2,1,1,2,3,3,4]
+30 */
