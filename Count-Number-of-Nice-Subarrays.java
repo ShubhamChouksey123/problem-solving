012eb86a37e43646c72331d3cc76bfb894d04e9e
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-04 20:33:32 +0530

    [1248] [Time Beats: 81.64%] [Memory Beats: 20.15%] - LeetPush

--- Count-Number-of-Nice-Subarrays.java
+++ Count-Number-of-Nice-Subarrays.java
@@ -1,31 +1,39 @@
 1class Solution {
-2    public int numberOfSubarrays(int[] nums, int k) {
-3        
-4        return atMost(nums, k) - atMost(nums, k - 1);
-5
-6    }
+2
+3    /**
+4        returns number of subarrays with max k off numbers
+5     */
+6    private int numberOfSubArraysWithMaxK(int[] nums, int k) {
 7
-8    public int atMost(int[] nums, int k) {
-9
-10        if(k < 0) 
-11            return 0;
-12        
-13        int start = 0, countOdd = 0, ans = 0;
-14        for (int end = 0; end < nums.length; end++) {
-15
-16            if(nums[end] %2 != 0){
-17                countOdd++;
-18            }
-19
-20            while(start <= end && countOdd > k){
-21                if(nums[start] %2 != 0){
-22                    countOdd--;
+8        // if(k == 0){
+9        //     return 0;
+10        // }
+11
+12        int n = nums.length, countOfOdds = 0, totalSubArray = 0;
+13        int start = 0;
+14
+15        for(int end = 0 ; end < n ; end++){
+16
+17            if( (nums[end] & 1) != 0 ){
+18                countOfOdds++;
+19            }
+20            while(start <= end && countOfOdds > k){
+21                if( (nums[start] & 1) != 0 ){
+22                    countOfOdds--;
 23                }
 24                start++;
 25            }
-26            ans = ans + (end - start + 1);
-27        }
-28        return ans; 
-29        
-30    }
-31}
+26            
+27            totalSubArray += (end - start + 1);
+28            
+29        }
+30        return totalSubArray;
+31    }
+32
+33    public int numberOfSubarrays(int[] nums, int k) {
+34        return numberOfSubArraysWithMaxK(nums, k) - numberOfSubArraysWithMaxK(nums, k - 1);
+35    }
+36
+37
+38    
+39}
