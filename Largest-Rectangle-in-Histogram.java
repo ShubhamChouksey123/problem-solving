616fff992cb414ef35864c728813fa3d96fcebec
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-04 11:59:20 +0530

    [84] [Time Beats: 93.32%] [Memory Beats: 19.01%] - LeetPush

--- Largest-Rectangle-in-Histogram.java
+++ Largest-Rectangle-in-Histogram.java
@@ -2,34 +2,25 @@
 2    public int largestRectangleArea(int[] heights) {
 3
 4        int n = heights.length;
-5        int maxArea = 0;
-6
-7        /**
-8            stack containing the valid start element height and its index of rectangle.
-9         */
-10        Deque<List<Integer>> startElemets = new ArrayDeque<>();
-11
-12        for(int i = 0 ; i < n ; i++){
-13            while(!startElemets.isEmpty() && startElemets.peek().get(0) > heights[i]){
-14                List<Integer> startElement = startElemets.pop();
-15                int height = startElement.get(0);
-16                int width = (!startElemets.isEmpty()) ? (i - startElemets.peek().get(1) - 1) : (i) ; 
-17                int curArea = height * width ;
-18                maxArea = Math.max(maxArea, curArea);
-19            }
-20
-21            startElemets.push(List.of(heights[i], i));
+5        int area = heights[0], maxArea = heights[0];
+6        Deque<Integer> increasingHeights = new ArrayDeque<>();
+7
+8        for(int i = 0 ; i < n ; i++){
+9            
+10            while(!increasingHeights.isEmpty() && heights[increasingHeights.peek()] > heights[i]){
+11                int height = heights[increasingHeights.poll()];
+12                int width = (increasingHeights.isEmpty()) ? i : i - increasingHeights.peek() - 1;
+13                maxArea = Math.max(maxArea, height * width);
+14            }   
+15            increasingHeights.push(i);
+16        }
+17
+18        while(!increasingHeights.isEmpty()){
+19            int height = heights[increasingHeights.poll()];
+20            int width = (increasingHeights.isEmpty()) ? n : n - increasingHeights.peek() - 1;
+21            maxArea = Math.max(maxArea, height * width);
 22        }
 23
-24        while(!startElemets.isEmpty()){
-25            List<Integer> startElement = startElemets.pop();
-26            int height = startElement.get(0);
-27            int width = (!startElemets.isEmpty()) ? (n - startElemets.peek().get(1) - 1) : (n) ; 
-28            int curArea = height * width ;
-29            maxArea = Math.max(maxArea, curArea);
-30        }
-31
-32
-33        return maxArea;
-34    }
-35}
+24        return maxArea;
+25    }
+26}
