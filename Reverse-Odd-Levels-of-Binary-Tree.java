a1f3a93a4888abf862ecb36adfdb8e1354133304
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-05 13:33:07 +0530

    [2415] [Time Beats: 93.65%] [Memory Beats: 74.32%] - LeetPush

--- Reverse-Odd-Levels-of-Binary-Tree.java
+++ Reverse-Odd-Levels-of-Binary-Tree.java
@@ -14,59 +14,24 @@
 14 * }
 15 */
 16class Solution {
-17
-18    private void swapChildNodeValues(TreeNode node){
-19
-20        if(node.left == null){
-21            return;
-22        }
-23        TreeNode tmp = node.left;
-24        node.left = node.right;
-25        node.right = tmp;
-26    }
+17    public TreeNode reverseOddLevels(TreeNode root) {
+18        reverseOddLevelsUtil(root.left, root.right, 0);
+19        return root;
+20    }
+21
+22    public void reverseOddLevelsUtil(TreeNode node1, TreeNode node2, int level) {
+23
+24        if(node1 == null || node2 == null){
+25            return; 
+26        }
 27
-28    public TreeNode reverseOddLevels(TreeNode root) {
-29
-30        Deque<TreeNode> queue = new ArrayDeque<>();
-31        queue.add(root);
-32        int level = 0, startIndex = 0;
-33        List<List<Integer>> values = new ArrayList<>();
-34        levelOrder(root, values, 0);
-35
-36
-37        while(!queue.isEmpty()){
-38
-39            int size = queue.size();
-40            int endIndex = startIndex + size - 1;
-41            for(int i = 0 ; i < size ; i++){
-42                TreeNode node = queue.pollFirst();
-43
-44                if(level % 2 != 0) {
-45                    node.val = values.get(level).get(size - i - 1);
-46                }else{
-47                    node.val = values.get(level).get(i);
-48                }
-49                
-50                if(node.left  != null)  queue.offerLast(node.left);
-51                if(node.right != null)  queue.offerLast(node.right);
-52            }
-53            startIndex = endIndex + 1;
-54            level++;
-55        }
-56        return root;
-57    }
-58
-59    public void levelOrder(TreeNode root, List<List<Integer>> values, int level) {
-60
-61        if(root == null)
-62            return;
-63
-64        if(values.size() == level){
-65            values.add(new ArrayList<>());
-66        }
-67        
-68        values.get(level).add(root.val);
-69        levelOrder(root.left,  values, level + 1);
-70        levelOrder(root.right, values, level + 1);
-71    }
-72}
+28        if(level %2 == 0){
+29            int tmp = node1.val;
+30            node1.val = node2.val;
+31            node2.val = tmp;
+32        }
+33
+34        reverseOddLevelsUtil(node1.left , node2.right, level + 1);
+35        reverseOddLevelsUtil(node1.right, node2.left , level + 1); 
+36    }
+37}
