8a703521a6cd6f484cc47241617ce1510cd544e5
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-15 23:38:35 +0530

    [1162] [Time Beats: 6.41%] [Memory Beats: 45.41%] - LeetPush

--- As-Far-from-Land-as-Possible.java
+++ As-Far-from-Land-as-Possible.java
@@ -1,58 +1,52 @@
 1class Solution {
-2    private final int[][] directions = new int[][]{
-3        {1,0}, {0, 1}, {-1, 0}, {0, -1}
-4    }; 
-5
-6    private boolean liesInRange(int n, int m, int x, int y){
-7        if(x >= 0 && x < n && y >= 0 && y < m)
-8            return true;
-9        return false;
-10    }
-11
-12    private void addAllNeighbours(int[][] grid, boolean[][] visited, Queue<int[]> queue, int x, int y, int distance){
+2
+3    private int N;
+4    private int M;
+5    private static final int[][] DIRECTIONS = new int[][]{
+6        {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+7    }; 
+8
+9    private void addAllNeighbours(int[][] grid, boolean[][] visited, int x, int y, int distance, Deque<int[]> queue){
+10        for(int[] direction : DIRECTIONS){
+11            int x1 = x + direction[0];
+12            int y1 = y + direction[1];
 13
-14        int n = grid.length, m = grid[0].length;
-15        for(int i = 0 ; i < 4 ; i++){
-16            int x1 = x + directions[i][0];
-17            int y1 = y + directions[i][1];
-18            if(liesInRange(n, m, x1, y1) && grid[x1][y1] == 0 && !visited[x1][y1]){
-19                visited[x1][y1] = true;
-20                queue.add(new int[]{x1, y1, distance});
-21            }
-22        }
-23
-24    }
-25    
-26    public int maxDistance(int[][] grid) {
-27
-28        int n = grid.length, m = grid[0].length;
-29        boolean[][] visited = new boolean[n][m];
-30        Queue<int[]> queue = new ArrayDeque<>();
-31
-32
-33        int landCount = 0;
-34        for(int i = 0 ; i < n ; i++){
-35            for(int j = 0 ; j < m ; j++){
-36                if(grid[i][j] == 1){
-37                    visited[i][j] = true; landCount++;
-38                    queue.add(new int[]{i, j, 0});
-39                }
-40            }
-41        }
-42
-43        if(landCount == n*m || landCount == 0){
-44            return -1;
-45        }
+14            if(x1 >= 0 && x1 < N && y1 >= 0 && y1 < M && grid[x1][y1] == 0 && !visited[x1][y1]){
+15                queue.offerLast(new int[]{x1, y1, distance});
+16                visited[x1][y1] = true;
+17            }
+18        }
+19    }
+20
+21    public int maxDistance(int[][] grid) {
+22        N = grid.length; M = grid[0].length;
+23        boolean[][] visited = new boolean[N][M];
+24
+25        Deque<int[]> queue = new ArrayDeque<>();
+26        int countLand = 0;
+27        
+28        for(int i = 0 ; i < N ; i++){
+29            for(int j = 0 ; j < M ; j++){
+30                if(grid[i][j] == 1){
+31                    visited[i][j] = true;
+32                    queue.offerLast(new int[]{i, j, 0}); countLand++;
+33                }
+34            }
+35        }
+36
+37        if(countLand == 0 || countLand == N * M){
+38            return -1;
+39        }
+40
+41        int distance = 0;
+42        while(!queue.isEmpty()){
+43            int[] element = queue.pollFirst();
+44            int x = element[0], y = element[1];
+45            distance = element[2];
 46
-47        int dist = -1;
-48        while(!queue.isEmpty()){
-49            int[] node = queue.poll();
-50            int x = node[0], y = node[1];
-51            dist = node[2];
-52            addAllNeighbours(grid, visited, queue, x, y, dist + 1);
-53        }
-54
-55        return dist;
-56        
-57    }
-58}
+47            addAllNeighbours(grid, visited, x, y, distance + 1, queue);
+48        }
+49        return distance;
+50
+51    }
+52}
