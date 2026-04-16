9ee88729d7aedbf98f4a83306ddce1196d228f8e
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-16 21:24:30 +0530

    [802] [Time Beats: 68.47%] [Memory Beats: 49.69%] - LeetPush

--- Find-Eventual-Safe-States.java
+++ Find-Eventual-Safe-States.java
@@ -1,50 +1,63 @@
 1class Solution {
 2
-3    void print(boolean[] arr){
-4        for(int i = 0 ; i < arr.length ; i++){
-5            System.out.print(arr[i] + ", ");
-6        }
-7        System.out.println("");
-8    }
-9
-10    private boolean isCycle(int[][] graph, boolean[] visited, boolean[] recStack, int node){
-11
-12        // cycle detected
-13        if(recStack[node]){
-14            return true;
-15        } 
-16
-17        // if already visited and not in rec Stack then, no cycle.
-18        if(visited[node]){
-19            return false;
+3    private int N;
+4    
+5    private boolean isSafeNode(int[][] graph, boolean[] visited, int[] isSafeNode, int nodeIndex){
+6        
+7        if(isSafeNode[nodeIndex] != -1){
+8            return isSafeNode[nodeIndex] == 1;
+9        }
+10
+11        if(visited[nodeIndex]){
+12            isSafeNode[nodeIndex] = 0;
+13            return false;
+14        }
+15
+16        visited[nodeIndex] = true;
+17        boolean isSafe = true;
+18        if(graph[nodeIndex].length == 0){
+19            isSafe = true;
 20        }
 21
-22        visited[node] = true;
-23        recStack[node] = true;
-24
-25        for(int neighbour : graph[node]){
-26            if(isCycle(graph, visited, recStack, neighbour)){
-27                return true;
-28            }
-29        
-30        }
-31        recStack[node] = false;
-32        return false;
-33    }
-34
-35    public List<Integer> eventualSafeNodes(int[][] graph) {
+22        
+23        for(int neighbourIndex : graph[nodeIndex]){
+24            if(!isSafeNode(graph, visited, isSafeNode, neighbourIndex)){
+25                isSafe = false;
+26            }
+27        }
+28        visited[nodeIndex] = false;
+29        isSafeNode[nodeIndex] = (isSafe == true) ? 1 : 0; 
+30        return isSafe;
+31    }
+32    
+33    public List<Integer> eventualSafeNodes(int[][] graph) {
+34        N = graph.length;
+35        boolean[] visited = new boolean[N];
 36
-37        int n = graph.length;
-38        boolean[] visited = new boolean[n];
-39        boolean[] recStack = new boolean[n];
-40
-41        List<Integer> result = new ArrayList<>();
-42
-43        for(int i = 0 ; i < n ; i++){
-44            boolean isCycle = isCycle(graph, visited, recStack, i);
-45            if(!isCycle) result.add(i);
-46        }
-47
-48        return result; 
-49    }
-50}
+37        /**
+38            -1 not sure
+39            0 => not safe
+40            1 => safe
+41         */
+42        int[] isSafeNode = new int[N];
+43        Arrays.fill(isSafeNode, -1);
+44
+45        for(int i = 0 ; i < N ; i++){
+46            if(isSafeNode[i] == -1){
+47                isSafeNode(graph, visited, isSafeNode, i);
+48            }
+49        }
+50
+51        List<Integer> ans = new ArrayList<>();
+52        for(int i = 0 ; i < N ; i++){
+53            if(isSafeNode[i] == 1){
+54                ans.add(i);
+55            }
+56        }
+57
+58        
+59        return ans; 
+60
+61        
+62    }
+63}
