8f8c070cd6e3c094da9343bb845603d68d5333e3
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-16 20:40:59 +0530

    [1376] [Time Beats: 98.54%] [Memory Beats: 97.51%] - LeetPush

--- Time-Needed-to-Inform-All-Employees.java
+++ Time-Needed-to-Inform-All-Employees.java
@@ -1,35 +1,35 @@
 1class Solution {
 2
-3    private int getTime(int n, int headID, int[] manager, int[] informTime, int empIndex, int[] totalInformTime){
-4        if(manager[empIndex] == -1){
-5            totalInformTime[empIndex] = 0;
+3    private int getTotalInformTime(int n, int[] manager, int[] informTime, int[] totalInformTime, int empIndex){
+4
+5        if(totalInformTime[empIndex] != -1){
 6            return totalInformTime[empIndex];
 7        }
 8
-9        if(totalInformTime[empIndex] != -1){
-10            return totalInformTime[empIndex];
+9        if(manager[empIndex] == -1){
+10            return 0;
 11        }
 12
 13        int managerIndex = manager[empIndex];
-14        totalInformTime[empIndex] = informTime[managerIndex] + getTime(n, headID, manager, informTime, managerIndex, totalInformTime);
-15        return totalInformTime[empIndex];
-16    }
-17
-18    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-19
-20        int[] totalInformTime = new int[n];
-21        Arrays.fill(totalInformTime, -1);
-22
-23        for(int i = 0 ; i < n ; i++){
-24            if(totalInformTime[i] == -1){
-25                getTime(n, headID, manager, informTime, i, totalInformTime);
-26            }
-27        }
-28
-29        int max = 0;
-30        for(int i = 0 ; i < n ; i++){
-31            max = Math.max(max, totalInformTime[i]);
-32        }
-33        return max;
+14        int time = informTime[managerIndex] + getTotalInformTime(n, manager, informTime, totalInformTime, managerIndex);
+15        totalInformTime[empIndex] = time;
+16        return time;
+17    }
+18
+19    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+20
+21        int[] totalInformTime = new int[n];
+22        Arrays.fill(totalInformTime, -1);
+23
+24        for(int i = 0 ; i < n ; i++){
+25            totalInformTime[i] =  getTotalInformTime(n, manager, informTime, totalInformTime, i);
+26        }
+27
+28        int maxtime = Integer.MIN_VALUE;
+29        for(int i = 0 ; i < n ; i++){
+30            maxtime = Math.max(totalInformTime[i] , maxtime);
+31        }
+32        return maxtime;
+33
 34    }
 35}
