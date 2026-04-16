602584207f6eed9c5418944f7f22166efe2127a6
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-16 20:17:12 +0530

    [1020] [Time Beats: 51.74%] [Memory Beats: 58.31%] - LeetPush

--- Number-of-Enclaves.java
+++ Number-of-Enclaves.java
@@ -1,59 +1,51 @@
 1class Solution {
 2
-3    private final int[][] directions = {
-4        {1, 0}, {-1, 0}, {0, 1}, {0, -1}
-5    };
-6
-7    private boolean liesInRange(int n, int m, int x, int y){
-8        if(x >= 0 && x < n && y >= 0 && y < m){
-9            return true;
-10        }
-11        return false;
-12    }
+3    private int N;
+4    private int M;
+5    private static final int[][] DIRECTIONS = new int[][]{
+6        {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+7    };
+8
+9    private void dfs(int[][] grid, boolean[][] visited, int x, int y){
+10
+11        if(grid[x][y] == 0)
+12            return;
 13
-14
-15    private void dfs(int[][] grid, boolean[][] visited, int x, int y){
-16        int n = grid.length, m = grid[0].length;
-17        if(grid[x][y] != 1){
-18            return;
-19        }
-20
-21        visited[x][y] = true;
-22
-23        for(int i = 0 ; i < 4 ; i++){
-24            int x1 = x + directions[i][0];
-25            int y1 = y + directions[i][1];
-26            if(liesInRange(n, m, x1, y1) && grid[x1][y1] == 1 && !visited[x1][y1]){
-27                dfs(grid, visited, x1, y1);
-28            }
-29        }
-30    } 
-31
-32    public int numEnclaves(int[][] grid) {
-33
-34        int n = grid.length, m = grid[0].length;
-35        boolean[][] visited = new boolean[n][m];
-36
-37        for(int i = 0 ; i < n ; i++){
-38            dfs(grid, visited, i, 0);   // left 
-39            dfs(grid, visited, i, m-1); // right 
-40        }
-41
-42        for(int j = 1 ; j < m - 1 ; j++){
-43            dfs(grid, visited, 0, j);   // top
-44            dfs(grid, visited, n-1, j); // bottom
-45        }
-46
-47        int ans = 0;
-48        for(int i = 0 ; i < n ; i++){
-49            for(int j = 0 ; j < m ; j++){
-50                if(grid[i][j] == 1 && !visited[i][j]){
-51                    ans++;
-52                }
-53            }
-54        }
-55        return ans;
-56
-57        
-58    }
-59}
+14        visited[x][y] = true;
+15
+16        for(int[] direction : DIRECTIONS){
+17            int x1 = x + direction[0];
+18            int y1 = y + direction[1];
+19
+20            if(x1 >= 0 && x1 < N && y1 >= 0 && y1 < M && grid[x1][y1] == 1 && !visited[x1][y1]){
+21                dfs(grid, visited, x1, y1);
+22            }
+23        }
+24    }
+25
+26    public int numEnclaves(int[][] grid) {
+27
+28        N = grid.length; M = grid[0].length;
+29        boolean[][] visited = new boolean[N][M];
+30
+31        for(int j = 0 ; j < M ; j++){
+32            dfs(grid, visited, 0, j);
+33            dfs(grid, visited, N-1, j);
+34        }
+35
+36        for(int i = 0 ; i < N ; i++){
+37            dfs(grid, visited, i, 0);
+38            dfs(grid, visited, i, M-1);
+39        }
+40        int count = 0;
+41        for(int i = 0 ; i < N ; i++){
+42            for(int j = 0 ; j < M ; j++){
+43                if(grid[i][j] == 1 && !visited[i][j]){
+44                    count++;
+45                }
+46            }
+47        }
+48        return count;
+49        
+50    }
+51}
