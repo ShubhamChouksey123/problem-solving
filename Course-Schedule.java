82bf7c6432885eee1bb38a3b9e5ce3c48a6970a1
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-18 00:22:10 +0530

    [207] [Time Beats: 5.01%] [Memory Beats: 5.05%] - LeetPush

--- Course-Schedule.java
+++ Course-Schedule.java
@@ -1,49 +1,56 @@
 1class Solution {
-2    
-3    private boolean isValid(int numCourses, List<List<Integer>> adj, Deque<Integer> stack, Set<Integer> inStack, boolean[] added, int course){
-4
-5        if(inStack.contains(course)){
-6            return false;
-7        }
-8
-9        stack.push(course);
-10        inStack.add(course);
-11
-12        for(int nextCourse : adj.get(course)){
-13            if(!added[nextCourse] && !isValid(numCourses, adj, stack, inStack, added, nextCourse)){
-14                return false;
-15            }
-16        }
-17        added[course] = true;
-18        stack.pop();
-19        inStack.remove(course);
-20        return true;
-21    }
-22    
-23    public boolean canFinish(int numCourses, int[][] prerequisites) {
-24        List<List<Integer>> adj = new ArrayList<>(numCourses);
-25        for(int i = 0 ; i < numCourses ; i++){
-26            adj.add(new ArrayList<>());
+2
+3    /**
+4        We will use the kahn'a Algorithm
+5     */
+6    public boolean canFinish(int numCourses, int[][] prerequisites) {
+7
+8        List<List<Integer>> adj = new ArrayList<>(numCourses);
+9        for(int i = 0 ; i < numCourses ; i++){
+10            adj.add(new ArrayList<>());
+11        }
+12
+13        int[] countOutwardEdges = new int[numCourses];
+14        for(int i = 0; i < prerequisites.length ; i++){
+15            int[] prerequisite = prerequisites[i];
+16            int u = prerequisite[1]; int v = prerequisite[0];
+17            adj.get(u).add(v);
+18            countOutwardEdges[v]++;
+19        }
+20        System.out.println("adj : " + adj);
+21        
+22        Deque<Integer> queue = new ArrayDeque<>();
+23        for(int i = 0 ; i < numCourses ; i++){
+24            if(countOutwardEdges[i] == 0){
+25                queue.offerLast(i);
+26            }
 27        }
-28        
-29        for(int i = 0; i < prerequisites.length ; i++){
-30            int[] prerequisite = prerequisites[i];
-31            adj.get(prerequisite[1]).add(prerequisite[0]);
-32        }
-33
-34        Deque<Integer> stack = new ArrayDeque<>();
-35        Set<Integer> inStack = new HashSet<>();
-36        boolean[] added = new boolean[numCourses];
-37
-38        for(int i = 0 ; i < numCourses ; i++){
-39            if(!added[i]){
-40                if(!isValid(numCourses, adj, stack, inStack, added, i)){
-41                    return false;
-42                }
-43            }
-44        }
-45
-46        
-47        return true;
-48    }
-49}
+28        if(queue.isEmpty()){
+29            return false;
+30        }
+31        boolean[] added = new boolean[numCourses];
+32
+33        while(!queue.isEmpty()){
+34            int node = queue.pollFirst();
+35            added[node] = true;
+36            System.out.println("Added node : " + node);
+37            for(int neighbour : adj.get(node)){
+38                if(added[neighbour]){
+39                    return false;
+40                }
+41                if(--countOutwardEdges[neighbour] == 0){
+42                    queue.offerLast(neighbour);
+43                }
+44            }
+45        }
+46
+47        for(int i = 0 ; i < numCourses ; i++){
+48            if(!added[i]){
+49                return false;
+50            }
+51        }
+52
+53        return true;
+54
+55    }
+56}
