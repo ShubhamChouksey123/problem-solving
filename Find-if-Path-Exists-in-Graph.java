8e71cca79f7d442cd9e6a85ad5e648889bbaef2e
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-19 00:08:20 +0530

    [1971] [Time Beats: 95.04%] [Memory Beats: 99.15%] - LeetPush

--- Find-if-Path-Exists-in-Graph.java
+++ Find-if-Path-Exists-in-Graph.java
@@ -1,38 +1,45 @@
 1class Solution {
-2
-3    private boolean isPathExists(int n, List<List<Integer>> adj, boolean[] visited, int destination, int node){
+2    private int[] parent;
+3    private int[] rank;
 4
-5        if(destination == node){
-6            return true;
-7        }
-8        visited[node] = true;
+5    private int find(int x){
+6        if(parent[x] != x){
+7            parent[x] = find(parent[x]);
+8        }
 9
-10        for(int neighbour : adj.get(node)){
-11            if(!visited[neighbour]){
-12                if(isPathExists(n, adj, visited, destination, neighbour)){
-13                    return true;
-14                }
-15            }
-16        }
-17        return false;
-18    }
-19
-20    /**
-21        check a path exists or not via DFS
-22     */
-23    public boolean validPath(int n, int[][] edges, int source, int destination) {
-24        List<List<Integer>> adj = new ArrayList<>();
-25        for(int i = 0 ; i < n ; i++){
-26            adj.add(new ArrayList<>());
+10        return parent[x];
+11    }
+12
+13    private void union(int x, int y){
+14        
+15        int parentX = find(x);
+16        int parentY = find(y);
+17
+18        if(parentX == parentY)  
+19            return;
+20
+21        if(rank[parentX] >= rank[parentY]){
+22            parent[parentY] = parentX;
+23            rank[parentX] += rank[parentY];
+24        }else{
+25            parent[parentX] = parentY;
+26            rank[parentY] += rank[parentX];
 27        }
-28        for(int[] edge : edges){
-29            int u = edge[0], v = edge[1];
-30            adj.get(u).add(v);
-31            adj.get(v).add(u);
-32        }   
+28    }
+29
+30    public boolean validPath(int n, int[][] edges, int source, int destination) {
+31        parent = new int[n];
+32        rank = new int[n];
 33
-34        boolean[] visited = new boolean[n];
-35
-36        return isPathExists(n, adj, visited, destination, source); 
-37    }
-38}
+34        for(int i = 0 ; i < n ; i++){
+35            rank[i] = 1;
+36            parent[i] = i;
+37        }
+38
+39        for(int[] edge : edges){
+40            union(edge[0], edge[1]);
+41        }
+42
+43        return find(source) == find(destination);
+44    }
+45}
