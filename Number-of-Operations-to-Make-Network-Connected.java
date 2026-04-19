7e68ecce86e4273818abdde768c40d3c7ca2cdbb
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-19 11:59:27 +0530

    [1319] [Time Beats: 99.43%] [Memory Beats: 54.76%] - LeetPush

--- Number-of-Operations-to-Make-Network-Connected.java
+++ Number-of-Operations-to-Make-Network-Connected.java
@@ -2,66 +2,58 @@
 2    
 3    private int[] parent;
 4    private int[] rank;
-5
-6    private int find(int x){
-7        if(parent[x] != x){
-8            parent[x] = find(parent[x]);
-9        }
-10        return parent[x];
-11    }
-12
-13    private void union(int x, int y){
-14        int rootX = find(x);
-15        int rootY = find(y);
-16
-17        if(rootX == rootY)  
-18            return;
-19
-20        if(rank[rootX] > rank[rootY]){
-21            parent[rootY] = rootX;
-22        }
-23        else if(rank[rootX] < rank[rootY]){
-24            parent[rootX] = rootY;
-25        }
-26        else{
-27            parent[rootY] = rootX;
-28            rank[rootX]++;
-29        }
-30    }
-31
-32    public int makeConnected(int n, int[][] connections) {
-33        
-34        parent = new int[n];
-35        rank = new int[n];
-36
-37        for(int i = 0 ; i < n ; i++){
-38            parent[i] = i;
-39            rank[i] = 1;
-40        }
-41
-42        int edgesNotNeeded = 0;
-43        for(int[] connection : connections){
-44            int x = connection[0], y = connection[1];
-45            int rootX = find(x);
-46            int rootY = find(y); 
-47            if(rootX == rootY){
-48                edgesNotNeeded++;
-49            }else{
-50                union(connection[0], connection[1]);
-51            }
-52        }
-53        
-54
-55        Set<Integer> roots = new HashSet<>();
-56        for(int i = 0 ; i < n ; i++){
-57            int root = find(i);
-58            roots.add(root);
-59        }
-60        int edgesNeeded = roots.size() - 1;
-61
-62        if(edgesNeeded > edgesNotNeeded){
-63            return -1;
-64        }
-65        return edgesNeeded;
-66    }
-67}
+5    private int components;
+6
+7    private int find(int x){
+8        if(parent[x] != x){
+9            parent[x] = find(parent[x]);
+10        }
+11        return parent[x];
+12    }
+13
+14    private boolean union(int x, int y){
+15        int rootX = find(x);
+16        int rootY = find(y);
+17
+18        if(rootX == rootY)  
+19            return false;
+20
+21        if(rank[rootX] > rank[rootY]){
+22            parent[rootY] = rootX;
+23        }
+24        else if(rank[rootX] < rank[rootY]){
+25            parent[rootX] = rootY;
+26        }
+27        else{
+28            parent[rootY] = rootX;
+29            rank[rootX]++;
+30        }
+31        components--;
+32        return true;
+33
+34    }
+35
+36    public int makeConnected(int n, int[][] connections) {
+37
+38        if(connections.length < n - 1)  
+39            return -1;
+40        
+41        parent = new int[n];
+42        rank = new int[n];
+43        components = n;
+44
+45        for(int i = 0 ; i < n ; i++){
+46            parent[i] = i;
+47            rank[i] = 1;
+48        }
+49
+50        int edgesNotNeeded = 0;
+51        for(int[] connection : connections){
+52            int x = connection[0], y = connection[1];
+53            union(x, y);
+54        }
+55        
+56
+57        return components - 1;
+58    }
+59}
