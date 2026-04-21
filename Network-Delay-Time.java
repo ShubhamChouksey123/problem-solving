90fb9b127ba24d6e85efbb3fa11bbdc8958ec41a
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-04-21 22:18:03 +0530

    [743] [Time Beats: 99.09%] [Memory Beats: 89.20%] - LeetPush

--- Network-Delay-Time.java
+++ Network-Delay-Time.java
@@ -1,66 +1,40 @@
 1class Solution {
-2    public int networkDelayTime(int[][] times, int n, int k) {
-3        
-4        k = k-1;
-5        List<List<List<Integer>>> adj = new ArrayList<>(n);
-6        for(int i = 0 ; i < n ; i++){
-7            adj.add(new ArrayList<>());
-8        }
-9
-10        for(int[] time : times){
-11            int u = time[0] - 1, v = time[1] - 1, w = time[2];
-12            adj.get(u).add(List.of(v, w));
-13        }
-14        
-15        boolean[] visited = new boolean[n];
-16        int[] dist = new int[n];
-17        Arrays.fill(dist, Integer.MAX_VALUE);
-18        dist[k] = 0;
-19
-20        /**
-21            queue containg (distance , nodeIndex)
-22         */
-23        Queue<int[]> queue = new PriorityQueue<>(
-24            (int[] a, int[] b) -> {
-25                if(a[0] == b[0]){
-26                    return Integer.compare(a[1], b[1]);
-27                }
-28                return Integer.compare(a[0], b[0]);
+2
+3    /**
+4        use bellman ford Algorithm to solve this shortest distance problem
+5     */
+6    public int networkDelayTime(int[][] times, int n, int k) {
+7
+8        int iterations = n - 1;
+9        int[] dist = new int[n];
+10        Arrays.fill(dist, Integer.MAX_VALUE);
+11        dist[k-1] = 0;
+12        
+13        while(iterations > 0){
+14            iterations--;
+15            boolean changed = false;
+16            for(int[] time : times){
+17                int u = time[0] - 1, v = time[1] - 1, w = time[2];
+18                if(dist[u] != Integer.MAX_VALUE){
+19                    int newDistance = dist[u] + w;
+20                    if(dist[v] > newDistance){
+21                        dist[v] = newDistance;
+22                        changed = true;
+23                    }  
+24                }
+25                
+26            }
+27            if(!changed){
+28                break;
 29            }
-30        );
+30        }
 31
-32        queue.add(new int[]{0 , k});
-33        Set<Integer> unvisitedNodes = new HashSet<>();
-34
-35        int curDistance = 0; int nodeIndex = k;
-36
-37        while(!queue.isEmpty()){
-38            int[] cur = queue.poll();
-39            nodeIndex = cur[1];
-40            curDistance = cur[0]; 
-41
-42            if(curDistance > dist[nodeIndex]) continue;
-43
-44            visited[nodeIndex] = true;
-45
-46
-47            for(List<Integer> neighbourInfo : adj.get(nodeIndex)){
-48                int neighbourIndex = neighbourInfo.get(0);
-49                int weight = neighbourInfo.get(1);
-50
-51                if(!visited[neighbourIndex] && dist[nodeIndex] + weight < dist[neighbourIndex]){
-52                    dist[neighbourIndex] = dist[nodeIndex] + weight;
-53                    queue.add(new int[]{dist[neighbourIndex], neighbourIndex});
-54                }
-55            } 
-56 
-57        }
-58
-59        int maxValue = 0;
-60        for(int d : dist){
-61            if(d == Integer.MAX_VALUE) return -1;
-62            maxValue = Math.max(maxValue, d);
-63        }
-64        return maxValue;
-65    }
-66}
+32        int maxValue = 0;
+33        for(int i = 0 ; i < n ; i++){
+34            if(i == k - 1) continue;
+35            maxValue = Math.max(maxValue, dist[i]);
+36        }
+37        return (maxValue == Integer.MAX_VALUE) ? -1 : maxValue;
+38        
+39    }
+40}
