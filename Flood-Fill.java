53c4ee5ec10319c37f77c016c6b5b1abc12719c5
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-03 21:22:35 +0530

    [733] [Time Beats: 37.07%] [Memory Beats: 24.79%] - LeetPush

--- Flood-Fill.java
+++ Flood-Fill.java
@@ -1,40 +1,33 @@
 1class Solution {
 2
-3    private static int n;
-4    private static int m;
-5    private static final int[][] DIRECTIONS = new int[][]{
-6        {1,0}, {0, 1}, {-1, 0}, {0,-1}
-7    };
-8
-9
-10    private void dfs(int[][] finalImage, boolean[][] visited, int x, int y, int originalColor, int newColor) {
-11        
-12        visited[x][y] = true;
-13        finalImage[x][y] = newColor;
-14
-15        for(int[] direction : DIRECTIONS){
-16            int x1 = x + direction[0];
-17            int y1 = y + direction[1];
-18            if(x1 >= 0 && x1 < n && y1 >= 0 && y1 < m && !visited[x1][y1] && finalImage[x1][y1] == originalColor){
-19                dfs(finalImage, visited, x1 , y1, originalColor, newColor);
-20            }
-21        }
-22    }
-23    
-24    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-25        n = image.length;
-26        m = image[0].length;
-27
-28        int[][] finalImage = new int[n][m];
-29        for(int i = 0 ; i < n ; i++){
-30            for(int j = 0 ; j < m ; j++){
-31                finalImage[i][j] = image[i][j];
-32            }
-33        }
-34
-35        boolean[][] visited = new boolean[n][m];
-36        dfs(finalImage, visited, sr, sc, image[sr][sc], color);  
-37
-38        return finalImage;
-39    }
-40}
+3    private static final int[][] DIRECTIONS = new int[][]{
+4        {1,0}, {0, 1}, {-1, 0}, {0,-1}
+5    };
+6
+7
+8    private void dfs(int[][] image, boolean[][] visited, int x, int y, int originalColor, int newColor) {
+9        
+10        visited[x][y] = true;
+11        image[x][y] = newColor;
+12
+13        for(int[] direction : DIRECTIONS){
+14            int x1 = x + direction[0];
+15            int y1 = y + direction[1];
+16            if(x1 >= 0 && x1 < image.length && y1 >= 0 && y1 < image[0].length && !visited[x1][y1] && image[x1][y1] == originalColor){
+17                dfs(image, visited, x1 , y1, originalColor, newColor);
+18            }
+19        }
+20    }
+21    
+22    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+23
+24
+25        int originalColor = image[sr][sc];
+26        if(originalColor != newColor){
+27            boolean[][] visited = new boolean[image.length][image[0].length];
+28            dfs(image, visited, sr, sc, originalColor, newColor); 
+29        }
+30
+31        return image;
+32    }
+33}
