9adf3e55786dc3a07189a8bc57ba9d069eb5ed40
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-06 23:46:07 +0530

    [399] [Time Beats: 5.35%] [Memory Beats: 31.26%] - LeetPush

--- Evaluate-Division.java
+++ Evaluate-Division.java
@@ -1,83 +1,76 @@
 1class Solution {
 2
-3    private double bfs(List<List<String>> equations, double[] values, String start, String end){
+3    private double bfs(Map<String, List<Pair<String, Double>>> adj, String start, String end){
 4
-5        
-6        Set<String> visited = new HashSet<>();
-7        visited.add(start);
-8
-9        Deque<String[]> queue = new ArrayDeque<>();
-10
-11        for(int i = 0 ; i < values.length ; i++){
-12            List<String> equation = equations.get(i);
-13            if(equation.get(0).equals(start) && !visited.contains(equation.get(1))){
-14                if(start.equals(end)){
-15                    return 1.0;
-16                }
-17                visited.add(equation.get(1));
-18                queue.offerLast(new String[]{equation.get(1), String.valueOf(values[i])});
-19            }
-20        }
-21        for(int i = 0 ; i < values.length ; i++){
-22            List<String> equation = equations.get(i);
-23            if(equation.get(1).equals(start) && !visited.contains(equation.get(0))){
-24                if(start.equals(end)){
-25                    return 1.0;
-26                }
-27                visited.add(equation.get(0));
-28                queue.offerLast(new String[]{equation.get(0), String.valueOf(1/ values[i])});
-29             
-30            }
-31        }
-32        
-33        String num = start;
-34        String den ;
-35        double factor = 1;
-36
-37
+5        if(!adj.containsKey(start) || !adj.containsKey(end)) return -1.0;
+6
+7        if(start.equals(end)) return 1.0;
+8        
+9        String num = start;
+10        double factor = 1.0;
+11
+12        Deque<Pair<String, Double>> queue = new ArrayDeque<>();
+13        Set<String> visited = new HashSet<>();
+14        
+15        visited.add(start);
+16        queue.offerLast(new Pair(start, factor));
+17
+18        while(!queue.isEmpty()){
+19            Pair<String, Double> node = queue.pollFirst();
+20            num = node.getKey();
+21            factor = Double.valueOf(node.getValue());
+22
+23            if(num.equals(end)){
+24                return factor;
+25            }
+26
+27            for(Pair<String, Double> pair : adj.get(num)){
+28                if(!visited.contains(pair.getKey())){
+29                    visited.add(pair.getKey());
+30                    queue.offerLast(new Pair(pair.getKey(), factor * pair.getValue()));
+31                }
+32
+33            }
+34        }
+35
+36        return -1.0;
+37    }
 38
-39        while(!queue.isEmpty()){
-40            String[] node = queue.pollFirst();
-41            den = node[0];
-42            factor = Double.valueOf(node[1]);
-43
-44            if(den.equals(end)){
-45                return factor;
-46            }
-47
-48            for(int i = 0 ; i < values.length ; i++){
-49                List<String> equation = equations.get(i);
-50                if(equation.get(0).equals(den) && !visited.contains(equation.get(1))  ){
-51                    visited.add(equation.get(1));
-52                    queue.offerLast(new String[]{equation.get(1), String.valueOf(factor * values[i])});
-53                }
-54            }
-55            for(int i = 0 ; i < values.length ; i++){
-56                List<String> equation = equations.get(i);
-57                if(equation.get(1).equals(den) && !visited.contains(equation.get(0))   ){
-58                    visited.add(equation.get(0));
-59                    queue.offerLast(new String[]{equation.get(0), String.valueOf(factor * 1/values[i])});
-60                }
-61            }
-62        }
-63
-64        return -1.0;
-65    }
-66
-67    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-68        int n = queries.size();
-69        double[] result = new double[n];
-70        
-71        for(int i = 0 ; i < queries.size() ; i++){
-72            List<String> query = queries.get(i);
-73            result[i] = bfs(equations, values, query.get(0), query.get(1));
-74
-75            // if(result[i] == -1.00){
-76            //     result[i] = bfs(equations, values, query.get(1), query.get(0), true);
-77            // }
-78        }
-79        
-80
-81        return result;
-82    }
-83}
+39    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+40        int n = queries.size();
+41        double[] result = new double[n];
+42
+43        Map<String, List<Pair<String, Double>>> adj = new HashMap<>();
+44        
+45        for(int i = 0 ; i < values.length ; i++){
+46            List<String> equation = equations.get(i);
+47            String u = equation.get(0);
+48            String v = equation.get(1); 
+49
+50            if(adj.containsKey(u)){
+51                adj.get(u).add(new Pair(v, values[i]));
+52            }
+53            else {
+54                List<Pair<String, Double>> listU = new ArrayList<>();
+55                listU.add(new Pair(v, values[i]));
+56                adj.put(u, listU);
+57            }
+58            if(adj.containsKey(v)){
+59                adj.get(v).add(new Pair(u, 1.0/values[i]));
+60            }
+61            else {
+62                List<Pair<String, Double>> listV = new ArrayList<>();
+63                listV.add(new Pair(u, 1.0/values[i]));
+64                adj.put(v, listV);
+65            }
+66        }
+67        
+68        for(int i = 0 ; i < queries.size() ; i++){
+69            List<String> query = queries.get(i);
+70            result[i] = bfs(adj, query.get(0), query.get(1));
+71        }
+72        
+73
+74        return result;
+75    }
+76}
