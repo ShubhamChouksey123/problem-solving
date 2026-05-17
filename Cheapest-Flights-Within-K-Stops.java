5a6f0246858aa5fbe27bed63a50e7bc8b45db3a1
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-17 12:58:43 +0530

    [787] [Time Beats: 85.77%] [Memory Beats: 80.96%] - LeetPush

--- Cheapest-Flights-Within-K-Stops.java
+++ Cheapest-Flights-Within-K-Stops.java
@@ -1,55 +1,58 @@
 1class Solution {
-2    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-3
-4
-5        // Adjacency list containing neigbour and its edge weight
-6        List<int[]>[] adj = new ArrayList[n];
-7        for(int i = 0 ; i < n ; i++){
-8            adj[i] = new ArrayList<>();
-9        }
+2
+3    /**
+4        Using the modified Dijkstra's Algorithm for finding shortest path in DAG
+5
+6    
+7     */
+8    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+9
 10
-11        for(int i = 0 ; i < flights.length ; i++){
-12            int u = flights[i][0], v = flights[i][1], w = flights[i][2];
-13            adj[u].add(new int[]{v, w});
-14            // adj[v].add(new int[]{u, w});
+11        // Adjacency list containing neigbour and its edge weight
+12        List<int[]>[] adj = new ArrayList[n];
+13        for(int i = 0 ; i < n ; i++){
+14            adj[i] = new ArrayList<>();
 15        }
 16
-17        int[] dist = new int[n];
-18        Arrays.fill(dist, Integer.MAX_VALUE);
-19        dist[src] = 0;
-20        
+17        for(int i = 0 ; i < flights.length ; i++){
+18            int u = flights[i][0], v = flights[i][1], w = flights[i][2];
+19            adj[u].add(new int[]{v, w});
+20        }
 21
-22        // Priority Queue containe {step, node, distance}
-23        Queue<int[]> pq = new PriorityQueue<>(
-24            (a, b)  -> Integer.compare(a[0], b[0])
-25        );
+22        int[] dist = new int[n];
+23        Arrays.fill(dist, Integer.MAX_VALUE);
+24        dist[src] = 0;
+25        
 26
-27        pq.offer(new int[]{0, src, 0});
-28
-29        while(!pq.isEmpty()){
-30            int[] curr = pq.poll();
-31            int node = curr[1];
-32            int step = curr[0], curDist = curr[2];
-33            // System.out.println("step : " + step + ", node : " + node + ", curDist : " + curDist);
-34            // System.out.println("dist : " + Arrays.toString(dist) );
+27        // Priority Queue containe {node, distance(price)}
+28        Deque<int[]> pq = new ArrayDeque<>();
+29        pq.offer(new int[]{src, 0});
+30        int stop = 0;
+31
+32        while(!pq.isEmpty()){
+33            
+34            if(stop > k) break;
 35
-36            if(node == dst) continue;
-37
+36            int size = pq.size();
+37            for(int i = 0 ; i < size ; i++){
 38
-39            for(int[] nodeEdgeWeight : adj[node]){
-40                int newWeight = curDist + nodeEdgeWeight[1];
-41                int newStep = step + 1;
-42                int neighbourNode = nodeEdgeWeight[0];
-43                
-44                if(newStep == k + 1 && neighbourNode != dst) continue;
-45
-46                if(newWeight < dist[neighbourNode]){
-47                    dist[neighbourNode] = newWeight;
-48                    pq.add(new int[]{newStep, neighbourNode, newWeight});
-49                }
-50                
-51            }
-52        }
-53        return dist[dst] != Integer.MAX_VALUE ? dist[dst] : -1;
-54    }
-55}
+39                int[] curr = pq.poll();
+40                int node = curr[0], curDist = curr[1];
+41
+42                for(int[] neighbourWeightArray : adj[node]){
+43                    int neighbourNode = neighbourWeightArray[0];
+44                    int neighbourWeight = neighbourWeightArray[1];
+45                    int newWeight = curDist + neighbourWeight;
+46
+47                    if(newWeight < dist[neighbourNode]){
+48                        dist[neighbourNode] = newWeight;
+49                        pq.offer(new int[]{neighbourNode, newWeight});
+50                    }
+51                }
+52            }
+53            stop++;
+54        }
+55        if(dist[dst] == Integer.MAX_VALUE) return -1;
+56        return dist[dst];
+57    }
+58}
