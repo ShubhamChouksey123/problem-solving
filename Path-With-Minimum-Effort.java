97539a1ebfd9b2f5c2b0027a93ce809f9a74db83
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-17 13:56:04 +0530

    [1631] [Time Beats: 21.20%] [Memory Beats: 57.89%] - LeetPush

---
+++ Path-With-Minimum-Effort.java
@@ -0,0 +1,46 @@
+1class Solution {
+2
+3    private static final int[][] DIRECTIONS = new int[][]{
+4        {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+5    };
+6
+7    public int minimumEffortPath(int[][] heights) {
+8
+9        int n = heights.length, m = heights[0].length;
+10
+11        int[][] minEfforts = new int[n][m];
+12        for(int i = 0 ; i < n ; i++){
+13            Arrays.fill(minEfforts[i], Integer.MAX_VALUE);
+14        }
+15
+16        minEfforts[0][0] = 0;
+17
+18        // Queue containing node and its effort
+19        // {x, y, effort}
+20        Queue<int[]> pq = new PriorityQueue<>(
+21            (a, b) -> Integer.compare(a[2], b[2])
+22        );
+23        pq.add(new int[]{0, 0, 0});
+24
+25        while(!pq.isEmpty()){
+26
+27            int[] curr = pq.poll();
+28            int nodeX = curr[0], nodeY = curr[1], effort = curr[2];
+29
+30            for(int[] direction : DIRECTIONS) {
+31                int x = nodeX + direction[0]; 
+32                int y = nodeY + direction[1];
+33
+34                if(x < 0 || x >= n || y < 0 || y >= m) continue;
+35
+36                int newEffort = Math.max(effort , Math.abs(heights[x][y] - heights[nodeX][nodeY] ));
+37
+38                if(newEffort < minEfforts[x][y]){
+39                    minEfforts[x][y] = newEffort; 
+40                    pq.add(new int[]{x, y, newEffort});
+41                }
+42            }
+43        }
+44        return minEfforts[n-1][m-1];
+45    }
+46}
