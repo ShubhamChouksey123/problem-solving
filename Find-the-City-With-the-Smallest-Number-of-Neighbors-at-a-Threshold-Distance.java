a65ef24ea065670768f103c1b818a5642e2e52e0
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-18 21:20:11 +0530

    [1334] [Time Beats: 99.44%] [Memory Beats: 74.27%] - LeetPush

--- Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.java
+++ Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.java
@@ -1,43 +1,39 @@
 1class Solution {
 2    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
 3
-4        int[][] distance = new int[n][n];
-5        for(int[] d: distance){
-6            Arrays.fill(d, Integer.MAX_VALUE);
-7        }
-8        for(int[] edge : edges){
-9            int u = edge[0], v = edge[1], w = edge[2];
-10            distance[u][v] = w;
-11            distance[v][u] = w;
-12        }
-13
-14        for(int k = 0 ; k < n ; k++){
-15            for(int i = 0 ; i < n ; i++){
-16                for(int j = 0 ; j < n ; j++){
-17                    if(distance[i][k] != Integer.MAX_VALUE && distance[k][j] != Integer.MAX_VALUE
-18                        && distance[i][k] + distance[k][j] < distance[i][j]
-19                    ){
-20                        distance[i][j] = distance[i][k] + distance[k][j]; 
-21                    }
-22                }
-23            }
-24        }
-25
-26        int smallestReachableCityCount = Integer.MAX_VALUE;
-27        int smallestReachableCityIndex = 0;
-28        for(int i = 0 ; i < n ; i++){
-29            int countReachableCities = 0;
-30            for(int j = 0 ; j < n ; j++){
-31                if(i != j && distance[i][j] <= distanceThreshold){
-32                    countReachableCities++;
-33                }
+4        int[][] dist = new int[n][n];
+5
+6        for(int i = 0 ; i < n ; i++){
+7            Arrays.fill(dist[i], Integer.MAX_VALUE / 2);
+8            dist[i][i] = 0;
+9        }
+10
+11        for(int[] edge : edges){
+12            int u = edge[0], v = edge[1], w = edge[2];
+13            dist[u][v] = w;
+14            dist[v][u] = w;
+15        }
+16
+17        for(int k = 0 ; k < n ; k++){
+18            for(int i = 0 ; i < n ; i++){
+19                for(int j = 0 ; j < n ; j++){
+20                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+21                }
+22            }
+23        }
+24
+25        int minCountCities = Integer.MAX_VALUE; int cityIndex = 0;
+26        for(int i = 0 ; i < n ; i++){
+27            int countCities = 0;
+28            for(int j = 0 ; j < n ; j++){
+29                if(i == j) continue;
+30                if(dist[i][j] <= distanceThreshold) countCities++;
+31            }
+32            if(countCities <= minCountCities){
+33                minCountCities = countCities; cityIndex = i;
 34            }
-35            if(countReachableCities <= smallestReachableCityCount){
-36                smallestReachableCityCount = countReachableCities;
-37                smallestReachableCityIndex = i;
-38            }
-39        }
-40        return smallestReachableCityIndex;
-41        
-42    }
-43}
+35        }
+36        return cityIndex;
+37   
+38    }
+39}
