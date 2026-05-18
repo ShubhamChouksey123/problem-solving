4f21ba111e78187aad7e41e916a3d75faefaf6ea
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-18 23:05:23 +0530

    [210] [Time Beats: 84.40%] [Memory Beats: 81.22%] - LeetPush

--- Course-Schedule-II.java
+++ Course-Schedule-II.java
@@ -1,48 +1,43 @@
 1class Solution {
-2
-3    /**
-4        BFS using Kahn's Algorithm
-5     */
-6    public int[] findOrder(int numCourses, int[][] prerequisites) {
-7        List<List<Integer>> adj = new ArrayList<>(numCourses);
-8        for(int i = 0 ; i < numCourses ; i++){
-9            adj.add(new ArrayList<>());
-10        }
-11
-12        int[] inwardEdgeCount = new int[numCourses];
-13        for(int[] prerequisite : prerequisites){
-14            int u = prerequisite[1];
-15            int v = prerequisite[0];
-16            adj.get(u).add(v);
-17            inwardEdgeCount[v]++;
-18        }
-19
-20        Deque<Integer> queue = new ArrayDeque<>();
-21        int[] ans = new int[numCourses];
-22        int indexAtAns = 0;
-23
-24        for(int i = 0 ; i < numCourses ; i++){
-25            if(inwardEdgeCount[i] == 0){
-26                queue.offerLast(i);
-27            }
-28        }
-29        
-30
-31        while(!queue.isEmpty()){
-32            int course = queue.pollFirst();
-33            ans[indexAtAns++] = course;
+2    public int[] findOrder(int numCourses, int[][] prerequisites) {
+3        
+4        List<Integer>[] adj = new ArrayList[numCourses];
+5        for(int i = 0 ; i < numCourses ; i++){
+6            adj[i] = new ArrayList<>();
+7        }
+8
+9        int[] inwardEdges = new int[numCourses];
+10        for(int[] prerequisite : prerequisites){
+11            int u = prerequisite[1], v = prerequisite[0];
+12            adj[u].add(v);
+13            inwardEdges[v]++;
+14        }
+15
+16        Deque<Integer> queue = new ArrayDeque<>();
+17        for(int i = 0 ; i < numCourses ; i++){
+18            if(inwardEdges[i] == 0){
+19                queue.offerLast(i);
+20            }
+21        }
+22
+23        List<Integer> ans = new ArrayList<>();
+24        while(!queue.isEmpty()){
+25            int node = queue.pollFirst();
+26            ans.add(node);
+27
+28            for(int neighbour : adj[node]){
+29                if(--inwardEdges[neighbour] == 0){
+30                    queue.offerLast(neighbour);
+31                }
+32            }
+33        }
 34
-35            for(int neighbour : adj.get(course)){
-36                if(--inwardEdgeCount[neighbour] == 0){
-37                    queue.offerLast(neighbour);
-38                }
-39            }
+35        if(ans.size() < numCourses) return new int[0];
+36
+37        int[] list = new int[numCourses];
+38        for(int i = 0; i < numCourses ; i++){
+39            list[i] = ans.get(i);
 40        }
-41
-42        if(indexAtAns != numCourses){
-43            return new int[0];
-44        }
-45
-46        return ans;
-47    }
-48}
+41        return list;
+42    }
+43}
