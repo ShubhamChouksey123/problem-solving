f772afc4b3873e264d7e3804a577111f535e1b83
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-18 22:13:10 +0530

    [1584] [Time Beats: 34.97%] [Memory Beats: 30.88%] - LeetPush

--- Min-Cost-to-Connect-All-Points.java
+++ Min-Cost-to-Connect-All-Points.java
@@ -1,55 +1,64 @@
 1class Solution {
 2
-3
-4    private void addAllNeigbours(Queue<int[]> priorityQueue, boolean[] visited, int index, int[][] points){
-5        
-6        int x1 = points[index][0], y1 = points[index][1];
-7        for(int j = 0 ; j < visited.length ; j++){
-8            if(!visited[j]){
-9                int x2 = points[j][0], y2 = points[j][1];
-10                int w = Math.abs(x1 - x2) + Math.abs(y1 - y2);
-11                priorityQueue.add(new int[]{index, j, w});
-12            }
-13        }
-14    }
-15
-16    /**
-17        Prim's Algorithm 
-18     */
-19    public int minCostConnectPoints(int[][] points) {
-20        
-21        int n = points.length;
-22
-23        Queue<int[]> priorityQueue = new PriorityQueue<>(
-24            (int[] a, int[] b) -> {
-25                return Integer.compare(a[2], b[2]);
-26            }
-27        );
-28
-29        boolean[] visited = new boolean[n];
-30        visited[0] = true;
-31        addAllNeigbours(priorityQueue, visited, 0, points);
-32
-33        int u = 0, v = 0, w = 0, cost = 0, edgeUsed = 0;
-34
-35        while(!priorityQueue.isEmpty()){
-36            int[] edge = priorityQueue.poll();
-37            u = edge[0]; v = edge[1]; w = edge[2];
-38            if(visited[u] && visited[v]) continue;
-39
-40            cost += w;
-41            if(!visited[u]){
-42                visited[u] = true;
-43                addAllNeigbours(priorityQueue, visited, u, points);
-44            }
-45            else{
-46                visited[v] = true;
-47                addAllNeigbours(priorityQueue, visited, v, points);
+3    private int[] parent;
+4    private int[] rank;
+5
+6    private int find(int x){
+7        if(parent[x] != x){
+8            parent[x] = find(parent[x]);
+9        }
+10        return parent[x];
+11    }
+12
+13    private boolean union(int x, int y){
+14        int rootX = find(x);
+15        int rootY = find(y);
+16
+17        if(rootX == rootY) return false;
+18
+19        if(rank[rootX] > rank[rootY]){
+20            parent[rootY] = rootX;
+21        }
+22        if(rank[rootX] < rank[rootY]){
+23            parent[rootX] = rootY;
+24        }
+25        else{
+26            parent[rootY] = rootX;
+27            rank[rootX]++;
+28        }
+29        return true;
+30    }
+31
+32    public int minCostConnectPoints(int[][] points) {
+33        int n = points.length;
+34        rank = new int[n];
+35        parent = new int[n];
+36        for(int i = 0 ; i < n ; i++){
+37            parent[i] = i;
+38            rank[i] = 1;
+39        }
+40
+41        List<int[]> edges = new ArrayList<>();
+42        int w = 0;
+43
+44        for(int i = 0 ; i < n ; i++){
+45            for(int j = i + 1 ; j < n ; j++){
+46                w = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+47                edges.add(new int[]{i, j, w});
 48            }
-49            edgeUsed++;
-50            if(edgeUsed == n-1) break;
-51        }
-52
-53        return cost;
-54    }
-55}
+49        }
+50
+51        Collections.sort(edges, 
+52            (a, b) -> Integer.compare(a[2], b[2]) 
+53        );
+54        
+55        int cost = 0;
+56        for(int[] edge: edges){
+57            int u = edge[0], v = edge[1]; w = edge[2];
+58            if(find(u) != find(v)){
+59                union(u, v); cost += w;
+60            }
+61        }
+62        return cost;
+63    }
+64}
