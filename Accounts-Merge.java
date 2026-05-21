a544a6a0e7832c854857eaf0c4473bdeb1c61720
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-21 16:46:48 +0530

    [721] [Time Beats: 54.30%] [Memory Beats: 85.32%] - LeetPush

--- Accounts-Merge.java
+++ Accounts-Merge.java
@@ -1,99 +1,93 @@
 1class Solution {
-2    /**
-3        Map containing email and its parent email 
-4     */
-5    Map<String, String> emailToParent;
-6    /**
-7        Map containing email and rank 
-8     */
-9    Map<String, Integer> emailToRank;
-10
-11    /**
-12    map containing email and owner name
-13     */
-14    Map<String, String> emailToName;
-15
+2
+3    private Map<String, Integer> emailToAccountId;
+4    private Map<String, String> emailToName;
+5
+6
+7    private int[] parent;
+8    private int[] rank;
+9
+10    private int find(int x){
+11        if(parent[x] != x){
+12            parent[x] = find(parent[x]);
+13        }
+14        return parent[x];
+15    }
 16
-17    private String find(String x){
-18        if(! emailToParent.get(x).equals(x) ){
-19            String root = find(emailToParent.get(x));
-20            emailToParent.put(x, root);
-21        }
-22        return emailToParent.get(x);
-23    }
-24
-25    private void union(String x, String y){
-26        String rootX = find(x);
-27        String rootY = find(y);
-28
-29        if( rootX.equals(rootY) )
-30            return;
-31        
-32        if(emailToRank.get(rootX) > emailToRank.get(rootY)){
-33            emailToParent.put(rootY, rootX);
-34        }
-35        else if(emailToRank.get(rootX) < emailToRank.get(rootY)){
-36            emailToParent.put(rootX, rootY);
-37        }
-38        else{
-39            emailToParent.put(rootY, rootX);
-40            emailToRank.put(rootX, emailToRank.getOrDefault(rootX, 0) + 1);
-41        }
-42    }
-43    
-44    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-45
-46        emailToParent = new HashMap<>();
-47        emailToRank = new HashMap<>();
-48        emailToName = new HashMap<>();
-49
-50        String name = null, rootEmail = null, email = null, parentEmail = null;
-51        for(List<String> account : accounts){
-52            name = account.get(0);
-53            for(int i = 1 ; i < account.size() ; i++){
-54                email = account.get(i);
-55                emailToParent.put(email, email);
-56                emailToRank.put(email, 1);
-57                emailToName.put(email, name);
-58            }
-59        }
-60
-61        
-62        for(List<String> account : accounts){
-63            name = account.get(0);
-64            rootEmail = account.get(1);
-65            for(int i = 2 ; i < account.size() ; i++){
-66                email = account.get(i);
-67                union(rootEmail, email);   
-68            }
-69        }
-70
-71        List<List<String>> ans = new ArrayList<>();
-72        /**
-73            Map containing rootEmail and its associated emails
-74         */
-75        Map<String, List<String>> rootToEmails = new HashMap<>();
+17    private boolean union(int x, int y){
+18        int rootX = find(x);
+19        int rootY = find(y);
+20
+21        if(rootX == rootY) return false;
+22
+23        if(rank[rootX] > rank[rootY]){
+24            parent[rootY] = rootX;
+25        }
+26        if(rank[rootX] < rank[rootY]){
+27            parent[rootX] = rootY;
+28        }
+29        else{
+30            parent[rootY] = rootX;
+31            rank[rootX]++;
+32        }
+33        return true;
+34    }
+35
+36    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+37
+38        int n = accounts.size();
+39        parent = new int[n];
+40        rank = new int[n];
+41        emailToAccountId = new HashMap<>();
+42        emailToName = new HashMap<>();
+43
+44        for(int i = 0 ; i < accounts.size() ; i++){
+45            parent[i] = i;
+46            rank[i] = 1;
+47        }
+48
+49        for(int i = 0 ; i < accounts.size(); i++){
+50            String name = accounts.get(i).get(0);
+51            
+52            for(int j = 1 ; j < accounts.get(i).size() ; j++){
+53                String email =  accounts.get(i).get(j);
+54                emailToName.put(email, name);
+55
+56                if(emailToAccountId.containsKey(email)){
+57                    int accountId = emailToAccountId.get(email);
+58                    union(accountId, i);
+59                }
+60                else{
+61                    emailToAccountId.put(email, i);
+62                }
+63            }
+64        }
+65
+66        Map<Integer, List<String>> accountsIdEmailMap = new HashMap<>();
+67
+68        for(Map.Entry<String, Integer> entry : emailToAccountId.entrySet()){
+69            String email = entry.getKey();
+70            int rootAccountId = find(entry.getValue());
+71
+72            accountsIdEmailMap.putIfAbsent(rootAccountId, new ArrayList<>());
+73            accountsIdEmailMap.get(rootAccountId).add(email);
+74        }
+75         
 76
-77        for(Map.Entry<String, String> entry : emailToParent.entrySet()){
-78            email = entry.getKey();
-79            rootEmail = find(email);
-80            if(rootToEmails.containsKey(rootEmail)){
-81                rootToEmails.get(rootEmail).add(email);
-82                continue;
-83            }
-84            List<String> associatedEmails = new ArrayList<>();
-85            associatedEmails.add(email);
-86            rootToEmails.put(rootEmail, associatedEmails);
+77        List<List<String>> ans = new ArrayList<>();
+78
+79        for(Map.Entry<Integer, List<String>> entry : accountsIdEmailMap.entrySet()){
+80            Collections.sort(entry.getValue());
+81
+82            String name = emailToName.get(entry.getValue().get(0));
+83            List<String> eachRow = new ArrayList<>();
+84            eachRow.add(name);
+85            eachRow.addAll(entry.getValue());
+86            ans.add(eachRow);
 87        }
 88
-89        for(Map.Entry<String, List<String>> entry : rootToEmails.entrySet()){
-90            Collections.sort(entry.getValue());
-91            List<String> account = new ArrayList<>();
-92            account.add(emailToName.get(entry.getKey()));
-93            account.addAll(entry.getValue());
-94            ans.add(account);
-95        }
-96        return ans;
-97        
-98    }
-99}
+89        return ans;
+90        
+91        
+92    }
+93}
