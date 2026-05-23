e2dd1228f2e8d9bad60754313a457d4c03dcb422
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-23 18:43:16 +0530

    [416] [Time Beats: 34.72%] [Memory Beats: 59.96%] - LeetPush

--- Partition-Equal-Subset-Sum.java
+++ Partition-Equal-Subset-Sum.java
@@ -1,41 +1,35 @@
 1class Solution {
-2    
-3    private Boolean[][] memo;
-4
-5    public boolean canPartitionUtil(int[] nums, int target, int index, int sum) {
-6
-7        if(index == nums.length){
-8            return (sum == target) ? true : false;
-9        } 
+2
+3    public boolean canPartition(int[] nums) {
+4        int n = nums.length;
+5
+6        int totalSum = 0;
+7        for(int num : nums){
+8            totalSum += num;
+9        }
 10
-11        if(sum > target){
-12            return false;
-13        }
-14
-15        if(memo[index][sum] != null){
-16            return memo[index][sum];
+11        if(totalSum % 2 != 0 ) return false;
+12        int target = totalSum / 2 ;
+13
+14        boolean[][] dp = new boolean[n+1][target + 1];
+15        for(int element = 0 ; element <= n ; element++){
+16            dp[element][0] = true;
 17        }
 18
-19        boolean exclude = canPartitionUtil(nums, target, index + 1, sum);  
-20        boolean include = canPartitionUtil(nums, target, index + 1, sum + nums[index]);
+19        for(int element = 1 ; element <= n ; element++){
+20            int index = element - 1;
 21
-22        boolean isPossible = include || exclude;
-23        memo[index][sum] = isPossible;
-24        return isPossible;
-25    }
-26
-27    public boolean canPartition(int[] nums) {
-28        int n = nums.length;
-29
-30        int totalSum = 0;
-31        for(int num : nums){
-32            totalSum += num;
-33        }
-34
-35        if(totalSum % 2 != 0 ) return false;
-36        int target = totalSum / 2 ;
-37
-38        memo = new Boolean[n][target + 1];
-39        return canPartitionUtil(nums, target, 0, 0);
-40    }
-41}
+22            for(int sum = 1; sum <= target ; sum++){
+23                dp[element][sum] = dp[element - 1][sum];
+24
+25                if(sum >= nums[index]){
+26                    dp[element][sum] = dp[element][sum] || dp[element - 1][sum - nums[index]];
+27                }
+28            }
+29        }
+30        
+31
+32
+33        return dp[n][target];
+34    }
+35}
