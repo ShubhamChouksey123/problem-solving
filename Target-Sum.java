5157eaeca1dfd0ac0004fa6c2c2c9ee1369141e8
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-23 14:26:23 +0530

    [494] [Time Beats: 43.24%] [Memory Beats: 13.17%] - LeetPush

--- Target-Sum.java
+++ Target-Sum.java
@@ -1,33 +1,35 @@
 1class Solution {
-2
-3    private Map<String, Integer> memo;
+2    
+3    private Map<Pair<Integer, Integer> , Integer> memo;
 4
-5    private int findTargetSumWaysUtil(int[] nums, int target, int index, int sum) {
-6        
+5    private int countWays(int[] nums, int target, int index, int sum) {
+6
 7        if(index == nums.length){
-8            if(sum == target){
-9                return 1;
-10            }
-11            return 0;
-12        }
-13
-14        String key = index + "," + sum; 
-15        if(memo.containsKey(key)){
-16            return memo.get(key);
-17        }
-18
-19        
-20        int minus = findTargetSumWaysUtil(nums, target, index + 1, sum - nums[index]);
-21        int add = findTargetSumWaysUtil(nums, target, index + 1, sum + nums[index]); 
-22        
-23        memo.put(key, minus + add);
-24        return minus + add;
-25    }
-26    
-27    public int findTargetSumWays(int[] nums, int target) {
-28        int n = nums.length, totalSum = 0;
-29
-30        memo = new HashMap<>();
-31        return findTargetSumWaysUtil(nums, target, 0, 0);
-32    }
-33}
+8            return (sum == target) ? 1 : 0;
+9        }
+10        Pair<Integer, Integer> pair = new Pair<>(index, sum);
+11        if(memo.containsKey(pair)){
+12            return memo.get(pair);
+13        }
+14
+15        int addWays = countWays(nums, target, index + 1, sum + nums[index]);
+16        int subWays = countWays(nums, target, index + 1, sum - nums[index]);
+17        
+18        int totalWays = addWays + subWays;
+19        memo.put(pair, totalWays);
+20        return totalWays;
+21    }
+22    
+23    public int findTargetSumWays(int[] nums, int target) {
+24
+25        int n = nums.length;
+26        int maxValue = 0;
+27        for(int num : nums){
+28            maxValue += num;
+29        }
+30
+31        memo = new HashMap<>();
+32        
+33        return countWays(nums, target, 0, 0);
+34    }
+35}
