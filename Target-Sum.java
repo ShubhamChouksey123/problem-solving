86adf5fd6d794b6f874e1bc24f757fdc5f9ab7ee
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-23 15:15:37 +0530

    [494] [Time Beats: 60.79%] [Memory Beats: 16.95%] - LeetPush

--- Target-Sum.java
+++ Target-Sum.java
@@ -3,37 +3,34 @@
 3    private int[][] memo;
 4    private int maxValue;
 5
-6    private int countWays(int[] nums, int target, int index, int sum) {
-7        if(index == nums.length){
-8            return (sum == target) ? 1 : 0;
-9        }
-10        
-11        if(memo[index][sum + maxValue] != -1){
-12            return memo[index][sum + maxValue];
-13        }
-14
-15        int addWays = countWays(nums, target, index + 1, sum + nums[index]);
-16        int subWays = countWays(nums, target, index + 1, sum - nums[index]);
-17        
-18        int totalWays = addWays + subWays;
-19        memo[index][sum + maxValue] = totalWays;
-20        return totalWays;
-21    }
-22    
-23    public int findTargetSumWays(int[] nums, int target) {
+6    
+7    public int findTargetSumWays(int[] nums, int target) {
+8
+9        int n = nums.length;
+10        maxValue = 0;
+11        
+12        for(int num : nums){
+13            maxValue += num;
+14        }
+15
+16        if(target < -maxValue || target > maxValue)
+17            return 0;
+18
+19        memo = new int[n+1][ 2 * maxValue + 1];
+20        for(int[] row : memo){
+21            Arrays.fill(row, 0);
+22        }
+23        memo[0][0 + maxValue] = 1; 
 24
-25        int n = nums.length;
-26        maxValue = 0;
-27        
-28        for(int num : nums){
-29            maxValue += num;
-30        }
-31
-32        memo = new int[n][ 2 * maxValue + 1];
-33        for(int[] row : memo){
-34            Arrays.fill(row, -1);
-35        }
-36        
-37        return countWays(nums, target, 0, 0);
-38    }
-39}
+25        for(int i = 1 ; i <= n ; i++){
+26            int num = nums[i-1];
+27            for(int j = 0 ; j <= 2 * maxValue ; j++){
+28                if(memo[i-1][j] > 0){
+29                    memo[i][j + num] += memo[i-1][j] ;
+30                    memo[i][j - num] += memo[i-1][j] ;
+31                }                
+32            }
+33        }
+34        return memo[n][target + maxValue];
+35    }
+36}
