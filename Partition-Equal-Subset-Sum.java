f8350449a7ba825a7eb4c4e2acd173a12cd4b5cd
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-23 19:01:27 +0530

    [416] [Time Beats: 64.08%] [Memory Beats: 92.22%] - LeetPush

--- Partition-Equal-Subset-Sum.java
+++ Partition-Equal-Subset-Sum.java
@@ -11,25 +11,20 @@
 11        if(totalSum % 2 != 0 ) return false;
 12        int target = totalSum / 2 ;
 13
-14        boolean[][] dp = new boolean[n+1][target + 1];
-15        for(int element = 0 ; element <= n ; element++){
-16            dp[element][0] = true;
-17        }
-18
-19        for(int element = 1 ; element <= n ; element++){
-20            int index = element - 1;
-21
-22            for(int sum = 1; sum <= target ; sum++){
-23                dp[element][sum] = dp[element - 1][sum];
-24
-25                if(sum >= nums[index]){
-26                    dp[element][sum] = dp[element][sum] || dp[element - 1][sum - nums[index]];
-27                }
-28            }
-29        }
-30        
-31
-32
-33        return dp[n][target];
-34    }
-35}
+14        boolean[] dp = new boolean[target + 1];
+15        dp[0] = true;
+16
+17        for(int element = 1 ; element <= n ; element++){
+18            int index = element - 1;
+19
+20            for(int sum = target; sum > 0 ; sum--){
+21                
+22                if(sum >= nums[index]){                    
+23                    dp[sum] = dp[sum] || dp[sum - nums[index]];
+24                }
+25            }
+26        }
+27        
+28        return dp[target];
+29    }
+30}
