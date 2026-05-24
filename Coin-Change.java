c06c9db3c90641bfe7cba150a5b5dc326b26fc18
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-24 23:37:35 +0530

    [322] [Time Beats: 25.83%] [Memory Beats: 16.27%] - LeetPush

--- Coin-Change.java
+++ Coin-Change.java
@@ -1,40 +1,34 @@
 1class Solution {
-2    
-3    private long[] memo;
-4
-5    private long minCoins(int[] coins, int amount){
+2    public int coinChange(int[] coins, int amount) {
+3
+4        int n = coins.length;
+5        int[][] dp = new int[n + 1][amount + 1];
 6
-7        if(amount == 0)
-8            return 0;
-9
-10        if(amount < 0)
-11            return 0;
-12
-13        if(memo[amount] != -1){
-14            return memo[amount];
-15        }
-16        
-17        long minCoin = Integer.MAX_VALUE;
-18        for(int coin : coins){
-19            if(coin <= amount){
-20                minCoin = Math.min(1 + minCoins(coins, amount - coin), minCoin);
-21            }
-22        }
-23        memo[amount] = minCoin;
-24        return minCoin;
-25
-26    }
-27    
-28    public int coinChange(int[] coins, int amount) {
+7        for(int[] row : dp){
+8            Arrays.fill(row, Integer.MAX_VALUE);
+9        }
+10
+11        for(int element = 0 ; element <= n ; element++){
+12            dp[element][0] = 0;
+13        }
+14
+15        for(int sum = 1 ; sum <= amount ; sum++){
+16            for(int element = 1 ; element <= n ; element++){
+17                int index = element - 1;
+18
+19                dp[element][sum] = dp[element - 1][sum];
+20
+21                if(sum >= coins[index] && dp[element][sum - coins[index]] != Integer.MAX_VALUE){
+22                    dp[element][sum] = Math.min(
+23                        dp[element][sum], 
+24                        1 + dp[element][sum - coins[index]]
+25                    );
+26                } 
+27            }
+28        }
 29
-30        memo = new long[amount + 1];
-31        Arrays.fill(memo, -1);
-32        memo[0] = 0;
-33
-34        long minCoin = minCoins(coins, amount);
-35
-36        if(minCoin == Integer.MAX_VALUE)
-37            return -1;
-38        return (int)minCoin; 
-39    }
-40}
+30        return (dp[n][amount] == Integer.MAX_VALUE) ? -1 : dp[n][amount];
+31        
+32        
+33    }
+34}
