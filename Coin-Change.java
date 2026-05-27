ea33d9b546f388247555a49db892bdfcbd993d53
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-27 21:57:06 +0530

    [322] [Time Beats: 32.59%] [Memory Beats: 16.09%] - LeetPush

--- Coin-Change.java
+++ Coin-Change.java
@@ -2,33 +2,27 @@
 2    public int coinChange(int[] coins, int amount) {
 3
 4        int n = coins.length;
-5        int[][] dp = new int[n + 1][amount + 1];
-6
-7        for(int[] row : dp){
-8            Arrays.fill(row, Integer.MAX_VALUE);
-9        }
-10
-11        for(int element = 0 ; element <= n ; element++){
-12            dp[element][0] = 0;
-13        }
-14
-15        for(int sum = 1 ; sum <= amount ; sum++){
-16            for(int element = 1 ; element <= n ; element++){
-17                int index = element - 1;
-18
-19                dp[element][sum] = dp[element - 1][sum];
-20
-21                if(sum >= coins[index] && dp[element][sum - coins[index]] != Integer.MAX_VALUE){
-22                    dp[element][sum] = Math.min(
-23                        dp[element][sum], 
-24                        1 + dp[element][sum - coins[index]]
-25                    );
-26                } 
-27            }
-28        }
-29
-30        return (dp[n][amount] == Integer.MAX_VALUE) ? -1 : dp[n][amount];
-31        
-32        
-33    }
-34}
+5        int[][] dp = new int[n+1][amount+1];
+6        for(int[] row : dp){
+7            Arrays.fill(row, Integer.MAX_VALUE);
+8        }
+9
+10        for(int i = 0 ; i <= n ; i++){
+11            dp[i][0] = 0;
+12        }
+13
+14        for(int element = 1 ; element <= n ; element++){
+15            int index = element - 1;
+16            for(int j = 1; j <= amount ; j++){
+17                dp[element][j] = dp[element - 1][j];
+18                if(j >= coins[index] && dp[element][j - coins[index]] != Integer.MAX_VALUE){
+19                    dp[element][j] = Math.min(
+20                        dp[element][j], 
+21                        1 + dp[element][j - coins[index]]
+22                    ) ; 
+23                }
+24            }
+25        } 
+26        return (dp[n][amount] == Integer.MAX_VALUE) ? -1 : dp[n][amount];
+27    }
+28}
