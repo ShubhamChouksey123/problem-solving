9ca62fed792160c8363e07c5b3a5be6aa35a27b4
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-27 22:52:42 +0530

    [279] [Time Beats: 72.18%] [Memory Beats: 30.30%] - LeetPush

--- Perfect-Squares.java
+++ Perfect-Squares.java
@@ -1,38 +1,24 @@
 1class Solution {
-2
-3    private int[] memo;
+2    
+3    public int numSquares(int n) {
 4
-5    private int numSquaresUtil(int n) {
-6        
-7        if(n <= 3){
-8            return n;
-9        }
-10
-11        if(memo[n] != -1){
-12            return memo[n];
-13        }
-14
-15
-16        int count = Integer.MAX_VALUE;
-17        for(int i = 2 ; i * i <= n ; i++){
-18            count = Math.min( count, 1 + numSquaresUtil(n - (i*i)) );
-19        }
-20        memo[n] = count;
-21        return count;
-22    }
-23    
-24    public int numSquares(int n) {
-25
-26        if(n <= 3) return n;
-27
-28        memo = new int[n+1];
-29        Arrays.fill(memo, -1);
-30
-31
-32        for(int i = 0 ; i <= 3 ; i++){
-33            memo[i] = i;
-34        }
-35        
-36        return numSquaresUtil(n);
-37    }
-38}
+5        if(n <= 3) return n;    
+6        int[] dp = new int[n+1];
+7        Arrays.fill(dp, Integer.MAX_VALUE);
+8        for(int i = 0 ; i <= 3 ; i++){
+9            dp[i] = i;
+10        }
+11
+12
+13
+14        for(int i = 2 ; i * i <= n ; i++){
+15            for(int j = 4 ; j <= n ; j++){
+16                if(j >= i * i){
+17                    dp[j] = Math.min(dp[j], 1 + dp[j - i*i]);
+18                }
+19            }
+20        }
+21        return dp[n];
+22            
+23    }
+24}
