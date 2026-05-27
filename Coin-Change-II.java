c9f42652c3f0c7db9211a1fc6d3d124faacaae85
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-27 22:35:07 +0530

    [518] [Time Beats: 69.16%] [Memory Beats: 97.30%] - LeetPush

--- Coin-Change-II.java
+++ Coin-Change-II.java
@@ -2,26 +2,20 @@
 2    public int change(int amount, int[] coins) {
 3
 4        int n = coins.length;
-5        int[][] dp = new int[n+1][amount+1];
-6        for(int[] row : dp){
-7            Arrays.fill(row, 0);
-8        }
-9
-10        for(int i = 0 ; i <= n ; i++){
-11            dp[i][0] = 1;
-12        }
-13
-14        for(int element = 1 ; element <= n ; element++ ){
-15            int index = element - 1;
-16            for(int j = 1 ; j <= amount ; j++){
-17                dp[element][j] = dp[element - 1][j];
-18                if(j >= coins[index] && dp[element][j - coins[index]] != 0){
-19                    
-20                    dp[element][j] += dp[element][j - coins[index]];
-21                } 
-22            }
-23        }
-24
-25        return dp[n][amount];
-26    }
-27}
+5        int[] dp = new int[amount+1];
+6        Arrays.fill(dp, 0);
+7        dp[0] = 1;
+8
+9        
+10        for(int element = 1 ; element <= n ; element++ ){
+11            int index = element - 1;
+12            for(int j = 1 ; j <= amount ; j++){
+13                if(j >= coins[index]){
+14                    dp[j] += dp[j - coins[index]];
+15                } 
+16            }
+17        }
+18
+19        return dp[amount];
+20    }
+21}
