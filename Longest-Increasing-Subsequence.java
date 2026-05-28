e799baf7ac75df7b573c790d5809cf0b9ce00106
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-28 22:09:46 +0530

    [300] [Time Beats: 62.54%] [Memory Beats: 30.93%] - LeetPush

--- Longest-Increasing-Subsequence.java
+++ Longest-Increasing-Subsequence.java
@@ -4,18 +4,17 @@
 4        int[] dp = new int[n];
 5        Arrays.fill(dp, 1);
 6
-7        for(int i = 1 ; i < n ; i++){
-8            for(int j = 0 ; j < i ; j++){
-9                if(nums[j] < nums[i]){
-10                    dp[i] = Math.max(dp[i], 1 + dp[j]);
-11                }
-12            }
-13        }
-14
-15        int maxValue = 1;
-16        for(int val : dp){
-17            maxValue = Math.max(maxValue, val);
-18        }
-19        return maxValue;
-20    }
-21}
+7        int maxValue = 1;
+8        for(int i = 1 ; i < n ; i++){
+9            for(int j = 0 ; j < i ; j++){
+10                if(nums[j] < nums[i]){
+11                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+12                }
+13            }
+14            maxValue = Math.max(maxValue, dp[i]);
+15        }
+16
+17        
+18        return maxValue;
+19    }
+20}
