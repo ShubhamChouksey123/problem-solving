ba755b887ad3f5948b5c24261236d75811c22d5d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-28 22:35:50 +0530

    [368] [Time Beats: 61.08%] [Memory Beats: 15.71%] - LeetPush

--- Largest-Divisible-Subset.java
+++ Largest-Divisible-Subset.java
@@ -1,61 +1,36 @@
 1class Solution {
-2
-3    private int largestDivisibleSubsetUntil(int[] nums, int[] memo, int[] prev, int index){
-4
-5        if(index == 0)
-6            return 1;
-7
-8        if(memo[index] != -1)
-9            return memo[index];
-10
-11        int maxSize = 1; 
-12        for(int i = 0 ; i < index ; i++){
-13            int premaxSize = largestDivisibleSubsetUntil(nums, memo, prev, i);
-14
-15            if(nums[index] % nums[i] == 0){
-16                if(1 + premaxSize > maxSize){
-17                    maxSize = 1 + premaxSize;
-18                    prev[index] = i;
-19                }
-20                
-21            }
-22        }
-23
-24        memo[index] = maxSize;
-25        return maxSize;
-26    }
-27
-28    public List<Integer> largestDivisibleSubset(int[] nums) {
-29
-30        int n = nums.length;
-31        Arrays.sort(nums);
-32
-33        int[] memo = new int[n];
-34        int[] prev = new int[n];
-35        Arrays.fill(memo, -1);
-36        Arrays.fill(prev, -1);
-37        memo[0] = 1;
-38        prev[0] = -1;
-39        
-40        largestDivisibleSubsetUntil(nums, memo, prev, n - 1);
-41
-42        int maxSize = 1;
-43        int maxSizeLastElement = 0;
-44        for(int index = 0; index < n ; index++){
-45            if(memo[index] > maxSize){
-46                maxSize = memo[index];
-47                maxSizeLastElement = index; 
-48            }
-49        }
-50
-51        List<Integer> ans = new ArrayList<>();
-52        while(maxSizeLastElement != -1){
-53            ans.add(nums[maxSizeLastElement]);
-54            maxSizeLastElement = prev[maxSizeLastElement];
-55        }
-56
-57        return ans;
-58    }
-59}
-60
-61
+2    public List<Integer> largestDivisibleSubset(int[] nums) {
+3
+4        int n = nums.length;
+5        Arrays.sort(nums);
+6
+7        int[] dp = new int[n];
+8        int[] parent = new int[n];
+9        Arrays.fill(dp, 1);
+10        Arrays.fill(parent, -1);
+11
+12        int maxSubsetSize = 1, maxSubsetIndex = 0;
+13        for(int i = 1 ; i < n ; i++){
+14            for(int j = 0 ; j < i ; j++){
+15                if(nums[j] < nums[i] && nums[i] % nums[j] == 0 && 1 + dp[j] > dp[i] ){
+16                    dp[i] = 1 + dp[j];
+17                    parent[i] = j;
+18                }
+19            }
+20            if(dp[i] > maxSubsetSize){
+21                maxSubsetSize = dp[i];
+22                maxSubsetIndex = i;
+23            }
+24        }
+25
+26        List<Integer> ans = new ArrayList<>();
+27        int index = maxSubsetIndex;
+28
+29        while(index != -1){
+30            ans.add(nums[index]);
+31            index = parent[index];
+32        }
+33        return ans;
+34        
+35    }
+36}
