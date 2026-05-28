587957410247a26f8e4913ae4d94ac8300ad78b9
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-29 00:02:52 +0530

    [1048] [Time Beats: 87.63%] [Memory Beats: 31.25%] - LeetPush

--- Longest-String-Chain.java
+++ Longest-String-Chain.java
@@ -1,75 +1,33 @@
 1class Solution {
-2
-3    private boolean isSmaller(String a, String b){
-4
-5        if(b.length() - a.length() != 1){
-6            return false;
-7        }
+2    public int longestStrChain(String[] words) {
+3
+4        int n = words.length;
+5        Arrays.sort(words, 
+6            (a, b) -> Integer.compare(a.length(), b.length())
+7        );
 8
-9        int countDifferences = 0; 
-10        int aIndex = 0, bIndex = 0;
-11        
-12        while(aIndex < a.length()){
-13            if(a.charAt(aIndex) == b.charAt(bIndex)){
-14                aIndex++; bIndex++;   
-15            }
-16            else {
-17                countDifferences++;
-18                bIndex++;
-19            }
-20            if(countDifferences > 1)
-21                return false;
-22        }
-23        return countDifferences <= 1;   
-24    }
+9        Map<String, Integer> lscAtIndex = new HashMap<>();
+10
+11        for(int index = 0 ; index < n ; index ++){
+12
+13            String word = words[index];
+14            int length = word.length();
+15            int curentMax = 1;
+16            for(int i = 0 ; i < length ; i++){
+17
+18                String s = word.substring(0, i) + word.substring(i+1) ;
+19                if(lscAtIndex.containsKey(s)){
+20                    curentMax = Math.max(curentMax, lscAtIndex.get(s) + 1); 
+21                }
+22            }
+23            lscAtIndex.put(word, curentMax);
+24        }
 25
-26    public int longestStrChainUtil(String[] words, int[] memo, int index) {
-27
-28        if(index == 0)
-29            return 1;
+26        int maxLengLcs = 1;
+27        for(int len : lscAtIndex.values()){
+28            maxLengLcs = Math.max(maxLengLcs, len);
+29        }
 30        
-31        if(memo[index] != -1){
-32            return memo[index];
-33        }
-34        
-35        int currentMaxLength = 1;
-36        for(int i = 0 ; i < index ; i++){
-37            int previousMaxLength = longestStrChainUtil(words, memo, i);
-38
-39            if(isSmaller(words[i], words[index])){
-40                if(1 + previousMaxLength > currentMaxLength){
-41                    currentMaxLength = 1 + previousMaxLength ;
-42                }
-43            } 
-44        }
-45
-46        memo[index] = currentMaxLength;
-47        return currentMaxLength;
-48    }
-49
-50    public int longestStrChain(String[] words) {
-51
-52        int n = words.length;
-53        Arrays.sort(words, 
-54            (a, b) -> {
-55                if(a.length() == b.length()){
-56                    return a.compareTo(b);
-57                }
-58                return Integer.compare(a.length(), b.length());
-59            }
-60        );
-61
-62        int[] memo = new int[n];
-63        Arrays.fill(memo, -1);
-64        memo[0] = 1;
-65        
-66        longestStrChainUtil(words, memo, n - 1);
-67
-68
-69        int maxValue = 1;
-70        for(int length : memo){
-71            maxValue = Math.max(maxValue, length);
-72        }
-73        return maxValue;
-74    }
-75}
+31        return maxLengLcs;
+32    }
+33}
