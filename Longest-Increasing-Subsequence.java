ecda6cd206d7fe66b98372801fe364c979e3a6fb
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-28 22:08:29 +0530

    [300] [Time Beats: 55.29%] [Memory Beats: 81.53%] - LeetPush

--- Longest-Increasing-Subsequence.java
+++ Longest-Increasing-Subsequence.java
@@ -1,38 +1,21 @@
 1class Solution {
-2
-3    private int lengthOfLISUtil(int[] nums, int[] lcs, int index){
-4
-5        if(index == 0)
-6            return 1;
-7
-8        if(lcs[index] != -1)
-9            return lcs[index];
-10
-11        int maxLen = 1;
-12        for(int i = 0 ; i < index ; i++){
-13            if(nums[i] < nums[index]){
-14                maxLen =  Math.max(maxLen, 1 + lengthOfLISUtil(nums, lcs, i)) ;
-15            }
-16        }
-17        lcs[index] = maxLen;
-18        return maxLen;
-19    }
-20
-21    public int lengthOfLIS(int[] nums) {
-22        
-23        int n = nums.length;
-24        int[] lcs = new int[n];
-25        Arrays.fill(lcs, -1);
-26        lcs[0] = 1;
-27
-28        for(int i = 1 ; i < n ; i++){
-29            lengthOfLISUtil(nums, lcs, i);
-30        }   
-31        
-32        int maxLcs = 1;
-33        for(int l : lcs){
-34            maxLcs = Math.max(l, maxLcs);
-35        }
-36        return maxLcs;
-37    }
-38}
+2    public int lengthOfLIS(int[] nums) {
+3        int n = nums.length;
+4        int[] dp = new int[n];
+5        Arrays.fill(dp, 1);
+6
+7        for(int i = 1 ; i < n ; i++){
+8            for(int j = 0 ; j < i ; j++){
+9                if(nums[j] < nums[i]){
+10                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+11                }
+12            }
+13        }
+14
+15        int maxValue = 1;
+16        for(int val : dp){
+17            maxValue = Math.max(maxValue, val);
+18        }
+19        return maxValue;
+20    }
+21}
