a120f1c7b2294d99eeb7dbccae8bf97882bc20eb
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-05-29 00:26:48 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 64.19%] - LeetPush

--- Unique-Paths.java
+++ Unique-Paths.java
@@ -1,28 +1,26 @@
 1class Solution {
-2    /**
-3    
-4        n rows and m columns 
-5     */
-6    public int uniquePaths(int n, int m) {
+2
+3    public int uniquePathsUtil(int[][] dp, int x, int y) {
+4
+5        if(x == 0 || y == 0)
+6            return 1;
 7
-8        int[][] dp = new int[n][m];
-9        
-10        // first column
-11        for(int i = 0 ; i < n ; i++){
-12            dp[i][0] = 1;
-13        }
-14
-15        // first row
-16        for(int j = 0 ; j < m ; j++){
-17            dp[0][j] = 1;
-18        }
-19
-20        for(int i = 1; i < n ; i++){
-21            for(int j = 1; j < m ; j++){
-22                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-23            }
-24        }
-25
-26        return dp[n-1][m-1];
-27    }
-28}
+8        if(dp[x][y] != -1){
+9            return dp[x][y];
+10        }
+11        
+12        dp[x][y] = uniquePathsUtil(dp, x - 1, y) + uniquePathsUtil(dp, x, y - 1);   
+13        return dp[x][y];
+14    }
+15
+16    public int uniquePaths(int n, int m) {
+17
+18        int[][] dp = new int[n][m];
+19        for(int[] row : dp){
+20            Arrays.fill(row, -1);
+21        }
+22        
+23        return uniquePathsUtil(dp, n - 1, m - 1);
+24        
+25    }
+26}
