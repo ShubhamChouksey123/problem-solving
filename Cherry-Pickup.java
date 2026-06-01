c20080fe8a9a13236e193043e5a56d3b358e9ae4
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-01 23:00:41 +0530

    [741] [Time Beats: 41.59%] [Memory Beats: 88.40%] - LeetPush

--- Cherry-Pickup.java
+++ Cherry-Pickup.java
@@ -8,10 +8,10 @@
 8    private int cherryPickup(int[][] grid, int n, int m, int[][][] dp, int row1, int col1, int col2) {
 9
 10        int row2 = row1 + col1 - col2;
-11        if(row1 == n - 1 && row2 == n - 1 && col1 == m - 1 && col2 == m - 1){
-12            if(grid[n-1][m-1] == 1)
-13                return 1;
-14            return 0;
+11
+12        // Both reached the bottom right corner
+13        if(row1 == n - 1 && col1 == m - 1){
+14            return grid[n-1][m-1];
 15        }
 16
 17        if(dp[row1][col1][col2] != -2)
@@ -26,7 +26,7 @@
 26            thisLevelSum += grid[row2][col2];
 27        }
 28
-29        int maxInnerSum = -1;
+29        int maxInnerSum = Integer.MIN_VALUE;
 30        for(int i = 0 ; i < 2 ; i++){
 31            int newRow1 = row1 + RIGHT_DOWN[i][0];
 32            int newCol1 = col1 + RIGHT_DOWN[i][1];
@@ -46,26 +46,27 @@
 46            }
 47        }
 48
-49        if(maxInnerSum == -1){
-50            dp[row1][col1][col2] = -1;
-51            return -1;
-52        }
-53        dp[row1][col1][col2] = thisLevelSum + maxInnerSum;
-54        return thisLevelSum + maxInnerSum;
-55    }
-56
-57    public int cherryPickup(int[][] grid) {
-58
-59        int n = grid.length, m = grid[0].length;
-60        int[][][] dp = new int[n][m][m];
-61        for(int[][] in : dp){
-62            for(int[] row : in){
-63                Arrays.fill(row, -2);
-64            }
-65        }
-66
-67        int maxSum = cherryPickup(grid, n, m, dp, 0 , 0, 0);
-68
-69        return (maxSum == -1) ? 0 : maxSum;
-70    }
-71}
+49        // Bloked Path
+50        if(maxInnerSum == Integer.MIN_VALUE){
+51            dp[row1][col1][col2] = Integer.MIN_VALUE;
+52            return Integer.MIN_VALUE;
+53        }
+54        dp[row1][col1][col2] = thisLevelSum + maxInnerSum;
+55        return dp[row1][col1][col2];
+56    }
+57
+58    public int cherryPickup(int[][] grid) {
+59
+60        int n = grid.length, m = grid[0].length;
+61        int[][][] dp = new int[n][m][m];
+62        for(int[][] in : dp){
+63            for(int[] row : in){
+64                Arrays.fill(row, -2);
+65            }
+66        }
+67
+68        int maxSum = cherryPickup(grid, n, m, dp, 0 , 0, 0);
+69
+70        return (maxSum == Integer.MIN_VALUE) ? 0 : maxSum;
+71    }
+72}
