8e8bfa00dab8e4abae9a693aae458f4c85571cbf
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-02 13:12:43 +0530

    [1143] [Time Beats: 16.94%] [Memory Beats: 67.03%] - LeetPush

--- Longest-Common-Subsequence.java
+++ Longest-Common-Subsequence.java
@@ -1,22 +1,43 @@
 1class Solution {
-2    public int longestCommonSubsequence(String text1, String text2) {
-3
-4        int n = text1.length(), m = text2.length();
-5        int[][] dp = new int[n+1][m+1];
-6        for(int[] row : dp){
-7            Arrays.fill(row, 0);
-8        }
-9
-10        for(int i = 1 ; i <= n ; i++){
-11            for(int j = 1 ; j <= m ; j++){
-12                if(text1.charAt(i-1) == text2.charAt(j-1)){
-13                    dp[i][j] = 1 + dp[i-1][j-1];
-14                }
-15                else{
-16                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-17                }
-18            }
-19        }
-20        return dp[n][m];
-21    }
-22}
+2    
+3    private int longestCommonSubsequence(String text1, String text2, int n, int m, int[][] dp, int x, int y) {
+4        
+5        if(x < 0 || y < 0)
+6            return 0;
+7        
+8        if(dp[x][y] != -1)
+9            return dp[x][y];
+10
+11        if(text1.charAt(x) == text2.charAt(y)){
+12            return 1 + longestCommonSubsequence(text1, text2, n, m, dp, x - 1, y - 1);
+13        }
+14
+15        int a = longestCommonSubsequence(text1, text2, n, m, dp, x - 1, y); 
+16        int b = longestCommonSubsequence(text1, text2, n, m, dp, x, y - 1);
+17
+18        int optimalValue = Math.max(a, b);
+19        dp[x][y] = optimalValue;
+20        return optimalValue; 
+21
+22    }
+23    
+24    public int longestCommonSubsequence(String text1, String text2) {
+25
+26        int n = text1.length(), m = text2.length();
+27        int[][] dp = new int[n][m];
+28        for(int[] row : dp){
+29            Arrays.fill(row, -1);
+30        }
+31        
+32        return longestCommonSubsequence(text1, text2, n, m, dp, n - 1, m - 1);  
+33    }
+34}
+35
+36/**
+37
+38"abcdeefgqznxkddxxws"
+39"acebcesnsdwandjadna"
+40
+41"abcdeefgqznxkddxxwsandsdnbagfsvafdtxvsa"
+42"acebcesnsdwandjadnasvafscvxbagsvdya"
+43 */
