d4c572aa01a00cf36d639b73eb029e97ac8d5e78
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-02 21:01:07 +0530

    [516] [Time Beats: 49.51%] [Memory Beats: 20.45%] - LeetPush

--- Longest-Palindromic-Subsequence.java
+++ Longest-Palindromic-Subsequence.java
@@ -9,24 +9,26 @@
 9            return dp[x][y];
 10
 11        if(s.charAt(x) == s.charAt(y)){
-12            return 1 + longestPalindromeSubseq(s, n, dp, x + 1, y - 1); 
-13        }
-14
-15        int a = longestPalindromeSubseq(s, n, dp, x + 1, y);
-16        int b = longestPalindromeSubseq(s, n, dp, x, y - 1);
-17
-18        int optimalValue = Math.max(a, b);
-19        dp[x][y] = optimalValue;
-20        return optimalValue;
-21    }
-22    
-23    public int longestPalindromeSubseq(String s) {
-24
-25        int n = s.length();   
-26        int[][] dp = new int[n][n];
-27        for(int[] row : dp){
-28            Arrays.fill(row, -1);
-29        }
-30        return longestPalindromeSubseq(s, n, dp, 0, n - 1); 
-31    }
-32}
+12            int ans = 1 + longestPalindromeSubseq(s, n, dp, x + 1, y - 1); 
+13            dp[x][y] = ans;
+14            return ans;
+15        }
+16
+17        int a = longestPalindromeSubseq(s, n, dp, x + 1, y);
+18        int b = longestPalindromeSubseq(s, n, dp, x, y - 1);
+19
+20        int optimalValue = Math.max(a, b);
+21        dp[x][y] = optimalValue;
+22        return optimalValue;
+23    }
+24    
+25    public int longestPalindromeSubseq(String s) {
+26
+27        int n = s.length();   
+28        int[][] dp = new int[n][n];
+29        for(int[] row : dp){
+30            Arrays.fill(row, -1);
+31        }
+32        return longestPalindromeSubseq(s, n, dp, 0, n - 1); 
+33    }
+34}
