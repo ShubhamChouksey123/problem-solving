256cde7b4fdb20ce919eef03856a096973f3f06d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-03 22:48:51 +0530

    [72] [Time Beats: 69.52%] [Memory Beats: 16.84%] - LeetPush

--- Edit-Distance.java
+++ Edit-Distance.java
@@ -1,31 +1,38 @@
 1class Solution {
-2    public int minDistance(String word1, String word2) {
-3
-4        int n = word1.length(), m = word2.length();
-5
-6        int[][] dp = new int[n+1][m+1];
-7
-8        dp[0][0] = 0;
-9        // first columnn
-10        for(int i = 1 ; i <= n ; i++){
-11            dp[i][0] = i;
-12        }
-13
-14        // first row
-15        for(int j = 1 ; j <= m ; j++){
-16            dp[0][j] = j;
+2
+3    private int minDistance(String word1, String word2, int n , int m, int[][] dp, int x, int y){
+4
+5        if(x == 0 && y > 0)
+6            return y;
+7        if(x > 0 && y == 0)
+8            return x;
+9        if(x == 0 && y == 0)
+10            return 0;
+11
+12        if(dp[x-1][y-1] != -1)
+13            return dp[x-1][y-1];
+14        
+15        if(word1.charAt(x - 1) == word2.charAt(y - 1)){
+16            return minDistance(word1, word2, n , m, dp, x - 1, y - 1);
 17        }
 18
-19        for(int i = 1 ; i <= n ; i++){
-20            for(int j = 1 ; j <= m ; j++){
-21                if(word1.charAt(i-1) == word2.charAt(j-1)){
-22                    dp[i][j] = dp[i-1][j-1];
-23                }
-24                else{
-25                    dp[i][j] = 1 + Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
-26                }
-27            }
-28        }
-29        return dp[n][m];
-30    }
-31}
+19        int a = minDistance(word1, word2, n , m, dp, x - 1, y);
+20        int b = minDistance(word1, word2, n , m, dp, x, y - 1);
+21        int c = minDistance(word1, word2, n , m, dp, x - 1, y - 1);
+22
+23        int optimalValue = Math.min(a, Math.min(b, c)) + 1;
+24        dp[x-1][y-1] = optimalValue;
+25        return optimalValue;
+26    }
+27
+28    public int minDistance(String word1, String word2) {
+29        
+30        int n = word1.length(), m = word2.length();
+31        int[][] dp = new int[n][m];
+32        for(int[] row : dp){
+33            Arrays.fill(row, -1);
+34        }
+35        
+36        return  minDistance(word1, word2, n , m, dp, n, m);
+37    }
+38}
