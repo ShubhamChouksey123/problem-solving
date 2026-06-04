2eb9907b9f41a377e2e3247d6977f78dd9a3f954
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-04 22:10:44 +0530

    [47] [Time Beats: 28.44%] [Memory Beats: 21.84%] - LeetPush

--- Permutations-II.java
+++ Permutations-II.java
@@ -1,44 +1,37 @@
 1class Solution {
-2
-3    private void permuteUnique(int[] nums, Set<List<Integer>> ans, boolean[] added, List<Integer> per){
-4
-5        if(per.size() == nums.length){
-6            ans.add(new ArrayList<>(per)); return;
-7        }
-8
-9        
-10
-11        for(int i = 0 ; i < nums.length ; i++){
-12
-13            if(added[i] == true){
-14                continue;
-15            }
-16
-17            if(i > 0 && nums[i] == nums[i-1] && added[i-1]){
-18                continue;
-19            }
+2    
+3    private void permuteUnique(int[] nums, List<List<Integer>> ans, boolean[] added, List<Integer> cur) {
+4        
+5        if(cur.size() == nums.length){
+6            ans.add(new ArrayList<>(cur));
+7            return;
+8        }
+9
+10        for(int i = 0 ; i < nums.length ; i++){
+11
+12            if(added[i] || (i > 0 && nums[i] == nums[i-1] && added[i-1])){
+13                continue;
+14            }
+15
+16            added[i] = true;
+17            cur.add(nums[i]);
+18
+19            permuteUnique(nums, ans, added, cur);
 20
-21            per.add(nums[i]);
-22            added[i] = true;
-23
-24            permuteUnique(nums, ans, added, per);
-25
-26            per.remove(per.size() - 1);
-27            added[i] = false;
-28        }
-29
-30    }
-31
-32    public List<List<Integer>> permuteUnique(int[] nums) {
-33        
-34        int n = nums.length;
-35        Arrays.sort(nums);
-36        Set<List<Integer>> ans = new HashSet<>();
-37        boolean[] added  = new boolean[n];
-38        List<Integer> per = new ArrayList<>();
-39
-40        permuteUnique(nums, ans, added, per);
-41
-42        return new ArrayList<>(ans);
-43    }
-44}
+21            cur.remove(cur.size() - 1);
+22            added[i] = false;
+23        }
+24            
+25    }
+26    
+27    public List<List<Integer>> permuteUnique(int[] nums) {
+28
+29        int n = nums.length;
+30        Arrays.sort(nums);
+31        List<List<Integer>> ans = new ArrayList<>();
+32        boolean[] added = new boolean[n];
+33        permuteUnique(nums, ans, added, new ArrayList<>()); 
+34
+35        return ans;
+36    }
+37}
