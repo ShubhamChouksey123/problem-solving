71021ccea931dbde40e392f6613c05a60f7e19e6
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-05 09:35:49 +0530

    [39] [Time Beats: 13.57%] [Memory Beats: 7.35%] - LeetPush

--- Combination-Sum.java
+++ Combination-Sum.java
@@ -1,32 +1,31 @@
 1class Solution {
-2    public void combinationSum(int[] candidates, int target, int sum, int start, List<Integer> cur, List<List<Integer>> result) {
-3        if(sum > target){
-4            return;
-5        }
-6
-7        if(sum == target){
-8            result.add(new ArrayList<>(cur)); return;
-9        }
-10
-11        for(int i = start ; i < candidates.length ; i++){
-12            int candidate = candidates[i];
-13
-14            cur.add(candidate);
-15            combinationSum(candidates, target, sum + candidate, i, cur, result);
+2    
+3    private void combinationSum(int[] candidates, int target, List<List<Integer>> ans, List<Integer> cur, int sum, int index) {
+4        
+5        if(sum == target){
+6            ans.add(new ArrayList<>(cur));
+7            return;
+8        }
+9        else if(sum > target){
+10            return;
+11        }
+12
+13        for(int i = index; i < candidates.length; i++){
+14            sum += candidates[i];
+15            cur.add(candidates[i]);
 16
-17            cur.remove(cur.size() - 1);
-18        }
-19        
-20
-21    }
-22    
-23    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-24
-25        Arrays.sort(candidates);
-26
-27        List<List<Integer>> result = new ArrayList<>();
-28        combinationSum(candidates, target, 0, 0, new ArrayList<>(), result);
-29        
-30        return result;
-31    }
-32}
+17            combinationSum(candidates, target, ans, cur, sum, i);
+18
+19            sum -= candidates[i];
+20            cur.remove(cur.size() - 1);
+21        }
+22    }
+23
+24    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+25
+26        Arrays.sort(candidates);
+27        List<List<Integer>> ans = new ArrayList<>();
+28        combinationSum(candidates, target, ans, new ArrayList<>(), 0, 0);
+29        return ans;
+30    }
+31}
