2b62b7bba7212eff72c96c5d01d9dd53eec103d2
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-05 18:44:53 +0530

    [131] [Time Beats: 72.82%] [Memory Beats: 7.23%] - LeetPush

--- Palindrome-Partitioning.java
+++ Palindrome-Partitioning.java
@@ -1,37 +1,38 @@
 1class Solution {
 2
-3    private boolean isPalindrom(String s, int start, int end){
-4        System.out.println("start : " + start + ", end : " + end);
-5        while(start < end){
-6            if(s.charAt(start) != s.charAt(end)){
-7                return false;
-8            }
-9            start++; end--;
-10        }
-11        return true;
-12    }
-13    private void partitionUtils(String s, int start, List<String> cur, List<List<String>> result) {
-14
-15        if(start == s.length()){
-16            result.add(new ArrayList<>(cur));
-17            return;
-18        }
-19
-20        for(int end = start ; end < s.length() ; end++){
-21            if(isPalindrom(s, start, end)){
-22                String substring = s.substring(start, end + 1);
-23                cur.add(substring);
-24                partitionUtils(s, end + 1, cur, result);
-25                cur.remove(cur.size() - 1);    
-26            }   
+3    private boolean isPalindrome(String s, int start, int end){
+4        while(start < end){
+5            if(s.charAt(start) != s.charAt(end)) return false;
+6            start++;
+7            end--;
+8        }
+9        return true;
+10    }
+11
+12    private void addPalindome(List<List<String>> ans, String s, int n, int start, List<String> cur){
+13
+14        if(start == n){
+15            ans.add(new ArrayList<>(cur));
+16            return;
+17        }
+18
+19        for(int end = start ; end < n ; end++){
+20            if(!isPalindrome(s, start, end)) continue;
+21
+22            cur.add(new String(s.substring(start, end + 1)));
+23
+24            addPalindome(ans, s, n, end + 1, cur);
+25
+26            cur.remove(cur.size() - 1);
 27        }
 28    }
-29    
+29
 30    public List<List<String>> partition(String s) {
-31        int n = s.length();
-32        List<List<String>> result = new ArrayList<>();
-33        partitionUtils(s, 0, new ArrayList<>(), result);
+31
+32        int n = s.length();
+33        List<List<String>> ans = new ArrayList<>();
 34
-35        return result;     
-36    }
-37}
+35        addPalindome(ans, s, n, 0, new ArrayList<>());
+36        return ans;
+37    }
+38}
