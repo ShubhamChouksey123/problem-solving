1bf52a4ee27326d3dadfc7586a54d4cc87f3b001
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-05 09:46:01 +0530

    [22] [Time Beats: 71.51%] [Memory Beats: 55.93%] - LeetPush

--- Generate-Parentheses.java
+++ Generate-Parentheses.java
@@ -1,26 +1,24 @@
 1class Solution {
-2    public void generateParenthesisUtil(int n, int openCount, int closeCount, StringBuilder builder, List<String> result) {
-3        if(openCount + closeCount == 2*n){
-4            String s = builder.toString();
-5            result.add(s); 
-6            return;
-7        }
-8
-9        if(openCount < n){
-10            builder.append('(');
-11            generateParenthesisUtil(n, openCount + 1, closeCount, builder, result);
-12            builder.deleteCharAt(builder.length() - 1);
-13        }
-14        if(openCount > closeCount){
-15            builder.append(')');
-16            generateParenthesisUtil(n, openCount, closeCount + 1, builder, result);
-17            builder.deleteCharAt(builder.length() - 1);
-18        }
-19    }
-20    
-21    public List<String> generateParenthesis(int n) {
-22        List<String> result = new ArrayList<>();
-23        generateParenthesisUtil(n, 0, 0, new StringBuilder(), result);
-24        return result;
-25    }
-26}
+2    
+3    private void generateParenthesis(int n, List<String> ans, String curr, int openCount, int closedCount) {
+4        
+5        if(openCount == n && closedCount == n){
+6            ans.add(new String(curr));
+7            return;
+8        }
+9
+10        if(openCount < n){
+11            generateParenthesis(n, ans, curr + '(', openCount + 1, closedCount);
+12        }
+13        if(closedCount < n && openCount > closedCount){
+14            generateParenthesis(n, ans, curr + ')', openCount, closedCount + 1);
+15        }
+16    }
+17    
+18    public List<String> generateParenthesis(int n) {
+19        
+20        List<String> ans = new ArrayList<>(); 
+21        generateParenthesis(n, ans, "", 0, 0) ;
+22        return ans;
+23    }
+24}
