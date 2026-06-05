2a8961060158c08251776cee3d5e1140f06eb128
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-05 18:19:27 +0530

    [51] [Time Beats: 16.83%] [Memory Beats: 27.82%] - LeetPush

--- N-Queens.java
+++ N-Queens.java
@@ -1,75 +1,72 @@
 1class Solution {
-2
-3    private boolean isSafeToAdd(int n, boolean[][] board, int x, int y){
-4        if(board[x][y]){
-5            return false;
-6        }
-7        int i = 0, j = 0;
-8        for(i = 0 ; i < n ; i++){
-9            if(board[i][y]) return false;    
-10        }
-11        for(j = 0 ; j < n ; j++){
-12            if(board[x][j]) return false;
-13        }
-14        
-15        // anti diagonal 
-16        int sum = x + y; j = 0;
-17        for(i = sum ; i >= 0 ; i--){
-18            j = sum - i;
-19            if( i < n && j >= 0 && j < n){
-20                if(board[i][j]) return false;
-21            }
-22            
-23        }
-24
-25        // main diagonal 
-26        i = 0 ; int diff = x - y;
-27        for(i = 0 ; i < n ; i++){
-28            j = i - diff;
-29            if(j >= 0 && j < n){
-30                if(board[i][j]) return false;
-31            }
-32        }
-33        return true;
-34    }
-35
-36    private List<String> convertBoardToString(int n, boolean[][] board){
-37        List<String> result = new ArrayList<>(); 
-38        for(int i = 0 ; i < n ; i++){
-39            StringBuilder stringBuilder  = new StringBuilder();
-40            for(int j = 0 ; j < n ; j++){
-41                stringBuilder.append(board[i][j] ? 'Q' : '.') ;
-42                
-43            }
-44            result.add(stringBuilder.toString());
-45        }
-46        return result;
-47    }
-48
+2    
+3    private void addBoardToResult(int n, boolean[][] board, List<List<String>> ans){
+4
+5        List<String> result = new ArrayList<>();
+6        for(int i = 0 ; i < n ; i++){
+7            StringBuilder builder = new StringBuilder();
+8            for(int j = 0 ; j < n ; j++){
+9                if(board[i][j]) {
+10                    builder.append('Q');
+11                }else{
+12                    builder.append('.');
+13                }
+14            }
+15            result.add(builder.toString());
+16        }
+17        ans.add(result);
+18    }
+19
+20    private boolean isValid(int n, boolean[][] board, int row, int column){
+21        
+22
+23        // same column check
+24        for(int i = 0 ; i < n ; i++){
+25            if(i == row) continue;
+26            if(board[i][column]) return false;
+27        }
+28
+29        // same row check
+30        for(int j = 0 ; j < n ; j++){
+31            if(j == column) continue;
+32            if(board[row][j]) return false;
+33        }
+34
+35        for(int i = 0 ; i < n ; i++){
+36            for(int j = 0 ; j < n ; j++){
+37                if(i == row && j == column) continue;
+38
+39                if(i + j == row + column && board[i][j]) return false;
+40
+41                if(i - j == row - column && board[i][j]) return false;
+42            }
+43        }
+44
+45        return true;
+46    }
+47
+48    private void solveNQueens(int n, boolean[][] board, List<List<String>> ans, int row) {
 49
-50    public void solveNQueensutil(List<List<String>> ans, int n, boolean[][] board, int row) {
-51        
-52        if(row == n){
-53            List<String> result = convertBoardToString(n, board);
-54            ans.add(result);
-55            return;
-56        }
-57
-58        for(int j = 0 ; j < n ; j++){
-59            if(isSafeToAdd(n, board, row, j)){
-60                board[row][j] = true;
-61                solveNQueensutil(ans, n, board, row + 1);
-62                board[row][j] = false;
-63            }
-64        }
-65        
-66        
-67    }
-68
-69    public List<List<String>> solveNQueens(int n) {
-70        List<List<String>> ans = new ArrayList<>();
-71        solveNQueensutil(ans, n, new boolean[n][n], 0); 
-72
-73        return ans; 
-74    }
-75}
+50        if(row == n){
+51            addBoardToResult(n, board, ans); return;
+52        }
+53
+54        for(int column = 0 ; column < n ; column++){
+55            if(!isValid(n, board, row, column)) continue;
+56                
+57            
+58            board[row][column] = true;
+59                solveNQueens(n, board, ans, row + 1);
+60
+61                board[row][column] = false;
+62        }
+63    }
+64    
+65    public List<List<String>> solveNQueens(int n) {
+66        boolean[][] board = new boolean[n][n];
+67        List<List<String>> ans = new ArrayList<>();
+68        solveNQueens(n, board, ans, 0); 
+69        
+70        return ans;
+71    }
+72}
