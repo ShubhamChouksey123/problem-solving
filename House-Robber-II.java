e1209295829459cab55d95a6a7c0802ef0389d9b
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-08 23:44:19 +0530

    [213] [Time Beats: 100.00%] [Memory Beats: 76.24%] - LeetPush

--- House-Robber-II.java
+++ House-Robber-II.java
@@ -1,30 +1,22 @@
 1class Solution {
 2    public int rob(int[] nums) {
 3
-4        int n = nums.length; 
+4        int n = nums.length;
 5        if(n == 1) return nums[0];
-6
-7        int[] dp = new int[n];
-8        dp[0] = nums[0];
-9        dp[1] = Math.max(nums[0], nums[1]);
-10
-11        if(n == 2) return dp[1];
-12        
-13        // We have included the first element
-14        for(int i = 2 ; i < nums.length - 1 ; i++){
-15            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
-16        }
-17        int maxValueWithFirst = dp[n-2];    
-18
-19        dp[0] = 0;
-20        dp[1] = nums[1];
-21
-22        // We have not included the first element
-23        for(int i = 2 ; i < nums.length ; i++){
-24            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
-25        }
-26        int maxValueWithoutFirst = dp[n-1];
-27
-28        return Math.max(maxValueWithFirst, maxValueWithoutFirst);
-29    }
-30}
+6        int[] dp = new int[n];
+7        
+8
+9        dp[0] = nums[0]; dp[1] = Math.max(nums[0], nums[1]);
+10        for(int i = 2 ; i < n - 1 ; i++){
+11            dp[i] = Math.max( nums[i] + dp[i-2] , dp[i-1] );
+12        }
+13        int firstCasemaxValue = dp[n-2];
+14
+15        dp[0] = 0; dp[1] = nums[1];
+16        for(int i = 2 ; i < n ; i++){
+17            dp[i] = Math.max( nums[i] + dp[i-2] , dp[i-1] );
+18        }
+19        int secondCasemaxValue = dp[n-1];
+20        return Math.max(firstCasemaxValue, secondCasemaxValue);
+21    }
+22}
