e8187a449cc9cc913383921b120d64e51b7994ce
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-10 23:01:52 +0530

    [416] [Time Beats: 22.65%] [Memory Beats: 30.92%] - LeetPush

--- Partition-Equal-Subset-Sum.java
+++ Partition-Equal-Subset-Sum.java
@@ -1,30 +1,39 @@
 1class Solution {
-2
-3    public boolean canPartition(int[] nums) {
-4        int n = nums.length;
-5
-6        int totalSum = 0;
-7        for(int num : nums){
-8            totalSum += num;
+2    
+3    public boolean canPartition(int[] nums, int target, Boolean[][] dp, int index, int sum) {
+4        
+5        if(sum == target){
+6            return true;
+7        }else if(sum > target){
+8            return false;
 9        }
 10
-11        if(totalSum % 2 != 0 ) return false;
-12        int target = totalSum / 2 ;
-13
-14        boolean[] dp = new boolean[target + 1];
-15        dp[0] = true;
-16
-17        for(int element = 1 ; element <= n ; element++){
-18            int index = element - 1;
-19
-20            for(int sum = target; sum > 0 ; sum--){
-21                
-22                if(sum >= nums[index]){                    
-23                    dp[sum] = dp[sum] || dp[sum - nums[index]];
-24                }
-25            }
-26        }
+11        if(index == nums.length){
+12            return sum == target ? true : false;
+13        }
+14
+15        if(dp[index][sum] != null)
+16            return dp[index][sum];
+17
+18        boolean a = canPartition(nums, target, dp, index + 1, sum); 
+19        boolean b = canPartition(nums, target, dp, index + 1, sum + nums[index]); 
+20
+21        Boolean result = a | b;
+22        dp[index][sum] = result;
+23        return result;
+24    }
+25    
+26    public boolean canPartition(int[] nums) {
 27        
-28        return dp[target];
-29    }
-30}
+28        int n = nums.length; int totalSum = 0;;
+29        for(int num : nums){
+30            totalSum += num;
+31        }
+32        if(totalSum % 2 != 0) return false;
+33
+34        Boolean[][] dp = new Boolean[n][totalSum/2];
+35        
+36        return canPartition(nums, totalSum/2, dp, 0, 0);
+37
+38    }
+39}
