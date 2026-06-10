04741cbb9d33e197be60644d7f586f2f784c0ffa
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-10 22:34:39 +0530

    [494] [Time Beats: 41.04%] [Memory Beats: 28.20%] - LeetPush

--- Target-Sum.java
+++ Target-Sum.java
@@ -1,36 +1,29 @@
 1class Solution {
-2    
-3    private int[][] memo;
-4    private int maxValue;
+2
+3    Map<Pair<Integer, Integer>, Integer> mp;
+4    public int findTargetSumWays(int[] nums, int target, int index, int sum) {
 5
-6    
-7    public int findTargetSumWays(int[] nums, int target) {
-8
-9        int n = nums.length;
-10        maxValue = 0;
-11        
-12        for(int num : nums){
-13            maxValue += num;
-14        }
+6        if(index >= nums.length){
+7            if(sum == target) 
+8                return 1;
+9            return 0;
+10        }
+11
+12        Pair<Integer, Integer> key = new Pair<>(index, sum);
+13         if(mp.containsKey(key)) 
+14            return mp.get(key);
 15
-16        if(target < -maxValue || target > maxValue)
-17            return 0;
-18
-19        memo = new int[n+1][ 2 * maxValue + 1];
-20        for(int[] row : memo){
-21            Arrays.fill(row, 0);
-22        }
-23        memo[0][0 + maxValue] = 1; 
-24
-25        for(int i = 1 ; i <= n ; i++){
-26            int num = nums[i-1];
-27            for(int j = 0 ; j <= 2 * maxValue ; j++){
-28                if(memo[i-1][j] > 0){
-29                    memo[i][j + num] += memo[i-1][j] ;
-30                    memo[i][j - num] += memo[i-1][j] ;
-31                }                
-32            }
-33        }
-34        return memo[n][target + maxValue];
-35    }
-36}
+16
+17        int a = findTargetSumWays(nums, target, index + 1, sum - nums[index]);
+18        int b = findTargetSumWays(nums, target, index + 1, sum + nums[index]);
+19
+20        mp.put(key, a + b);
+21        return a + b;
+22    }
+23
+24    public int findTargetSumWays(int[] nums, int target) {
+25        int n = nums.length;
+26        mp = new HashMap<>();
+27        return findTargetSumWays(nums, target, 0, 0); 
+28    }
+29}  
