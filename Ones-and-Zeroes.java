99b2e0c6d752927295d2e79f9fa6e9bfd1b8252e
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-11 21:13:07 +0530

    [474] [Time Beats: 18.35%] [Memory Beats: 8.62%] - LeetPush

--- Ones-and-Zeroes.java
+++ Ones-and-Zeroes.java
@@ -1,48 +1,47 @@
 1class Solution {
-2    
-3    private int[][][] memo;
-4
-5    private int countZeros(String s){
-6        int count = 0;
-7        for(char c : s.toCharArray()){
-8            if(c == '0') count++;
-9        }
-10        return count;
-11    }
-12
-13    private int findMaxFormUtil(String[] strs, int index, int m, int n) {
-14        
-15        if(m < 0 || n < 0) return Integer.MIN_VALUE;
-16
-17        if(index == strs.length){
-18            return 0; 
-19        }
+2
+3    private int countZeros(String str){
+4        int count = 0;
+5        for(char c : str.toCharArray()){
+6            if(c == '0') count++;
+7        }
+8        return count;
+9    }
+10    
+11    private int findMaxForm(String[] strs, int[][][] dp, int m, int n, int index) {
+12        
+13        if(index == strs.length){
+14            return 0;
+15        }
+16        if(m == 0 && n == 0) return 0;
+17
+18        if(dp[m][n][index] != -1)
+19            return dp[m][n][index];
 20
-21        if(memo[index][m][n] != -1){
-22            return memo[index][m][n];
-23        }
-24
-25        String s = strs[index];
-26        int zeroCount = countZeros(s);
-27        int oneCount = s.length() - zeroCount;
-28
-29        int a = 1 + findMaxFormUtil(strs, index + 1, m - zeroCount, n - oneCount);
-30        int b = findMaxFormUtil(strs, index + 1, m, n);
-31        
-32        int max = Math.max(a, b);
-33        memo[index][m][n] = max;
-34        
-35        return max;
-36    }
-37    
-38    public int findMaxForm(String[] strs, int m, int n) {
-39        int len = strs.length;
-40        memo = new int[len + 1][m+1][n+1];
-41        for(int i = 0 ; i <= len ; i++){
-42            for(int j = 0 ; j <= m ; j++){
-43                Arrays.fill(memo[i][j], -1);
-44            }
-45        }
-46        return findMaxFormUtil(strs, 0, m, n);
-47    }
-48}
+21        int countZeros = countZeros(strs[index]);
+22        int countOnes = strs[index].length() - countZeros;
+23
+24        int a = 0;
+25        if(countZeros <= m && countOnes <= n){
+26            a = 1 + findMaxForm(strs, dp, m - countZeros , n - countOnes, index + 1);
+27        }
+28        int b = findMaxForm(strs, dp, m , n, index + 1);
+29
+30        int optimalValue = Math.max(a, b);
+31        dp[m][n][index] = optimalValue;
+32        return optimalValue;
+33    }
+34    
+35    public int findMaxForm(String[] strs, int m, int n) {
+36        
+37        int totalStrings = strs.length;
+38        int[][][] dp = new int[m+1][n+1][totalStrings];
+39        for(int[][] matrix : dp){
+40            for(int[] row : matrix){
+41                Arrays.fill(row, -1);
+42            }
+43        }
+44
+45        return findMaxForm(strs, dp, m, n, 0);
+46    }
+47}
