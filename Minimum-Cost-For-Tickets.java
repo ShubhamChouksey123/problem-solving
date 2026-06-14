9e16f6d0429972ecc20d41c537abb59f1dafaa69
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-14 21:42:02 +0530

    [983] [Time Beats: 75.54%] [Memory Beats: 58.18%] - LeetPush

--- Minimum-Cost-For-Tickets.java
+++ Minimum-Cost-For-Tickets.java
@@ -3,32 +3,24 @@
 3    public int mincostTickets(int[] days, int[] costs) {
 4        
 5        int n = days.length;
-6        int[] dp = new int[366];
+6        int[] dp = new int[366 + 30];
 7
 8        int indexOnDay = n - 1;
-9        for(int i = 365 ; i >= 1 ; i--){
-10            if(indexOnDay >= 0 && days[indexOnDay] == i){
-11                // we might need to purchase ticket
-12                int a = costs[0];
-13                if(i + 1 <= 365){
-14                    a += dp[i+1];
-15                }
-16                int b = costs[1];
-17                if(i + 7 <= 365){
-18                    b += dp[i+7];
-19                }
-20                int c = costs[2];
-21                if(i + 30 <= 365){
-22                    c += dp[i+30];
-23                }
-24                dp[i] = Math.min(a, Math.min(b, c));
-25                indexOnDay--;
-26            }
-27            else{
-28                if(i+1 <= 365)
-29                    dp[i] = dp[i+1];
-30            }
-31        }
-32        return dp[1];
-33    }
-34}
+9        int a, b, c;
+10        for(int i = days[indexOnDay] ; i >= 1 ; i--){
+11            if(indexOnDay >= 0 && days[indexOnDay] == i){
+12                // we might need to purchase ticket
+13                a = costs[0] + dp[i+1];
+14                b = costs[1] + dp[i+7];
+15                c = costs[2] + dp[i+30];
+16
+17                dp[i] = Math.min(a, Math.min(b, c));
+18                indexOnDay--;
+19            }
+20            else{
+21                dp[i] = dp[i+1];
+22            }
+23        }
+24        return dp[1];
+25    }
+26}
