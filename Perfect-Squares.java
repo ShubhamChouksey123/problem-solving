825882cf94aa28b6f2dfdd09b67e0d89be455e19
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-14 20:45:54 +0530

    [279] [Time Beats: 34.06%] [Memory Beats: 50.87%] - LeetPush

--- Perfect-Squares.java
+++ Perfect-Squares.java
@@ -1,21 +1,23 @@
 1class Solution {
 2    
-3    public int numSquares(int n) {
+3    private int numSquares(int[] dp, int n) {
 4
-5        if(n <= 3) return n;    
-6        int[] dp = new int[n+1];
-7        Arrays.fill(dp, Integer.MAX_VALUE);
-8        for(int i = 0 ; i <= 3 ; i++){
-9            dp[i] = i;
-10        }
-11
+5        if(n <= 3) return n;
+6        if(dp[n] != -1) return dp[n];
+7
+8        int minNumbers = n;
+9        for(int i = 2 ; i * i <= n ; i++){
+10            minNumbers = Math.min(1 + numSquares(dp, n - (i*i)), minNumbers);
+11        }
 12
-13        for(int target = 4 ; target <= n ; target++){
-14            for(int i = 2 ; i*i <= target ; i++){
-15                dp[target] = Math.min(dp[target], 1 + dp[target - i*i]);
-16            }
-17        }
-18        return dp[n];
-19            
-20    }
-21}
+13        dp[n] = minNumbers;
+14        return minNumbers;
+15    }
+16    
+17    public int numSquares(int n) {
+18
+19        int[] dp = new int[n+1];
+20        Arrays.fill(dp, -1);
+21        return numSquares(dp, n);
+22    }
+23}
