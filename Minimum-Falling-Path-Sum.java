c245a9cb766289b71144ab54e1869db077c1a3d3
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-15 22:41:04 +0530

    [931] [Time Beats: 5.70%] [Memory Beats: 30.72%] - LeetPush

--- Minimum-Falling-Path-Sum.java
+++ Minimum-Falling-Path-Sum.java
@@ -2,37 +2,32 @@
 2    public int minFallingPathSum(int[][] matrix) {
 3
 4        int n = matrix.length;
-5        int[] prev = new int[n];
-6        int[] curr = new int[n];
-7
-8        for(int column = 0; column < n ; column++){
-9            prev[column] = matrix[0][column];
+5        List<Integer> prev = new ArrayList<>();
+6        List<Integer> curr = new ArrayList<>();
+7        
+8        for(int i = 0 ; i < n ; i++){
+9            prev.add(matrix[0][i]);
 10        }
 11
 12        for(int row = 1 ; row < n ; row++){
-13            curr = new int[n];
-14            for(int column = 0; column < n ; column++){
-15                
-16                curr[column] = matrix[row][column];
-17                int minValue = prev[column];
-18                if(column-1 >= 0){
-19                    minValue = Math.min(minValue, prev[column - 1]); 
-20                }
-21                if(column+1 < n){
-22                    minValue = Math.min(minValue, prev[column + 1]); 
-23                }
-24                curr[column] += minValue;
-25            
-26            }
-27            prev = curr;
-28            
+13            curr = new ArrayList<>();
+14            for(int column = 0 ; column < n ; column++){
+15                int minAboveSum = prev.get(column);
+16                if(column - 1 >= 0){
+17                    minAboveSum = Math.min(minAboveSum, prev.get(column - 1));
+18                }
+19                if(column + 1 < n){
+20                    minAboveSum = Math.min(minAboveSum, prev.get(column + 1));
+21                }
+22                curr.add(minAboveSum + matrix[row][column]);
+23            } 
+24            prev = curr;
+25        }
+26        int minPathSum = Integer.MAX_VALUE;
+27        for(int column = 0 ; column < n ; column++){
+28            minPathSum = Math.min(minPathSum, prev.get(column));
 29        }
 30
-31        int minValue = prev[0];
-32        for(int row = 1 ; row < n ; row++){
-33            minValue = Math.min(minValue , prev[row]);
-34        }
-35
-36        return minValue;
-37    }
-38}
+31        return minPathSum;
+32    }   
+33}
