ee9a28056b93aa9c5abdea2740f04a01f3f5ec6a
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-15 21:53:27 +0530

    [63] [Time Beats: 17.17%] [Memory Beats: 44.93%] - LeetPush

--- Unique-Paths-II.java
+++ Unique-Paths-II.java
@@ -2,28 +2,24 @@
 2    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 3
 4        int n = obstacleGrid.length, m = obstacleGrid[0].length;
-5
-6        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1)
-7            return 0;
-8
-9        int[][] dp = new int[n][m];
-10        for(int[] row : dp){
-11            Arrays.fill(row, 0);
-12        }
-13        dp[0][0] = 1;
-14
-15        for(int i = 0 ; i < n ; i++){
-16            for(int j = 0 ; j < m ; j++){
-17                if(obstacleGrid[i][j] == 1)
-18                    continue;
-19                if(i-1 >= 0){
-20                    dp[i][j] = dp[i-1][j]; 
-21                }
-22                if(j-1 >= 0){
-23                    dp[i][j] += dp[i][j-1]; 
-24                }
-25            }
-26        }
-27        return dp[n-1][m-1];
-28    }
-29}
+5        if(obstacleGrid[0][0] == 1) return 0;
+6
+7        int[][] countWays = new int[n][m];
+8        countWays[0][0] = 1;
+9
+10        for(int i = 0 ; i < n ; i++){
+11            for(int j = 0 ; j < m ; j++){
+12                if(obstacleGrid[i][j] == 1) continue;
+13                if(i - 1 >= 0){
+14                    countWays[i][j] += countWays[i-1][j];
+15                }
+16                if(j - 1 >= 0){
+17                    countWays[i][j] += countWays[i][j-1];
+18                }
+19            }
+20        }
+21
+22
+23        return countWays[n-1][m-1];
+24    }
+25}
