28cae0006bb5f2ad7e3221d43e34e2e57645f4a6
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-15 23:03:22 +0530

    [221] [Time Beats: 20.83%] [Memory Beats: 57.05%] - LeetPush

--- Maximal-Square.java
+++ Maximal-Square.java
@@ -1,38 +1,30 @@
 1class Solution {
 2    public int maximalSquare(char[][] matrix) {
-3
+3        
 4        int n = matrix.length, m = matrix[0].length;
 5        int[][] dp = new int[n][m];
-6        
-7        // The max edge of the square
-8        int maxValue = 0;
-9        for(int i = 0 ; i < n ; i++){
-10            for(int j = 0 ; j < m ; j++){
-11                if(matrix[i][j] == '0'){
-12                    dp[i][j] = 0;
-13                    continue; 
-14                }
-15
-16                int left = 0, diagonal = 0, top = 0;
-17                if(j - 1 >= 0){
-18                    left = dp[i][j-1];
-19                }
-20                if(i - 1 >= 0 && j - 1 >= 0){
-21                    diagonal = dp[i-1][j-1];
-22                }
-23                if(i - 1 >= 0){
-24                    top = dp[i-1][j];
-25                }
-26
-27                int validSquareEdge = Math.min(Math.min(left, diagonal), top) + 1;
-28                dp[i][j] = validSquareEdge;
-29                maxValue = Math.max(maxValue, validSquareEdge);
-30            }
-31        }
-32        return maxValue * maxValue;
-33           
-34    }
-35
-36
-37    public record Area (int left, int diagonal, int top) {}
-38}
+6        int maxValue = 0;
+7
+8        for(int row = 0 ; row < n ; row++){
+9            for(int column = 0 ; column < m ; column++){
+10                if(matrix[row][column] == '0') continue;
+11                int minValue = Integer.MAX_VALUE;
+12                int a = 0, b = 0, c = 0;
+13                if(row - 1 >= 0){
+14                    a = dp[row-1][column];
+15                }
+16                if(column - 1 >= 0){
+17                    b = dp[row][column-1];
+18                }
+19                if(row - 1 >= 0 && column - 1 >= 0){
+20                    c = dp[row-1][column-1];
+21                }
+22                minValue = Math.min(a, Math.min(b, c));    
+23                dp[row][column] = minValue + 1;
+24                maxValue = Math.max(maxValue, dp[row][column]);
+25            }
+26        }
+27
+28        return maxValue * maxValue;
+29    }
+30}
