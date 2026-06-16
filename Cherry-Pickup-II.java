d9a0cf979397f5b7acfaba56d13462b680bb89e2
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-16 08:57:47 +0530

    [1463] [Time Beats: 76.01%] [Memory Beats: 75.35%] - LeetPush

--- Cherry-Pickup-II.java
+++ Cherry-Pickup-II.java
@@ -1,51 +1,53 @@
 1class Solution {
 2
-3
-4    private static final int[] DIRECTIONS = new int[]{
-5        -1, 0, 1
-6    };
-7
-8    private int cherryPickupUtil(int[][] grid, int n, int m, int[][][] dp, int row, int col1, int col2) {
-9
-10        if(row == n - 1){
-11            return grid[row][col1] + grid[row][col2];
-12        }
-13
-14        if(dp[row][col1][col2] != -1)
-15            return dp[row][col1][col2];
-16
-17        int currentCherries = grid[row][col1] + grid[row][col2];
-18        int maxNextLevelPick = 0;
-19
-20        for(int i = 0 ; i < 3 ; i++){
-21            int nextCol1 = col1 + DIRECTIONS[i];
+3    private static final int[] DIRECTIONS = new int[]{
+4        -1, 0, 1
+5    };
+6    
+7    public int cherryPickup(int[][] grid, int[][][] memo, int n , int m, int row, int col1, int col2) {
+8         
+9        if(row == n){
+10            return 0;
+11        }
+12
+13        if(memo[row][col1][col2] != -1)
+14            return memo[row][col1][col2];
+15
+16        int currentRowCost = grid[row][col1] + grid[row][col2];
+17
+18        int minNextCost = 0;
+19        for(int i = 0 ; i < 3 ; i++){
+20            int newRow = row + 1;
+21            int newCol1 = col1 + DIRECTIONS[i];
 22
-23            if(nextCol1 < 0 || nextCol1 >= m) continue;
-24                
-25            for(int j = 0 ; j < 3 ; j++){
-26                int nextCol2 = col2 + DIRECTIONS[j];
+23            if(newCol1 < 0 || newCol1 >= m) continue;
+24
+25            for(int j= 0 ; j < 3 ; j++){
+26                int newCol2 = col2 + DIRECTIONS[j];
 27
-28                if(nextCol2 < 0 || nextCol2 >= m) continue;
-29                if(nextCol1 == nextCol2) continue;
+28                if(newCol2 < 0 || newCol2 >= m) continue;
+29                if(newCol1 == newCol2) continue;
 30
-31                int nextLevelPick = cherryPickupUtil(grid, n, m, dp, row + 1, nextCol1, nextCol2);
-32                maxNextLevelPick = Math.max(maxNextLevelPick, nextLevelPick);
+31                int nextCost = cherryPickup(grid, memo, n , m, newRow, newCol1, newCol2); 
+32                minNextCost = Math.max(minNextCost, nextCost);
 33            }
 34        }
-35        dp[row][col1][col2] = currentCherries + maxNextLevelPick;
-36        return currentCherries + maxNextLevelPick;
+35        memo[row][col1][col2] = currentRowCost + minNextCost;
+36        return currentRowCost + minNextCost;
 37    }
-38
+38    
 39    public int cherryPickup(int[][] grid) {
-40        int n = grid.length, m = grid[0].length;
-41
-42        int[][][] dp = new int[n][m][m];
-43        for(int[][] in: dp){
-44            for(int[] row : in){
-45                Arrays.fill(row, -1);
-46            }   
-47        }
-48
-49        return cherryPickupUtil(grid, n, m, dp, 0, 0, m - 1);
-50    }
-51}
+40
+41        int n = grid.length, m = grid[0].length;
+42
+43        int[][][] memo = new int[n][m][m];
+44
+45        for(int[][] matrix : memo){
+46            for(int[] row : matrix){
+47                Arrays.fill(row, -1);
+48            }
+49        }
+50        return cherryPickup(grid, memo, n, m, 0, 0, m-1);
+51
+52    }
+53}
