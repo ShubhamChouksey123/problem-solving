4858902bba61af14a330cd3a23521d21f6f8cd1d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-16 19:21:20 +0530

    [174] [Time Beats: 26.00%] [Memory Beats: 8.24%] - LeetPush

--- Dungeon-Game.java
+++ Dungeon-Game.java
@@ -1,29 +1,27 @@
 1class Solution {
 2    public int calculateMinimumHP(int[][] dungeon) {
-3        
+3
 4        int n = dungeon.length, m = dungeon[0].length;
 5        int[][] dp = new int[n][m];
 6
 7        for(int i = n - 1 ; i >= 0 ; i--){
 8            for(int j = m - 1 ; j >= 0 ; j--){
-9                int minNextHealthRequired = Integer.MAX_VALUE;
-10                if(i != n - 1){
-11                    minNextHealthRequired = dp[i+1][j];
+9                int best = 0;
+10                if(i + 1 < n && j + 1 < m){
+11                    best = Math.min(dp[i+1][j], dp[i][j+1]);
 12                }
-13                if(j != m - 1){
-14                    minNextHealthRequired = dp[i][j+1];
+13                else if(i + 1 < n){
+14                    best = dp[i+1][j];
 15                }
-16                if(minNextHealthRequired == Integer.MAX_VALUE){
-17                    minNextHealthRequired = 0;
-18                } 
-19                
-20                if(i != n - 1 && j != m - 1){
-21                    minNextHealthRequired = Math.max(dp[i][j+1], dp[i+1][j]); 
-22                }
-23                
-24                dp[i][j] = Math.min(dungeon[i][j] + minNextHealthRequired, 0);
-25            }
-26        }
-27        return -1*dp[0][0] + 1;
-28    }
-29}
+16                else if(j + 1 < m){
+17                    best = dp[i][j+1];
+18                }
+19                               
+20                dp[i][j] = Math.max(0, - dungeon[i][j] + best);
+21            }
+22        }
+23
+24        
+25        return dp[0][0] + 1;
+26    }
+27}
