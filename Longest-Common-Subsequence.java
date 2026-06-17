3f5ad4c4acdc98f72b6e54df6aa5194ba1420429
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-17 08:39:31 +0530

    [1143] [Time Beats: 14.21%] [Memory Beats: 68.00%] - LeetPush

--- Longest-Common-Subsequence.java
+++ Longest-Common-Subsequence.java
@@ -1,43 +1,35 @@
 1class Solution {
 2    
-3    private int longestCommonSubsequence(String text1, String text2, int n, int m, int[][] dp, int x, int y) {
+3    public int longestCommonSubsequence(String text1, String text2, int[][] memoCache, int index1, int index2) {
 4        
-5        if(x < 0 || y < 0)
-6            return 0;
-7        
-8        if(dp[x][y] != -1)
-9            return dp[x][y];
-10
-11        if(text1.charAt(x) == text2.charAt(y)){
-12            return 1 + longestCommonSubsequence(text1, text2, n, m, dp, x - 1, y - 1);
-13        }
-14
-15        int a = longestCommonSubsequence(text1, text2, n, m, dp, x - 1, y); 
-16        int b = longestCommonSubsequence(text1, text2, n, m, dp, x, y - 1);
-17
-18        int optimalValue = Math.max(a, b);
-19        dp[x][y] = optimalValue;
-20        return optimalValue; 
+5        if(index1 < 0 || index2 < 0) return 0;
+6
+7        if(memoCache[index1][index2] != -1) return memoCache[index1][index2]; 
+8
+9        int ans = 0;
+10        if(text1.charAt(index1) == text2.charAt(index2)){
+11            ans = 1 + longestCommonSubsequence(text1, text2, memoCache, index1 - 1, index2 - 1);
+12        }
+13        else{
+14            ans = Math.max(
+15                longestCommonSubsequence(text1, text2, memoCache, index1, index2 - 1),
+16                longestCommonSubsequence(text1, text2, memoCache, index1 - 1, index2)
+17            );
+18        }
+19        memoCache[index1][index2] = ans;
+20        return ans;
 21
 22    }
 23    
 24    public int longestCommonSubsequence(String text1, String text2) {
 25
 26        int n = text1.length(), m = text2.length();
-27        int[][] dp = new int[n][m];
-28        for(int[] row : dp){
+27        int[][] memoCache = new int[n][m];
+28        for(int[] row : memoCache){
 29            Arrays.fill(row, -1);
 30        }
-31        
-32        return longestCommonSubsequence(text1, text2, n, m, dp, n - 1, m - 1);  
-33    }
-34}
-35
-36/**
-37
-38"abcdeefgqznxkddxxws"
-39"acebcesnsdwandjadna"
-40
-41"abcdeefgqznxkddxxwsandsdnbagfsvafdtxvsa"
-42"acebcesnsdwandjadnasvafscvxbagsvdya"
-43 */
+31
+32
+33        return longestCommonSubsequence(text1, text2, memoCache, n - 1, m - 1);
+34    }
+35}
