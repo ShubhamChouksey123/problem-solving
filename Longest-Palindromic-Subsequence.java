e3c75d90c6ae39e13cc18fad42035c48d8319be5
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-17 09:13:20 +0530

    [516] [Time Beats: 49.48%] [Memory Beats: 30.74%] - LeetPush

--- Longest-Palindromic-Subsequence.java
+++ Longest-Palindromic-Subsequence.java
@@ -1,34 +1,32 @@
 1class Solution {
 2    
-3    public int longestPalindromeSubseq(String s, int n, int[][] memoCache , int start, int end) {
-4        
-5        if(start == end) return 1;
-6        if(start > end) return 0;
+3    public int longestPalindromeSubseq(String s, int n, int[][] dp, int x, int y) {
+4
+5        if(x == n || y < 0)
+6            return 0;
 7
-8        if(memoCache[start][end] != -1) return memoCache[start][end];
-9
-10        int longestPalLength = 1;
-11        if(s.charAt(start) == s.charAt(end)){
-12            longestPalLength = 2 + longestPalindromeSubseq(s, n, memoCache , start + 1, end - 1); 
+8        if(dp[x][y] != -1)
+9            return dp[x][y];
+10
+11        if(s.charAt(x) == s.charAt(y)){
+12            return 1 + longestPalindromeSubseq(s, n, dp, x + 1, y - 1); 
 13        }
-14        else{
-15            longestPalLength = Math.max(
-16                longestPalindromeSubseq(s, n, memoCache , start + 1, end),
-17                longestPalindromeSubseq(s, n, memoCache , start, end - 1)
-18            );
-19        }
-20        memoCache[start][end] = longestPalLength;
-21        return longestPalLength;
-22    }
-23    
-24    public int longestPalindromeSubseq(String s) {
-25
-26        int n = s.length();
-27        int[][] memoCache = new int[n][n];
-28        for(int[] row : memoCache){
-29            Arrays.fill(row, -1);
-30        }
-31        
-32        return longestPalindromeSubseq(s, n, memoCache , 0, n - 1); 
-33    }
-34}
+14
+15        int a = longestPalindromeSubseq(s, n, dp, x + 1, y);
+16        int b = longestPalindromeSubseq(s, n, dp, x, y - 1);
+17
+18        int optimalValue = Math.max(a, b);
+19        dp[x][y] = optimalValue;
+20        return optimalValue;
+21    }
+22    
+23    public int longestPalindromeSubseq(String s) {
+24
+25        int n = s.length();   
+26        int[][] dp = new int[n][n];
+27        for(int[] row : dp){
+28            Arrays.fill(row, -1);
+29        }
+30        return longestPalindromeSubseq(s, n, dp, 0, n - 1); 
+31    }
+32}
