8251a18e54ac39a4e032df1989a17df866f8ad6f
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-17 10:05:06 +0530

    [72] [Time Beats: 98.41%] [Memory Beats: 70.21%] - LeetPush

--- Edit-Distance.java
+++ Edit-Distance.java
@@ -1,39 +1,37 @@
 1class Solution {
-2
-3    private int minDistance(String word1, String word2, int n , int m, int[][] dp, int x, int y){
-4
-5        if(x == 0 && y > 0)
-6            return y;
-7        if(x > 0 && y == 0)
-8            return x;
-9        if(x == 0 && y == 0)
-10            return 0;
-11
-12        if(dp[x][y] != -1)
-13            return dp[x][y];
-14        
-15        if(word1.charAt(x - 1) == word2.charAt(y - 1)){
-16            dp[x][y] = minDistance(word1, word2, n , m, dp, x - 1, y - 1);
-17            return dp[x][y];
-18        }
-19
-20        int a = minDistance(word1, word2, n , m, dp, x - 1, y);
-21        int b = minDistance(word1, word2, n , m, dp, x, y - 1);
-22        int c = minDistance(word1, word2, n , m, dp, x - 1, y - 1);
-23
-24        int optimalValue = Math.min(a, Math.min(b, c)) + 1;
-25        dp[x][y] = optimalValue;
-26        return optimalValue;
+2    
+3    public int minDistance(String word1, String word2, int[][] memoCache, int index1, int index2) {
+4        
+5        if(index1 < 0 && index2 >= 0) return index2 + 1;
+6        if(index2 < 0 && index1 >= 0) return index1 + 1;
+7        if(index1 < 0 && index2 < 0) return 0;
+8
+9        if(memoCache[index1][index2] != -1) return memoCache[index1][index2];
+10
+11        int ans = 0;
+12        if(word1.charAt(index1) == word2.charAt(index2)){
+13            ans = minDistance(word1, word2, memoCache, index1 - 1, index2 - 1);
+14        }
+15        else{
+16            ans = 1 + Math.min(
+17                minDistance(word1, word2, memoCache, index1 - 1, index2 - 1),
+18                Math.min(
+19                    minDistance(word1, word2, memoCache, index1, index2 - 1),
+20                    minDistance(word1, word2, memoCache, index1 - 1, index2)
+21                )
+22            );
+23        }
+24
+25        memoCache[index1][index2] = ans;        
+26        return ans;
 27    }
-28
+28    
 29    public int minDistance(String word1, String word2) {
-30        
-31        int n = word1.length(), m = word2.length();
-32        int[][] dp = new int[n+1][m+1];
-33        for(int[] row : dp){
-34            Arrays.fill(row, -1);
-35        }
-36        
-37        return  minDistance(word1, word2, n , m, dp, n, m);
-38    }
-39}
+30        int n = word1.length(), m = word2.length();
+31        int[][] memoCache = new int[n][m];
+32        for(int[] row : memoCache){
+33            Arrays.fill(row, -1);
+34        }
+35        return minDistance(word1, word2, memoCache, n-1, m-1);
+36    }
+37}
