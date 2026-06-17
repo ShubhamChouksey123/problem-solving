5259beabb48870e60c021d7a4d28ab07971ea48d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-17 10:43:00 +0530

    [712] [Time Beats: 16.94%] [Memory Beats: 79.37%] - LeetPush

--- Minimum-ASCII-Delete-Sum-for-Two-Strings.java
+++ Minimum-ASCII-Delete-Sum-for-Two-Strings.java
@@ -1,37 +1,38 @@
 1class Solution {
-2    public int minimumDeleteSum(String s1, String s2) {
-3
-4        int n = s1.length(), m = s2.length();
-5        int[] prev = new int[m+1];
-6        int[] curr = new int[m+1];
-7
-8
+2    private int minimumDeleteSum(String s1, String s2, int[][] memoCache, int length1, int length2) {
+3        
+4        if(length1 == 0 && length2 == 0) return 0;
+5
+6        if(memoCache[length1][length2] != -1){
+7            return memoCache[length1][length2];
+8        }
 9
-10        for(int j = 1 ; j <= m ; j++){
-11            prev[j] = prev[j-1] + (int)s2.charAt(j-1); 
-12        }
-13
-14        for(int i = 1 ; i <= n ; i++){
-15            
-16            curr[0] = prev[0] + (int)s1.charAt(i-1); 
-17            for(int j = 1 ; j <= m ; j++){
-18
-19                if(s1.charAt(i-1) == s2.charAt(j-1)){
-20                    curr[j] = prev[j-1];
-21                }
-22                else{
-23                    curr[j] = Math.min(
-24                        prev[j] + (int)s1.charAt(i-1),
-25                        curr[j-1] + (int)s2.charAt(j-1)
-26                    );
-27                }
-28            }
+10        int ans = 0;
+11        if(length1 > 0 && length2 > 0 && s1.charAt(length1 - 1) == s2.charAt(length2 - 1)){
+12            ans = minimumDeleteSum(s1, s2, memoCache, length1 - 1, length2 - 1);
+13        }
+14        else{
+15            int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE;
+16            if(length2 > 0){
+17                a = s2.charAt(length2 - 1) + minimumDeleteSum(s1, s2, memoCache, length1, length2 - 1);
+18            }
+19            if(length1 > 0){
+20                b = s1.charAt(length1 - 1) + minimumDeleteSum(s1, s2, memoCache, length1 - 1, length2);
+21            }
+22            ans = Math.min(a, b);
+23        }
+24        memoCache[length1][length2] = ans;
+25        return ans;
+26    }
+27
+28    public int minimumDeleteSum(String s1, String s2){
 29
-30            int[] temp = prev;
-31            prev = curr;
-32            curr = temp;
-33
+30        int n = s1.length(), m = s2.length();
+31        int[][] memoCache = new int[n+1][m+1];
+32        for(int[] row : memoCache){
+33            Arrays.fill(row, -1);
 34        }
-35        return prev[m];
-36    }
-37}
+35
+36        return minimumDeleteSum(s1, s2, memoCache, n, m); 
+37    }
+38}
