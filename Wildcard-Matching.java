1b13b5e40c73320615fc92544b6831ef5580707a
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-18 12:15:35 +0530

    [44] [Time Beats: 11.49%] [Memory Beats: 11.57%] - LeetPush

--- Wildcard-Matching.java
+++ Wildcard-Matching.java
@@ -6,33 +6,35 @@
 6        if(pIndex == m){
 7            return sIndex == n;
 8        }
-9        if(sIndex > n && pIndex < m) return false;
-10
-11        if(memoCache[sIndex][pIndex] != null) return memoCache[sIndex][pIndex];
-12
-13        if(p.charAt(pIndex) != '*'){
-14            boolean isMatchNext = false;
-15            if(sIndex < n && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '?')){
-16                isMatchNext = isMatch(s, p, n, m, memoCache, sIndex + 1, pIndex + 1) ;
-17                memoCache[sIndex][pIndex] = isMatchNext;
-18                return isMatchNext;
-19            }
-20            memoCache[sIndex][pIndex] = false;
-21            return false;
-22        }
-23
-24        boolean takeChar = isMatch(s, p, n, m, memoCache, sIndex + 1, pIndex) ;
-25        boolean noTakeChar = isMatch(s, p, n, m, memoCache, sIndex, pIndex + 1) ;
-26
-27        boolean optimalValue = takeChar | noTakeChar;
-28        memoCache[sIndex][pIndex] = optimalValue;
-29        return optimalValue;
-30    }
-31    
-32    public boolean isMatch(String s, String p) {
-33
-34        int n = s.length(), m = p.length();
-35        Boolean[][] memoCache = new Boolean[n+1][m+1];
-36        return isMatch(s, p, n, m, memoCache, 0, 0);
-37    }
-38}
+9
+10        if(memoCache[sIndex][pIndex] != null) return memoCache[sIndex][pIndex];
+11
+12        if(p.charAt(pIndex) != '*'){
+13            boolean isMatchNext = false;
+14            if(sIndex < n && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '?')){
+15                isMatchNext = isMatch(s, p, n, m, memoCache, sIndex + 1, pIndex + 1) ;
+16                memoCache[sIndex][pIndex] = isMatchNext;
+17                return isMatchNext;
+18            }
+19            memoCache[sIndex][pIndex] = false;
+20            return false;
+21        }
+22
+23        boolean takeChar = false;
+24        if(sIndex < n){
+25            takeChar = isMatch(s, p, n, m, memoCache, sIndex + 1, pIndex) ;
+26        }
+27        boolean noTakeChar = isMatch(s, p, n, m, memoCache, sIndex, pIndex + 1) ;
+28
+29        boolean optimalValue = takeChar | noTakeChar;
+30        memoCache[sIndex][pIndex] = optimalValue;
+31        return optimalValue;
+32    }
+33    
+34    public boolean isMatch(String s, String p) {
+35
+36        int n = s.length(), m = p.length();
+37        Boolean[][] memoCache = new Boolean[n+1][m+1];
+38        return isMatch(s, p, n, m, memoCache, 0, 0);
+39    }
+40}
