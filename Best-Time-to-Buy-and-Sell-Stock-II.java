a3006eae3a390e42d2e342f863967d17da79c5df
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-21 20:52:43 +0530

    [122] [Time Beats: 10.22%] [Memory Beats: 5.41%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-II.java
+++ Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -1,48 +1,38 @@
 1class Solution {
 2    
-3    public int maxProfit(int[] prices, int[][] dp, int index, boolean buy) {
-4        if(index == prices.length)
-5            return 0;
-6
-7        if(buy && dp[index][1] != -1){
-8            return dp[index][1];
-9        }
-10        if(!buy && dp[index][0] != -1){
-11            return dp[index][0];
-12        }
-13
-14        int a = 0, b = 0, optimalValue = 0;
-15        if(buy){
-16            // skip buying on that day
-17            a = maxProfit(prices, dp, index + 1, true);
-18
-19            // buy on that day
-20            b = -prices[index] + maxProfit(prices, dp, index + 1, false);
-21            
-22            optimalValue = Math.max(a, b);
-23            dp[index][1] = optimalValue;
+3    public int maxProfit(int[] prices, int n, int[][] memoCache, int index, int buy) {
+4        
+5        if(index == n){
+6            return 0;
+7        }
+8
+9        if(memoCache[index][buy] != -1) 
+10            return memoCache[index][buy];
+11
+12        int optimalValue = 0; 
+13        if(buy == 1){
+14            int skipDay = maxProfit(prices, n, memoCache, index + 1, 1);
+15            int buyDay = -prices[index] + maxProfit(prices, n, memoCache, index + 1, 0);
+16
+17            optimalValue = Math.max(skipDay, buyDay);
+18        }
+19        else{
+20            int skipDay = maxProfit(prices, n, memoCache, index + 1, 0);
+21            int sellDay = prices[index] + maxProfit(prices, n, memoCache, index + 1, 1);
+22
+23            optimalValue = Math.max(skipDay, sellDay);
 24        }
-25        else{
-26
-27            // skip selling on that day
-28            a = maxProfit(prices, dp, index + 1, false);
-29
-30            // sell on that day
-31            b = prices[index] + maxProfit(prices, dp, index + 1, true);
-32
-33            optimalValue = Math.max(a, b);
-34            dp[index][0] = optimalValue;
+25        memoCache[index][buy] = optimalValue;
+26        return optimalValue;
+27    }
+28    
+29    public int maxProfit(int[] prices) {
+30        
+31        int n = prices.length;
+32        int[][] memoCache = new int[n][2];
+33        for(int[] row : memoCache){
+34            Arrays.fill(row, -1);
 35        }
-36        return optimalValue;
+36        return maxProfit(prices, n, memoCache, 0, 1); 
 37    }
-38    
-39    public int maxProfit(int[] prices) {
-40
-41        int n = prices.length;
-42        int[][] dp = new int[n][2];
-43        for(int[] row : dp){
-44            Arrays.fill(row, -1);
-45        }
-46        return maxProfit(prices, dp, 0, true);
-47    }
-48}
+38}
