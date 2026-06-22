2ef60040c74e3b4c5558b8b04c89d904c202d3f2
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-22 21:16:47 +0530

    [10] [Time Beats: 100.00%] [Memory Beats: 10.80%] - LeetPush

--- Regular-Expression-Matching.java
+++ Regular-Expression-Matching.java
@@ -1,42 +1,40 @@
 1class Solution {
 2    
-3    public boolean isMatch(String s, String p, int n, int m, Boolean[][] memoCache, int index1, int index2) {
+3    public boolean isMatch(String s, String p, int n, int m, Boolean[][] memoCache, int sIndex, int pIndex) {
 4        
-5        
-6        if(index2 == p.length()){
-7            return index1 == s.length();
-8        }
-9        if(index1 > s.length() || index2 > p.length()) return false;
-10
-11
-12        if(memoCache[index1][index2] != null)
-13            return memoCache[index1][index2];
-14        
-15        if((index2 + 1 < m && p.charAt(index2 + 1) != '*') || index2 == m - 1 ){
-16            if((index1 < n && s.charAt(index1) == p.charAt(index2)) || p.charAt(index2) == '.'){
-17                boolean ans = isMatch(s, p, n, m, memoCache, index1 + 1, index2 + 1) ;
-18                memoCache[index1][index2] = ans;
-19                return ans;
-20            }
-21            memoCache[index1][index2] = false;
-22            return false;
-23        }
-24
-25        boolean takeChar = false;
-26        if(index1 < n && index2 < m && (s.charAt(index1) == p.charAt(index2) || p.charAt(index2) == '.' ) ){
-27            takeChar = isMatch(s, p, n, m, memoCache, index1 + 1, index2);
-28        }
-29
-30        boolean noTakeChar = isMatch(s, p, n, m, memoCache, index1, index2 + 2);
-31        boolean ans = takeChar || noTakeChar;
-32        memoCache[index1][index2] = ans;
-33        return ans; 
-34    }
-35    
-36    public boolean isMatch(String s, String p) {
+5        if(pIndex == p.length()){
+6            return sIndex == s.length();
+7        }
+8        if(sIndex > n || pIndex > m) return false;
+9
+10        if(memoCache[sIndex][pIndex] != null)
+11            return memoCache[sIndex][pIndex];
+12
+13        if((pIndex + 1 < m && p.charAt(pIndex + 1) != '*') || pIndex == m - 1){
+14            boolean ans = false;
+15            if(sIndex < n && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '.')){
+16                ans = isMatch(s, p, n, m, memoCache, sIndex + 1, pIndex + 1);
+17            }
+18            memoCache[sIndex][pIndex] = ans;
+19            return ans;
+20        }
+21
+22        boolean skipChar = isMatch(s, p, n, m, memoCache, sIndex, pIndex + 2);
+23        boolean takeChar = false;
+24        if(sIndex < n && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '.')){
+25            takeChar = isMatch(s, p, n, m, memoCache, sIndex + 1, pIndex);
+26        }
+27        
+28        boolean optimalValue = skipChar || takeChar;
+29        memoCache[sIndex][pIndex] = optimalValue;
+30        return optimalValue; 
+31    }
+32    
+33    public boolean isMatch(String s, String p) {
+34        
+35        int n = s.length(), m = p.length();
+36        Boolean[][] memoCache = new Boolean[n+1][m+1];
 37
-38        int n = s.length(), m = p.length();
-39        Boolean[][] memoCache = new Boolean[n+1][m+1];
-40        return isMatch(s, p, n, m, memoCache, 0, 0);    
-41    }
-42}
+38        return isMatch(s, p, n, m, memoCache, 0, 0);
+39    }
+40}
