b56ed8f4900ac881802b4be488f09e9205f1b799
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-22 19:56:51 +0530

    [1092] [Time Beats: 71.85%] [Memory Beats: 13.76%] - LeetPush

--- Shortest-Common-Supersequence.java
+++ Shortest-Common-Supersequence.java
@@ -1,5 +1,5 @@
 1class Solution {
-2    public String shortestCommonSupersequence(String str2, String str1) {
+2    public String shortestCommonSupersequence(String str1, String str2) {
 3        
 4        int n = str1.length(), m = str2.length();
 5        int[][] dp = new int[n+1][m+1];
@@ -14,45 +14,43 @@
 14        for(int i = 1 ; i <= n ; i++){
 15            for(int j = 1 ; j <= m ; j++){
 16                if(str1.charAt(i-1) == str2.charAt(j-1)){
-17                    dp[i][j] = 1 + dp[i-1][j-1];
+17                    dp[i][j]= 1 + dp[i-1][j-1];
 18                }else{
-19                    dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1]);
+19                    dp[i][j]= 1 + Math.min(dp[i][j-1], dp[i-1][j]);
 20                }
 21            }
-22            System.out.println(Arrays.toString(dp[i]));
-23        }
-24
+22        }
+23
+24        int i = n , j = m;
 25
-26        int i = n, j = m;
-27        StringBuilder builder = new StringBuilder();  
-28        while(i >= 1 && j >= 1){
-29            System.out.println("i : " + i + ", j : " + j);
-30            
-31            if(str1.charAt(i-1) == str2.charAt(j-1)){
-32                builder.append(str1.charAt(i-1));
-33                i--; j--;
-34            }
-35            else if(dp[i-1][j] > dp[i][j-1]){
-36                builder.append(str2.charAt(j-1));
-37                j--;
-38            }
-39            else {
-40                builder.append(str1.charAt(i-1));
-41                i--;
-42            }
-43            System.out.println(builder.toString());
-44        }
-45
-46        while(i > 0){
-47            builder.append(str1.charAt(i-1));
-48            i--;
-49        }
-50        while(j > 0){
-51            builder.append(str2.charAt(j-1));
-52            j--;
-53        }
-54
-55        String ans = builder.reverse().toString();
-56        return ans;
-57    }
-58}
+26        StringBuilder builder = new StringBuilder();
+27        while(i >= 1 && j >= 1){
+28            if(str1.charAt(i-1) == str2.charAt(j-1)){
+29                builder.append(str1.charAt(i - 1));
+30                i--;
+31                j--;
+32            }
+33            else if(dp[i-1][j] <= dp[i][j-1]){
+34                builder.append(str1.charAt(i - 1));
+35                i--;
+36            }else{
+37                builder.append(str2.charAt(j - 1));
+38                j--;
+39            }
+40        }
+41
+42        while(i >= 1){
+43            builder.append(str1.charAt(i - 1));
+44            i--;
+45        }
+46
+47        while(j >= 1){
+48            builder.append(str2.charAt(j - 1));
+49            j--;
+50        }
+51
+52        String ans = builder.reverse().toString();
+53
+54        return ans;
+55    }
+56}
