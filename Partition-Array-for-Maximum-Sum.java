9c579365ce29491eee3a2c07955e075f08cc0277
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-23 18:15:47 +0530

    [1043] [Time Beats: 99.79%] [Memory Beats: 94.55%] - LeetPush

--- Partition-Array-for-Maximum-Sum.java
+++ Partition-Array-for-Maximum-Sum.java
@@ -1,26 +1,31 @@
 1class Solution {
 2    
-3    private int maxSumAfterPartitioning(int[] arr, int k, int[] dp, int start) {
+3    public int maxSumAfterPartitioning(int[] arr, int n, int k, int[] memoCache, int start) {
 4        
-5        if(start >= arr.length) return 0;
-6        if(dp[start] != -1) return dp[start];
-7
-8        int maxValue = arr[start];
-9        int maxSum = 0;
-10        for(int end = start ; end <= Math.min(start + k - 1, arr.length - 1) ; end++){
-11            maxValue = Math.max(maxValue, arr[end]);
-12            int thisSetSum = maxValue * (end - start + 1);
-13            int sum = thisSetSum + maxSumAfterPartitioning(arr, k, dp, end + 1); 
-14            maxSum = Math.max(maxSum, sum);
-15        }
-16        dp[start] = maxSum;
-17        return maxSum;
-18    }
-19    
-20    public int maxSumAfterPartitioning(int[] arr, int k) {
-21        int n = arr.length;
-22        int[] dp = new int[n];
-23        Arrays.fill(dp, -1);
-24        return maxSumAfterPartitioning(arr, k, dp, 0);    
-25    }
-26}
+5        if(start >= n) return 0;
+6
+7        if(memoCache[start] != -1){
+8            return memoCache[start];
+9        }
+10
+11        int maxValue = 0;
+12        int partitionMaxValue = 0;
+13        for(int len = 1 ; len <= k && start + len - 1 < n ; len++){
+14            int index = start + len - 1;
+15            partitionMaxValue = Math.max(partitionMaxValue, arr[index]);
+16            int partitionSum = (partitionMaxValue * len) + maxSumAfterPartitioning(arr, n, k, memoCache, index + 1);
+17            maxValue = Math.max(maxValue, partitionSum);
+18        }
+19
+20        memoCache[start] = maxValue;
+21        return maxValue;
+22    }
+23    
+24    public int maxSumAfterPartitioning(int[] arr, int k) {
+25        
+26        int n = arr.length;
+27        int[] memoCache = new int[n];
+28        Arrays.fill(memoCache, -1);
+29        return maxSumAfterPartitioning(arr, n, k, memoCache, 0);
+30    }
+31}
