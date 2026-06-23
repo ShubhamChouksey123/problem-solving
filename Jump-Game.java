f8962d2449b9b1f5bff5c38d43dd7460adb9cf9b
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-23 22:52:23 +0530

    [55] [Time Beats: 88.82%] [Memory Beats: 90.92%] - LeetPush

--- Jump-Game.java
+++ Jump-Game.java
@@ -2,17 +2,16 @@
 2    public boolean canJump(int[] nums) {
 3
 4        int n = nums.length;
-5        boolean[] canReach = new boolean[n];
-6        canReach[0] = true;
+5        
+6        int maxReach = 0;
 7
 8        for(int i = 0; i < n ; i++){
-9            if(canReach[i]){
-10                for(int j = 1 ; j <= nums[i] && i + j < n ; j++){
-11                    canReach[i + j] = true;
-12                }
-13            }
-14        } 
-15        
-16        return canReach[n-1];
-17    }
-18}
+9            
+10            if(i > maxReach) return false;
+11
+12            maxReach = Math.max(maxReach, i + nums[i]);
+13        } 
+14        
+15        return maxReach >= n - 1 ;
+16    }
+17}
