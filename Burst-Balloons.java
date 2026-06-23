681536c5626fde6f2566fc30257d1d6d761da181
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-23 19:18:58 +0530

    [312] [Time Beats: 21.91%] [Memory Beats: 66.38%] - LeetPush

--- Burst-Balloons.java
+++ Burst-Balloons.java
@@ -1,38 +1,43 @@
 1class Solution {
-2
-3    public int maxCoins(int[] nums, int[][] dp, int start, int end) {
-4
-5        if(start > end)
-6            return 0;
-7        if(dp[start][end] != -1)
-8            return dp[start][end];
-9
-10        int maxCoinsCollected = 0;
-11        for(int index = start ; index <= end ; index++){
-12            int currentBalloon = nums[start - 1] * nums[index] * nums[end + 1]; 
-13            int subProblemleft = maxCoins(nums, dp, start, index - 1);
-14            int subProblemRight = maxCoins(nums, dp, index + 1, end);
-15            int cointCollected = currentBalloon + subProblemleft + subProblemRight;
-16            maxCoinsCollected = Math.max(maxCoinsCollected, cointCollected);
-17        }
-18        dp[start][end] = maxCoinsCollected;
-19        return maxCoinsCollected;
-20    }
+2    
+3    public int maxCoins(int[] nums, int n, int[][] memoCache, int start, int end) {
+4        
+5        if(start > end) return 0;
+6        if(start == end) return nums[start -1] * nums[start] * nums[start + 1];
+7
+8        if(memoCache[start][end] != -1){
+9            return memoCache[start][end];
+10        }
+11
+12        int maxCoins = 0;
+13        for(int index = start; index <= end ; index++){
+14            int burstCoins = nums[start-1] * nums[index] * nums[end+1];
+15            int leftSubsetCoins = maxCoins(nums, n, memoCache, start, index - 1);
+16            int rightSubsetCoins = maxCoins(nums, n, memoCache, index + 1, end);
+17
+18            int thisSetSum = burstCoins + leftSubsetCoins + rightSubsetCoins;
+19            maxCoins = Math.max(maxCoins, thisSetSum);
+20        }
 21
-22    public int maxCoins(int[] nums) {
-23
-24        int n = nums.length;
-25        int[] newNums = new int[n+2];
-26        newNums[0] = 1; newNums[n+1] = 1;
-27        for(int i = 1 ; i <= n ; i++){
-28            newNums[i] = nums[i-1];
-29        }
-30        
-31        int[][] dp = new int[n+2][n+2];
-32        for(int[] row : dp){
-33            Arrays.fill(row, -1);
-34        }
-35
-36        return maxCoins(newNums, dp, 1, n); 
-37    }
-38}
+22        memoCache[start][end] = maxCoins;
+23        return maxCoins;
+24    }
+25    
+26    public int maxCoins(int[] nums) {
+27        
+28        int length = nums.length;
+29        int n = length + 2;
+30        int[] newNums = new int[n];
+31        
+32        newNums[0] = 1; newNums[n-1] = 1;
+33        for(int i = 0 ; i < length ; i++){
+34            newNums[i+1] = nums[i];
+35        }
+36
+37        int[][] memoCache = new int[n][n];
+38        for(int[] row : memoCache){
+39            Arrays.fill(row, -1);
+40        }
+41        return maxCoins(newNums, n, memoCache, 1, n - 2); 
+42    }
+43}
