d6227e117f3f7d0148b34ae0d91aeed381f9b571
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-23 21:37:38 +0530

    [1547] [Time Beats: 40.49%] [Memory Beats: 14.29%] - LeetPush

--- Minimum-Cost-to-Cut-a-Stick.java
+++ Minimum-Cost-to-Cut-a-Stick.java
@@ -1,35 +1,41 @@
 1class Solution {
-2    private int minCost(int n, int[] cuts, int[][] dp, int start, int end) {
-3        
-4        if(start > end)
-5            return 0;
-6
-7        if(dp[start][end] != -1)
-8            return dp[start][end];
+2    
+3    public int minCost(int[] cuts, int[][] memoCache, int start, int end) {
+4        
+5        if(start > end) return 0;
+6        if(memoCache[start][end] != -1){
+7            return memoCache[start][end];
+8        }
 9
 10        int minCost = Integer.MAX_VALUE;
-11        for(int index = start ; index <= end ; index++){
-12            int cost = (cuts[end + 1] - cuts[start - 1]) + minCost(n, cuts, dp, start, index - 1) + minCost(n, cuts, dp, index + 1, end);
-13            minCost = Math.min(minCost, cost);
-14        }
-15        dp[start][end] = minCost;
-16        return minCost;
-17    }
-18    
-19    public int minCost(int n, int[] cuts) {
-20        
-21        Arrays.sort(cuts);
-22        int[] cutsFull = new int[cuts.length + 2];
-23        cutsFull[0] = 0; cutsFull[cuts.length + 1] = n;
-24        for(int i = 0 ; i < cuts.length ; i++){
-25            cutsFull[i+1] = cuts[i];    
-26        }
-27
-28        int[][] dp = new int[cutsFull.length][cutsFull.length];
-29        for(int[] row : dp){
-30            Arrays.fill(row, -1);
-31        }
-32
-33        return minCost(n, cutsFull, dp, 1, cuts.length);
-34    }
-35}
+11        for(int i = start ; i <= end ; i++){
+12            int currentCutCost = cuts[end + 1] - cuts[start - 1];
+13            int leftSubSetCost = minCost(cuts, memoCache, start, i - 1); 
+14            int rightSubSetCost = minCost(cuts, memoCache, i + 1, end);
+15
+16            minCost = Math.min(minCost, currentCutCost + leftSubSetCost + rightSubSetCost);
+17        }
+18
+19        memoCache[start][end] = minCost;
+20        return minCost;
+21    }
+22    
+23    public int minCost(int n, int[] cuts) {
+24        
+25        Arrays.sort(cuts);
+26        int length = cuts.length;
+27        int newLength = length + 2;
+28        int[] newCuts = new int[newLength];
+29        newCuts[0] = 0; newCuts[newLength - 1] = n;
+30        for(int i = 0 ; i < length ; i++){
+31            newCuts[i+1] = cuts[i];
+32        }
+33
+34        int[][] memoCache = new int[newLength][newLength];
+35        for(int[] row : memoCache){
+36            Arrays.fill(row, -1);
+37        }
+38
+39        return minCost(newCuts, memoCache, 1, newLength - 2);
+40    }
+41}
