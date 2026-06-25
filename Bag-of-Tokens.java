2bafd1b869e654dbfc950254f1f3cc8654ba5d59
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-25 21:27:52 +0530

    [948] [Time Beats: 19.43%] [Memory Beats: 74.28%] - LeetPush

--- Bag-of-Tokens.java
+++ Bag-of-Tokens.java
@@ -2,23 +2,25 @@
 2    public int bagOfTokensScore(int[] tokens, int power) {
 3
 4        Arrays.sort(tokens);
-5        int n = tokens.length, score = 0;
-6        int start = 0 , end = n-1;
-7
-8        while(start <= end){
-9            if(tokens[start] <= power){
-10                score++;
-11                power -= tokens[start];
-12                start++;
-13            }
-14            else if(score > 0 && start < end){
-15                power += tokens[end];
-16                score--;
-17                end--;
-18            }else{
-19                break;
-20            }
-21        }
-22        return score;
-23    }
-24}
+5        int n = tokens.length;
+6        int score = 0;
+7        int maxScore = 0;
+8
+9        int start = 0, end = n - 1;
+10
+11        while(start <= end){
+12            if(power >= tokens[start]){
+13                power -= tokens[start];
+14                score++; start++;
+15            }
+16            else if(score > 0){
+17                power += tokens[end];
+18                end--; score--;
+19            }
+20            else break;
+21            maxScore = Math.max(maxScore, score);
+22        }
+23
+24        return maxScore; 
+25    }
+26}
