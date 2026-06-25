663146c162341964e9d18f54fbcaa005af42d5f9
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-25 23:06:37 +0530

    [1094] [Time Beats: 100.00%] [Memory Beats: 26.76%] - LeetPush

--- Car-Pooling.java
+++ Car-Pooling.java
@@ -1,25 +1,26 @@
 1class Solution {
 2    public boolean carPooling(int[][] trips, int capacity) {
-3
-4        int[] pickupLocation = new int[1001]; 
-5        int[] dropLocation = new int[1001]; 
-6
+3        
+4        int[] timeline = new int[1001];
+5
+6        int from = 0, to = 0, people = 0;
 7        for(int i = 0 ; i < trips.length ; i++){
-8            int people = trips[i][0], from = trips[i][1], to = trips[i][2];
-9            pickupLocation[from] += people;
-10            dropLocation[to] += people;
+8            from = trips[i][1];
+9            to = trips[i][2];
+10            people = trips[i][0];
 11
-12        }
-13
-14        int peopleInCar = 0;
+12            timeline[from] += people;
+13            timeline[to] -= people;
+14        }
 15
-16        for(int i = 0 ; i <= 1000 ; i++){
-17            peopleInCar -= dropLocation[i];
-18
-19            if(peopleInCar + pickupLocation[i] > capacity) return false;
-20
-21            peopleInCar += pickupLocation[i];
-22        }
-23        return true;
-24    }
-25}
+16        int peopleInCar = 0;
+17
+18        for(int change : timeline){
+19            peopleInCar += change;
+20            if(peopleInCar > capacity) return false;
+21        }
+22        return true;
+23
+24
+25    }
+26}
