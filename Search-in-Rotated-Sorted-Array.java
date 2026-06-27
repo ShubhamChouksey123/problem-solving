15e17ad0981566f025a742750394323a8c0b5a92
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-27 21:26:21 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 27.19%] - LeetPush

--- Search-in-Rotated-Sorted-Array.java
+++ Search-in-Rotated-Sorted-Array.java
@@ -1,32 +1,31 @@
 1class Solution {
-2    public int searchutil(int[] nums, int target, int start, int end) {
-3        if(start > end){
-4            return -1;
-5        }
+2    
+3    public int search(int[] nums, int target, int start, int end) {
+4        
+5        if(start > end) return -1;
 6
-7        int mid = (start + end)/2;
-8        if(nums[mid] == target){
-9            return mid;
-10        }
-11
-12        if(nums[start] <= nums[mid]){
-13            // In the second half, pivot lies
-14            if(nums[start] <= target && target <= nums[mid]){
-15                return searchutil(nums, target, start, mid - 1);
-16            }else{
-17                return searchutil(nums, target, mid + 1, end); 
-18            }
-19        }else{
-20            // In the first half, Pivot lies
-21            if(nums[mid] <= target && target <= nums[end]){
-22                return searchutil(nums, target, mid + 1, end);
-23            }else{
-24                return searchutil(nums, target, start, mid - 1); 
-25            }
-26        }
-27    }
-28    
-29    public int search(int[] nums, int target) {
-30        return searchutil(nums, target, 0, nums.length - 1); 
-31    }
-32}
+7        int mid = start + (end - start)/2;
+8        if(nums[mid] == target) return mid;
+9
+10        if(nums[start] <= nums[mid]){
+11            // first half is striclty increasing
+12            if(nums[start] <= target && target < nums[mid]){
+13                return search(nums, target, start, mid - 1);
+14            }
+15            return search(nums, target, mid + 1, end);
+16        }
+17        else{
+18            // seccond half is increasing
+19            if(nums[mid] < target && target <= nums[end]){
+20                return search(nums, target, mid + 1, end);
+21            }
+22            return search(nums, target, start, mid - 1);
+23        }
+24    }
+25    
+26    public int search(int[] nums, int target) {
+27
+28        int n = nums.length;
+29        return search(nums, target, 0, n - 1);    
+30    }
+31}
