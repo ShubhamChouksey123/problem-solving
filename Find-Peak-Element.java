97e65afab6c245244fa1edb5d999ef33b3656c7d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-27 23:07:55 +0530

    [162] [Time Beats: 0.38%] [Memory Beats: 5.28%] - LeetPush

--- Find-Peak-Element.java
+++ Find-Peak-Element.java
@@ -1,20 +1,28 @@
 1class Solution {
 2    
 3    public int findPeakElement(int[] nums, int start, int end) {
-4        if(start == end){
-5            return start;
-6        }
-7
-8        int mid = start + (end - start) / 2;
-9
-10        if(nums[mid] < nums[mid + 1]){
-11            return findPeakElement(nums, mid + 1, end); 
-12        }else{
-13            return findPeakElement(nums, start, mid);
-14        }
-15    }
-16    
-17    public int findPeakElement(int[] nums) {
-18        return findPeakElement(nums, 0, nums.length - 1);
-19    }
-20}
+4        
+5        if(start > end) return -1;
+6        System.out.println("start : " + nums[start] + ", end : " + nums[end]);
+7        if(start == end) return start;
+8        if(start + 1 == end){
+9            return (nums[start] < nums[end]) ? end : start;
+10        }
+11
+12        int mid = start + (end - start)/2;
+13
+14        if(nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) return mid;
+15
+16        if(nums[mid] < nums[mid + 1]){
+17            return findPeakElement(nums, mid + 1, end);
+18        }
+19
+20        return findPeakElement(nums, start, mid);
+21
+22    }
+23    
+24    public int findPeakElement(int[] nums) {
+25        
+26        return findPeakElement(nums, 0, nums.length - 1); 
+27    }
+28}
