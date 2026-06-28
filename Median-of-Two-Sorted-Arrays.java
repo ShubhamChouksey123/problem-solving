a5ec7f475920d2727bae959ffe323ead4cfc7499
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-28 20:51:33 +0530

    [4] [Time Beats: 100.00%] [Memory Beats: 70.91%] - LeetPush

--- Median-of-Two-Sorted-Arrays.java
+++ Median-of-Two-Sorted-Arrays.java
@@ -1,43 +1,49 @@
 1class Solution {
 2    
-3    public double findMedianSortedArraysUtil(int[] nums1, int[] nums2, int n, int m, int start, int end) {
-4        
-5        int mid = start + (end - start)/2;
-6
-7        int l1 = mid, l2 = (n + m + 1)/2 - l1;
+3    /**
+4        start and end are the length of first segment in nums1
+5     */
+6    public double findMedianSortedArrays(int[] nums1, int[] nums2, int n, int m, int start, int end) {
+7
 8        
-9        int maxLeft1 = (l1 == 0) ? Integer.MIN_VALUE : nums1[l1 - 1];
-10        int maxLeft2 = (l2 == 0) ? Integer.MIN_VALUE : nums2[l2 - 1]; 
-11        
-12        int minRight1 = (l1 == n) ? Integer.MAX_VALUE : nums1[l1]; 
-13        int minRight2 = (l2 == m) ? Integer.MAX_VALUE : nums2[l2];
-14
-15        if(maxLeft1 <= minRight2  &&  maxLeft2 <= minRight1){
-16            // correct partition
-17            if((n+m) %2 == 0){
-18                // even elements => average of the two mid values
-19                return ((double)Math.max(maxLeft1, maxLeft2) + (double)Math.min(minRight1, minRight2) ) / (double)2 ;  
-20            }
-21            else{
-22                // odd elements => middle value
-23                return (double)Math.max(maxLeft1, maxLeft2);
-24            }
-25        } 
-26
-27        if(maxLeft1 > minRight2){
-28            return findMedianSortedArraysUtil(nums1, nums2, n, m, start, l1 - 1);
-29        }
-30        return findMedianSortedArraysUtil(nums1, nums2, n, m, l1 + 1, end);
-31    }
-32    
-33    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-34        int n = nums1.length;
-35        int m = nums2.length;
-36
-37        if(n > m){
-38            return findMedianSortedArrays(nums2, nums1);
-39        }
-40
-41        return findMedianSortedArraysUtil(nums1, nums2, n, m, 0, n);
-42    }
-43}
+9        int mid = start + (end - start)/2;
+10
+11        int l1 = mid; int l2 = 0;
+12        if((n + m) % 2 == 0){
+13            l2 = ((n+m)/2) - l1;
+14        }else{
+15            l2 = ((n+m)/2 + 1) - l1;
+16        }
+17        
+18        int a = (l1 == 0) ? Integer.MIN_VALUE : nums1[l1 - 1];
+19        int x = (l2 == 0) ? Integer.MIN_VALUE : nums2[l2 - 1];
+20
+21
+22        int b = (l1 == n) ? Integer.MAX_VALUE : nums1[l1];
+23        int y = (l2 == m) ? Integer.MAX_VALUE : nums2[l2];
+24
+25
+26        if(a <= y && x <= b){
+27            // found the correct partition
+28            if((n + m) % 2 == 0){
+29                return ((double)Math.max(a, x) + Math.min(b, y)) / (double)2;
+30            }else{
+31                return (double)Math.max(a, x);
+32            }
+33        }
+34        else if(a > y){
+35            return findMedianSortedArrays(nums1, nums2, n, m, start, mid - 1);
+36        }
+37        return findMedianSortedArrays(nums1, nums2, n, m, mid + 1, end);
+38    }
+39    
+40    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+41        
+42        int n = nums1.length;
+43        int m = nums2.length;
+44
+45        if(n > m) return findMedianSortedArrays(nums2, nums1); 
+46
+47        return findMedianSortedArrays(nums1, nums2, n, m, 0, n); 
+48    }
+49}
