88adf89b82c006115f94a5491c4d0a5f1983ab49
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-29 22:37:03 +0530

    [137] [Time Beats: 69.32%] [Memory Beats: 62.59%] - LeetPush

--- Single-Number-II.java
+++ Single-Number-II.java
@@ -1,17 +1,20 @@
 1class Solution {
 2    public int singleNumber(int[] nums) {
 3
-4        int ans = 0;
-5        for(int i = 0 ; i < 32 ; i++){
-6            int count = 0;
-7            for(int num : nums){
-8                count += (1 & (num >> i));
-9            }
-10            if(count % 3 != 0){
-11                ans = ans | ( 1 << i);
+4        int n = nums.length;
+5        int ans = 0;
+6
+7        for(int i = 0 ; i < 32 ; i++){
+8            int countSetBit = 0;
+9            for(int j = 0 ; j < n ; j++){
+10                if(((nums[j] >> i) & 1) != 0) countSetBit++;
+11                
 12            }
-13        }
-14
-15        return ans;
-16    }
-17}
+13            if(countSetBit % 3 != 0){
+14                ans = ans | (1 << i);
+15            }
+16        }
+17        return ans;
+18        
+19    }
+20}
