7f7d8f599a1baeac3ebfbcf4e0490e1ddb43a878
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-29 22:36:40 +0530

    [260] [Time Beats: 100.00%] [Memory Beats: 39.37%] - LeetPush

--- Single-Number-III.java
+++ Single-Number-III.java
@@ -1,27 +1,29 @@
 1class Solution {
 2    public int[] singleNumber(int[] nums) {
-3        int xor = 0;
-4        for(int num: nums){
-5            xor ^= num;
-6        }    
-7
-8        int ithDifferentBit = 32;
-9        for(int i = 0 ; i < 32 ; i++){
-10            if((xor & (1<<i)) != 0){
-11                ithDifferentBit = i;
-12                break;
-13            }
-14        }
-15
-16        int fisrtNumber = 0, secondNumber = 0;
-17        for(int num: nums){
-18            if((num & ( 1 << ithDifferentBit)) != 0){
-19                fisrtNumber ^= num;
-20            }
-21            else{
-22                secondNumber ^= num;
-23            }
-24        }
-25        return new int[]{fisrtNumber, secondNumber}; 
-26    }
-27}
+3        
+4        int n = nums.length;
+5        int result = 0;
+6        for(int num : nums){
+7            result ^= num;
+8        }
+9
+10        int ithBitSet = 0;
+11        for(ithBitSet = 0 ; ithBitSet < 32 ; ithBitSet++){
+12            if((result & 1) != 0) 
+13                break;
+14            result = result >> 1;
+15        }
+16
+17        int aSetXor = 0, bSetXor = 0;
+18        for(int num : nums){
+19            if( ( (num >> ithBitSet) & 1) == 0){
+20                aSetXor = aSetXor ^ num;
+21            }
+22            else{
+23                bSetXor = bSetXor ^ num;
+24            }
+25        }
+26
+27        return new int[]{aSetXor, bSetXor};
+28    }
+29}
