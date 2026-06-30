9366345874517f5ca21325792e63008b792ab91c
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-30 21:11:36 +0530

    [215] [Time Beats: 24.57%] [Memory Beats: 32.76%] - LeetPush

--- Kth-Largest-Element-in-an-Array.java
+++ Kth-Largest-Element-in-an-Array.java
@@ -1,13 +1,13 @@
 1class Solution {
 2    public int findKthLargest(int[] nums, int k) {
-3        Queue<Integer> minHeap = new PriorityQueue<>();
-4
-5        for(int num : nums){
-6            minHeap.add(num);
-7            if(minHeap.size() > k){
-8                minHeap.poll();
-9            }
-10        }
-11        return minHeap.peek();
+3
+4        Queue<Integer> largestElementBucket = new PriorityQueue<>();
+5
+6        for(int num : nums){
+7            largestElementBucket.add(num);
+8            if(largestElementBucket.size() > k) largestElementBucket.poll();
+9        }
+10
+11        return largestElementBucket.peek();
 12    }
 13}
