be7d1a478a37d748df1226f26ee7b33a0a7bb8b7
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-30 21:42:35 +0530

    [264] [Time Beats: 5.15%] [Memory Beats: 10.17%] - LeetPush

--- Ugly-Number-II.java
+++ Ugly-Number-II.java
@@ -1,24 +1,22 @@
 1class Solution {
 2    public int nthUglyNumber(int n) {
-3        
-4        int p2 = 0, p3 = 0, p5 = 0;
-5        long[] ugly = new long[n];
-6        ugly[0] = 1L;
+3
+4        Queue<Long> pq = new PriorityQueue<>();
+5        pq.add(1l);
+6        int index = 0;
 7
-8        for(int i = 1 ; i < n ; i++){
-9            long p2Value = ugly[p2] * 2;
-10            long p3Value = ugly[p3] * 3;
-11            long p5Value = ugly[p5] * 5;
-12
-13            long nextUglyNumber = Math.min(p2Value, Math.min(p3Value, p5Value));
-14            ugly[i] = nextUglyNumber;
-15
-16            if(nextUglyNumber == p2Value) p2++;
-17            if(nextUglyNumber == p3Value) p3++;
-18            if(nextUglyNumber == p5Value) p5++;
+8        long num = 0;
+9        while(index < n){
+10            long topElement = pq.poll();
+11            if(topElement <= num){
+12                continue;
+13            }
+14            index++; num = topElement;
+15            pq.add(num * 2l);
+16            pq.add(num * 3l);
+17            pq.add(num * 5l);
+18        }
 19
-20        }
-21        return (int) ugly[n-1];
-22            
-23    }
-24}
+20        return (int)num;
+21    }
+22}
