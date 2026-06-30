53a28ede465162a7c5d564527a7c131ea223e546
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-06-30 20:16:25 +0530

    [347] [Time Beats: 79.40%] [Memory Beats: 81.10%] - LeetPush

--- Top-K-Frequent-Elements.java
+++ Top-K-Frequent-Elements.java
@@ -1,29 +1,32 @@
 1class Solution {
 2    public int[] topKFrequent(int[] nums, int k) {
-3        Map<Integer, Integer> freq = new HashMap<>();
-4        for(int num : nums){
-5            freq.put(num, freq.getOrDefault(num, 0) + 1);
-6        }    
-7
-8        // freq => value 
-9        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
-10            (int[] a, int[] b) -> {
-11                return Integer.compare(a[0], b[0]);
-12            }    
+3
+4        Map<Integer, Integer> frequencyOrNum = new HashMap<>();
+5        for(int num : nums){
+6            frequencyOrNum.put(num, frequencyOrNum.getOrDefault(num, 0) + 1);   
+7        }
+8
+9        Queue<int[]> topKElements = new PriorityQueue<>(
+10            (a, b) -> {
+11                return Integer.compare(a[1], b[1]);
+12            }
 13        );
 14
-15        for(Map.Entry<Integer, Integer> entry : freq.entrySet()){
-16            minHeap.add(new int[]{entry.getValue(), entry.getKey()});
-17            if(minHeap.size() > k){
-18                minHeap.poll();
-19            }
-20        }
-21
-22        int[] ans = new int[k]; int i = 0 ;
-23        while(!minHeap.isEmpty()){
-24            ans[i] = minHeap.poll()[1]; i++;
-25        }
-26        
-27        return ans;
-28    }
-29}
+15        for(Map.Entry<Integer, Integer> entry : frequencyOrNum.entrySet()){
+16            int num = entry.getKey();
+17            int times = entry.getValue();
+18            topKElements.add(new int[]{num, times});
+19
+20            if(topKElements.size() > k){
+21                topKElements.poll();  
+22            }
+23        }
+24
+25        int[] ans = new int[k]; int i = 0;
+26        while(!topKElements.isEmpty()){
+27            ans[i++] = topKElements.poll()[0];
+28        }
+29
+30        return ans;
+31    }
+32}
