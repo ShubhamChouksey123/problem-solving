a83adf5dad7ef4b49e95c9b8e516f5f4c31ff9a8
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-07-01 18:53:39 +0530

    [373] [Time Beats: 38.40%] [Memory Beats: 14.57%] - LeetPush

--- Find-K-Pairs-with-Smallest-Sums.java
+++ Find-K-Pairs-with-Smallest-Sums.java
@@ -1,30 +1,30 @@
 1class Solution {
 2    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
 3
-4        int n = nums1.length, m = nums2.length;
-5        List<List<Integer>> result = new ArrayList<>();
-6
-7        Queue<int[]> minHeap = new PriorityQueue<>(
-8            (int[] a, int[] b) -> {
-9                return Integer.compare(a[0], b[0]);
-10            }
-11        );
-12        
-13        int i = 0, j = 0;
-14        for(i = 0 ; i < k && i < n ; i++){
-15            minHeap.add(new int[]{nums1[i] + nums2[0], i, 0});
-16        }
-17
-18        while(k > 0 && !minHeap.isEmpty()){
-19            int[] tmp = minHeap.poll();
-20            i = tmp[1]; j = tmp[2];
-21            result.add(List.of(nums1[i], nums2[j])); k--;
-22            
-23            if(j + 1 < m){
-24                minHeap.add(new int[]{nums1[i] + nums2[j + 1], i, j + 1});
+4        Queue<int[]> pq = new PriorityQueue<>(
+5            (a, b) -> {
+6                return Integer.compare(a[2], b[2]);
+7            }
+8        );
+9        
+10        int n = nums1.length, m = nums2.length;
+11        for(int i = 0 ; i < n ; i++){
+12            pq.add(new int[]{i, 0, nums1[i] + nums2[0]});
+13        }
+14
+15        List<List<Integer>> result = new ArrayList<>();
+16
+17        int addedInResult = 0;
+18        while(addedInResult < k && !pq.isEmpty()){
+19            int[] topElement = pq.poll();
+20            int indexInNum1 = topElement[0], indexInNum2 = topElement[1];
+21            result.add(List.of(nums1[indexInNum1], nums2[indexInNum2]));
+22            addedInResult++;
+23            if(indexInNum2 + 1 < m){
+24                pq.add(new int[]{indexInNum1, indexInNum2 + 1, nums1[indexInNum1] + nums2[indexInNum2 + 1]});
 25            }
 26        }
-27        
+27
 28        return result;
 29    }
 30}
