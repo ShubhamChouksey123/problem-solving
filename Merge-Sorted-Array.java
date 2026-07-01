414c3a84f7eb99bd59dd0130693a08789056c702
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-07-01 20:21:05 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 76.33%] - LeetPush

--- Merge-Sorted-Array.java
+++ Merge-Sorted-Array.java
@@ -1,28 +1,26 @@
 1class Solution {
 2    public void merge(int[] nums1, int m, int[] nums2, int n) {
-3        
-4        int i = m - 1, j = n - 1;
-5        System.out.println("i : " + i + ", and j : " + j);
-6        
-7        while(i >= 0 || j >= 0){
-8            if(i >= 0 && j >= 0){
-9                if(nums1[i] > nums2[j]){
-10                    nums1[i + j + 1] = nums1[i]; i--;
-11                }
-12                else{
-13                    nums1[i + j + 1] = nums2[j]; j--;
-14                }
-15                
+3
+4        int index1 = m - 1;
+5        int index2 = n - 1;
+6
+7        int index = n + m - 1;
+8        while(index >= 0){
+9            if(index1 >= 0 && index2 >= 0){
+10                if(nums1[index1] > nums2[index2]){
+11                    nums1[index] = nums1[index1--];
+12                }
+13                else{
+14                    nums1[index] = nums2[index2--];
+15                }
 16            }
-17            else if(i >= 0){
-18                nums1[i + j + 1] = nums1[i]; i--; 
+17            else if(index1 >= 0){
+18                nums1[index] = nums1[index1--];
 19            }
-20            else if(j >= 0){
-21                nums1[i + j + 1] = nums2[j]; j--; 
-22            }else{
-23                break;
-24            }
-25        }
-26
-27    }
-28}
+20            else{
+21                nums1[index] = nums2[index2--];
+22            }
+23            index--;
+24        }   
+25    }
+26}
