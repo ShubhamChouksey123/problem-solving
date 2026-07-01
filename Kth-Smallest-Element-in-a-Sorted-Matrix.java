557b89227ea01160b6333e166eeaf33d699d0495
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-07-01 19:15:30 +0530

    [378] [Time Beats: 32.31%] [Memory Beats: 69.92%] - LeetPush

--- Kth-Smallest-Element-in-a-Sorted-Matrix.java
+++ Kth-Smallest-Element-in-a-Sorted-Matrix.java
@@ -1,30 +1,27 @@
 1class Solution {
-2
-3    public int kthSmallest(int[][] matrix, int k) {
-4        
+2    public int kthSmallest(int[][] matrix, int k) {
+3
+4        int n = matrix.length, m = matrix[0].length;
 5        Queue<int[]> minHeap = new PriorityQueue<>(
-6            (int[] a, int[] b) -> {
-7                return Integer.compare(matrix[a[0]][a[1]], matrix[b[0]][b[1]]); 
+6            (a, b) -> {
+7                return Integer.compare(a[2], b[2]);
 8            }
 9        );
 10
-11        int n = matrix.length, m = matrix[0].length;
-12        for(int i = 0 ; i < Math.min(k, n) ; i++){
-13            minHeap.add(new int[]{i, 0});
-14        }
-15
-16        int r = 0, c = 0;
-17        int[] tmp;
-18        while(k > 0 && !minHeap.isEmpty()){
-19            tmp = minHeap.poll();
-20            r = tmp[0]; c = tmp[1]; k--;
-21
-22            if(c + 1 < m){
-23                minHeap.add(new int[]{r, c + 1});
-24            }
-25        }
-26
-27        return matrix[r][c];
-28
-29    }
-30}
+11        for(int i = 0 ; i < n && i < k ; i++){
+12            minHeap.add(new int[]{i, 0, matrix[i][0]});
+13        }
+14        
+15        int element = 0;
+16        while(k > 0 && !minHeap.isEmpty()){
+17            int[] top = minHeap.poll();
+18            int row = top[0], column = top[1]; 
+19            element = top[2];
+20            k--;
+21            if(column + 1 < m){
+22                minHeap.add(new int[]{row, column + 1, matrix[row][column + 1]});
+23            }
+24        }
+25        return element;
+26    }
+27}
