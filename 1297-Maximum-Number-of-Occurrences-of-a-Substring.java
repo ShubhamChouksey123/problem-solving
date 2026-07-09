ed7e88af3ebaaf521342d5aeb190c53cd24eb031
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-07-09 22:23:53 +0530

    [1297] [Time Beats: 45.85%] [Memory Beats: 52.70%] - LeetPush

--- 1297-Maximum-Number-of-Occurrences-of-a-Substring.java
+++ 1297-Maximum-Number-of-Occurrences-of-a-Substring.java
@@ -1,46 +1,37 @@
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-        
+
         int start = 0, end = 0;
         int n = s.length();
-        Set<Character> windowsChars = new HashSet<>(); 
-        int[] lastIndexAt = new int[26];
-        Map<String, Integer> stringOccurances = new HashMap<>();
+        Map<Character, Integer> lastIndexAt = new HashMap<>(); 
+        Set<Character> distinctCharInWindow = new HashSet<>(); 
+        Map<String, Integer> substringOccurances = new HashMap<>(); 
 
+        for(end = 0 ; end < n ; end++){
 
-        for(end = 0; end < n ; end++){
             char c = s.charAt(end);
-            windowsChars.add(c);
-            lastIndexAt[c - 'a'] = end;
-
-            while(windowsChars.size() > maxLetters){
+            lastIndexAt.put(c, end);
+            distinctCharInWindow.add(c);
+            
+            if(end - start + 1 > minSize){
                 char startChar = s.charAt(start);
-                if(lastIndexAt[startChar - 'a'] == start){
-                    windowsChars.remove(startChar);
+                if(lastIndexAt.get(startChar) == start){
+                    distinctCharInWindow.remove(startChar);
                 }
                 start++;
             }
 
-            while(end - start + 1 > maxSize){
-                char startChar = s.charAt(start);
-                if(lastIndexAt[startChar - 'a'] == start){
-                    windowsChars.remove(startChar);
-                }
-                start++;
-            }
-
-            int tmpStart = start;
-            while(end - tmpStart + 1 >= minSize && tmpStart < n){
-                String tmp = s.substring(tmpStart, end + 1);
-                stringOccurances.put(tmp, stringOccurances.getOrDefault(tmp, 0) + 1);
-                tmpStart++;
+            if(distinctCharInWindow.size() <= maxLetters && end - start + 1 == minSize){
+                String tmp = s.substring(start, end + 1);
+                substringOccurances.put(tmp, substringOccurances.getOrDefault(tmp, 0) + 1);
+                
             }
         }
+
         int maxValue = 0;
-        for(int time : stringOccurances.values()){
-            maxValue = Math.max(time, maxValue);
+        for(int time : substringOccurances.values()){
+            maxValue = Math.max(maxValue, time);
         }
-        // System.out.println(stringOccurances);
         return maxValue;
     }
 }
