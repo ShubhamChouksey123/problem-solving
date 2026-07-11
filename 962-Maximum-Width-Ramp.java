3520e3fa01f47fa1d4e3cb0d56df8accdffb6f20
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-07-11 17:19:13 +0530

    [962] [Time Beats: 69.96%] [Memory Beats: 100.00%] - LeetPush

---
+++ 962-Maximum-Width-Ramp.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+
+
+        int n = nums.length;
+        Deque<int[]> stack = new ArrayDeque<>();
+        
+        for(int i = 0 ; i < n ; i++){
+            if(stack.isEmpty() || stack.peekLast()[0] >= nums[i]){
+                stack.offerLast(new int[]{nums[i], i});
+            }
+        }
+       
+
+        int maxWidthRamp = 0;
+        for(int i = n - 1 ; i > 0 ; i--){
+
+            while(!stack.isEmpty() && stack.peekLast()[1] >= i ){
+                stack.pollLast();
+            } 
+
+            while(!stack.isEmpty() && stack.peekLast()[1] < i && stack.peekLast()[0] <= nums[i] ){
+                int ramp = i - stack.pollLast()[1];
+                maxWidthRamp = Math.max(maxWidthRamp, ramp);
+            } 
+        }
+          
+        
+        return maxWidthRamp;
+    }
+}
