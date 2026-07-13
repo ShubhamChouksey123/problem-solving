7c5e3c9cd68a6d1532e861b95c58e6b7495bb1d9
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-07-14 00:05:01 +0530

    [1051] [Time Beats: 40.58%] [Memory Beats: 45.39%] - LeetPush

---
+++ 1051-Height-Checker.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        
+        int n = heights.length;
+        int[] expected = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            expected[i] = heights[i];
+        }
+        
+        Arrays.sort(expected);
+        
+        int count = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(expected[i] != heights[i]) count++;
+        }
+        return count;
+    }
+}
