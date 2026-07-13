12c15fe8e1357027d69e4677b9f12d54d15cd835
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-07-13 21:34:16 +0530

    [1472] [Time Beats: 33.91%] [Memory Beats: 37.11%] - LeetPush

--- 1472-Design-Browser-History.java
+++ 1472-Design-Browser-History.java
@@ -1,66 +1,33 @@
 class BrowserHistory {
 
-    private int idTotal;
-    private Map<Integer, String> idToUrl;
-    private Deque<Integer> backwardQ;
-    private Deque<Integer> forwardQ;
-
+    private List<String> urls;
+    private int indexAt;
 
     public BrowserHistory(String homepage) {
-        idTotal = 0;
-        idToUrl = new HashMap<>();
-        backwardQ = new ArrayDeque<>();
-        forwardQ = new ArrayDeque<>();
-
-        visit(homepage);
+        urls = new ArrayList<>();
+        indexAt = 0;
+        urls.add(homepage);
     }
     
     public void visit(String url) {
-        idToUrl.put(idTotal, url);
-        backwardQ.offerLast(idTotal++);
-        forwardQ.clear();
+        
+        int lastIndex = urls.size() - 1;
+        while(lastIndex > indexAt){
+            urls.remove(lastIndex);
+            lastIndex--;
+        }
+        urls.add(url);
+        indexAt++;
     }
     
     public String back(int steps) {
-        
-        int time = 0;
-        while(!backwardQ.isEmpty() && time < steps){
-            int top = backwardQ.pollLast();
-            forwardQ.offerFirst(top);
-            time++;
-        }
-
-        Integer id = null;
-        if(!backwardQ.isEmpty()){
-            id = backwardQ.peekLast();
-        }
-        else if(!forwardQ.isEmpty()){
-            backwardQ.offerLast(forwardQ.pollFirst());
-            id = backwardQ.peekLast();
-        }
-        if(id != null){
-            return idToUrl.get(id);
-        }
-        return null;
-        
+        indexAt = Math.max(0, indexAt - steps);
+        return urls.get(indexAt);   
     }
     
     public String forward(int steps) {
-
-        int time = 0;
-        while(!forwardQ.isEmpty() && time < steps){
-            int top = forwardQ.pollFirst();
-            backwardQ.offerLast(top);
-            time++;
-        }
-        Integer id = null;
-        if(!backwardQ.isEmpty()){
-            id = backwardQ.peekLast();
-        }
-        if(id != null){
-            return idToUrl.get(id);
-        }
-        return null;
+        indexAt = Math.min(urls.size() - 1, indexAt + steps) ;
+        return urls.get(indexAt);      
     }
 }
 
