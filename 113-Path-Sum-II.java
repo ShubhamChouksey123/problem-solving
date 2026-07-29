126e3cba7844a565e9d599667c91baf469dcdd8d
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-07-29 20:46:12 +0530

    [113] [Time Beats: 99.98%] [Memory Beats: 57.20%] - LeetPush

---
+++ 113-Path-Sum-II.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private void pathSumUtil(TreeNode root, int targetSum, List<List<Integer>> ans, List<Integer> cur, int ancestralSum){
+
+        if(root == null) return;
+
+        cur.add(root.val);
+        ancestralSum += root.val;
+
+        if(root.left == null && root.right == null){
+            if(ancestralSum == targetSum){
+                ans.add(new ArrayList<>(cur));
+            }
+        }
+
+        if(root.left != null){
+            pathSumUtil(root.left, targetSum, ans, cur, ancestralSum);
+        }
+        if(root.right != null){
+            pathSumUtil(root.right, targetSum, ans, cur, ancestralSum);
+        }
+        
+        cur.remove(cur.size() - 1);
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        pathSumUtil(root, targetSum, ans, new ArrayList<>(), 0);
+        return ans;
+    }
+}
