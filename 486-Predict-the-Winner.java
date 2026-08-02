f799669d6f3b79c381ee145e8a1e20f00fcb1b2c
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-08-02 11:28:15 +0530

    [486] [Time Beats: 100.00%] [Memory Beats: 41.35%] - LeetPush

--- 486-Predict-the-Winner.java
+++ 486-Predict-the-Winner.java
@@ -1,18 +1,24 @@
 class Solution {
     
-    public int predictTheWinner(int[] nums, int start, int end) {
+    public int predictTheWinner(int[] nums, int[][] memoCache, int start, int end) {
         
         if(start == end) return nums[start];
 
-        int takeLeft = nums[start] -  predictTheWinner(nums, start + 1, end);
-        int takeRight = nums[end] -  predictTheWinner(nums, start, end - 1);
+        if(memoCache[start][end] != -1) return memoCache[start][end];
 
-        return Math.max(takeLeft, takeRight);
+        int takeLeft = nums[start] -  predictTheWinner(nums, memoCache, start + 1, end);
+        int takeRight = nums[end] -  predictTheWinner(nums, memoCache, start, end - 1);
+
+        memoCache[start][end] = Math.max(takeLeft, takeRight);
+        return memoCache[start][end];
     }
     
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        return predictTheWinner(nums, 0, n - 1) >= 0;    
+        int[][] memoCache = new int[n][n];
+        for(int[] row : memoCache) Arrays.fill(row, -1);
+
+        return predictTheWinner(nums, memoCache, 0, n - 1) >= 0;    
 
     }
 }
