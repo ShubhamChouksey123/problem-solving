2fc095f73c48e5e2936e8dc5f5aed473f04d4559
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-11 21:09:53 +0530

    [1091] [Time Beats: 19.54%] [Memory Beats: 70.96%] - LeetPush

--- 1091-Shortest-Path-in-Binary-Matrix.java
+++ 1091-Shortest-Path-in-Binary-Matrix.java
@@ -1,44 +1,44 @@
 class Solution {
-    
+
     private static final int[][] DIRECTIONS = new int[][]{
-        {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}
-    }; 
+        {1,0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}
+    };
+
+
+    public int bfs(int[][] grid) {
+        
+        int n = grid.length, m = grid[0].length;
+        if(grid[0][0] == 1) return -1;
+
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.offerLast(new int[]{0, 0, 0});
+        boolean[][] visited = new boolean[n][m];
+        visited[0][0] = true;
+
+        int a = 0, b = 0, dist = -1;
+        while(!queue.isEmpty()){
+
+            int[] top = queue.pollFirst();
+            a = top[0]; b = top[1]; dist = top[2];
+
+            if(a == n - 1 && b == m - 1) return dist + 1;
+
+            for(int[] direction : DIRECTIONS){
+                int x = a + direction[0];
+                int y = b + direction[1];
+
+                if(x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 1|| visited[x][y]) continue;
+
+                visited[x][y] = true;
+                queue.offerLast(new int[]{x, y, dist + 1});
+            } 
+        }
+        return -1;
+    }
 
     public int shortestPathBinaryMatrix(int[][] grid) {
-
-        int n = grid.length;
-
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-
-
-        boolean[][] visited = new boolean[n][n];
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.offerLast(new int[]{0, 0, 1});
-        visited[0][0] = true;
-        int x = 0, y = 0;
-        int ans = -1;
-
-        while(!queue.isEmpty()){
-            int[] element = queue.pollFirst();  
-            
-            x = element[0]; y = element[1];
-            if(x == n - 1 && y == n - 1){
-                ans = element[2]; break;
-            }
-
-            for(int[] direction : DIRECTIONS ){
-                int nextRow = x + direction[0];
-                int nextColumn = y + direction[1];
-                
-                if(nextRow < 0 || nextRow >= n || nextColumn < 0 || nextColumn >= n 
-                    || visited[nextRow][nextColumn] || grid[nextRow][nextColumn] == 1
-                ) continue;
-            
-                visited[nextRow][nextColumn] = true;
-                queue.offerLast(new int[]{nextRow, nextColumn, element[2] + 1});
-            }
-        }   
-        return ans;
+        
+        return bfs(grid) ;
 
     }
 }
