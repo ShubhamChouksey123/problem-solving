3e209f7e81d1207490532f281570d75c8004c821
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-12 18:31:48 +0530

    [494] [Time Beats: 47.46%] [Memory Beats: 18.36%] - LeetPush

--- 494-Target-Sum.java
+++ 494-Target-Sum.java
@@ -1,26 +1,32 @@
 class Solution {
-    
-    public int ways(int[] nums, int[] memoCache, int index, int target) {
 
-        if(index >= nums.length) return (target == 0) ? 1 : 0;
-        
-        // if(memoCache[index] != Integer.MIN_VALUE) return memoCache[index];
-        
-        
-        int a = ways(nums, memoCache, index + 1, target - nums[index]);
-        int b = ways(nums, memoCache, index + 1, target + nums[index]);
+    private int findTargetSumWays(int[] nums, int total, int[][] memoCache, int target, int sum, int index) {
 
-        int totalWays = a + b;
-        memoCache[index] = totalWays;
+       if(index >= nums.length) return (target == sum) ? 1 : 0;
+       if(memoCache[total + sum][index] != Integer.MIN_VALUE) return memoCache[total + sum][index];
+
+        int plus = findTargetSumWays(nums, total, memoCache, target, sum + nums[index], index + 1);
+        int minus = findTargetSumWays(nums, total, memoCache, target, sum - nums[index], index + 1);
+
+        int totalWays = plus + minus;
+        memoCache[total + sum][index] = totalWays;
         return totalWays;
-    }
-    
-    public int findTargetSumWays(int[] nums, int target) {
-        
-        int[] memoCache = new int[nums.length];
-        Arrays.fill(memoCache, Integer.MIN_VALUE);
-        ways(nums, memoCache, 0, target);
 
-        return memoCache[0];
+    }
+
+    public int findTargetSumWays(int[] nums, int target) {
+
+        int n = nums.length;
+        int total = 0;
+        for(int num : nums){
+            total += num;
+        }
+
+        int[][] memoCache = new int[(2 * total) + 1][n];
+        for(int[] row : memoCache){
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+    
+        return findTargetSumWays(nums, total, memoCache, target, 0, 0); 
     }
 }
