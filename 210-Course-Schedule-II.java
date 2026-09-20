c0d1e36f1ddbb810c80611ac6bb5fdf440755e35
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-20 13:03:17 +0530

    [210] [Time Beats: 93.20%] [Memory Beats: 39.54%] - LeetPush

--- 210-Course-Schedule-II.java
+++ 210-Course-Schedule-II.java
@@ -1,59 +1,44 @@
 class Solution {
 
-    private boolean dfs(List<Integer>[] adj, Deque<Integer> stack, List<Integer> reverseAns, boolean[] addedInResult, boolean[] addedInStack,  int node){
-
-        stack.offerLast(node);
-        addedInStack[node] = true;
-
-        for(int neighbour : adj[node]){
-            if(addedInResult[neighbour]) continue;
-            if(addedInStack[neighbour]) return false;
-
-            if(!dfs(adj, stack, reverseAns, addedInResult, addedInStack,  neighbour)){
-                return false;
-            }
-        }
-
-        stack.pollLast();
-        reverseAns.add(node);
-        addedInStack[node] = false;
-        addedInResult[node] = true;
-        return true;
-    } 
-
+    /**
+        Kahn's Algorithm
+     */
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
-        List<Integer>[] adj = new ArrayList[numCourses];
 
+        List<Integer>[] adj = new ArrayList[numCourses];
         for(int i = 0 ; i < numCourses ; i++){
             adj[i] = new ArrayList<>();
         }
 
+        int[] inwardEdges = new int[numCourses];
         for(int[] prerequisite : prerequisites){
             int u = prerequisite[1];
             int v = prerequisite[0];
+            inwardEdges[v]++;
             adj[u].add(v);
         }
 
-
-        Deque<Integer> stack = new ArrayDeque<>();
-        List<Integer> reverseAns = new ArrayList<>();
-        boolean[] addedInResult = new boolean[numCourses];
-        boolean[] addedInStack = new boolean[numCourses];
-
-
+        Deque<Integer> queue = new ArrayDeque<>();
         for(int i = 0 ; i < numCourses ; i++){
-            if(!addedInResult[i]){
-                if(!dfs(adj, stack, reverseAns, addedInResult, addedInStack, i)){
-                    return new int[0];
+            if(inwardEdges[i] == 0) queue.offerLast(i);
+        }
+
+        int[] ans = new int[numCourses];
+        int indexAt = 0;
+        while(!queue.isEmpty()){
+
+            int node = queue.pollFirst();
+            ans[indexAt++] = node;
+
+            for(int neighbour : adj[node]){
+                inwardEdges[neighbour]--;
+                if(inwardEdges[neighbour] == 0){
+                    queue.offerLast(neighbour);
                 }
             }
         }
 
-        int[] ans = new int[numCourses];
-        for(int i = 0 ; i <  numCourses ; i++){
-            ans[i] = reverseAns.get(numCourses - i - 1);
-        }
+        if(indexAt != numCourses) return new int[0];
         return ans;
     }
 }
