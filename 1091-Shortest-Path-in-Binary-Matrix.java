507a73c59e4897932f1e4ebe0e459354e91e16ca
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-20 21:40:27 +0530

    [1091] [Time Beats: 54.22%] [Memory Beats: 71.52%] - LeetPush

--- 1091-Shortest-Path-in-Binary-Matrix.java
+++ 1091-Shortest-Path-in-Binary-Matrix.java
@@ -1,44 +1,37 @@
 class Solution {
 
     private static final int[][] DIRECTIONS = new int[][]{
-        {1,0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}
+        {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1,-1}
     };
 
+    public int shortestPathBinaryMatrix(int[][] grid) {
 
-    public int bfs(int[][] grid) {
-        
         int n = grid.length, m = grid[0].length;
+
         if(grid[0][0] == 1) return -1;
 
         Deque<int[]> queue = new ArrayDeque<>();
-        queue.offerLast(new int[]{0, 0, 0});
         boolean[][] visited = new boolean[n][m];
+        queue.offer(new int[]{0, 0, 1});
         visited[0][0] = true;
 
-        int a = 0, b = 0, dist = -1;
         while(!queue.isEmpty()){
-
             int[] top = queue.pollFirst();
-            a = top[0]; b = top[1]; dist = top[2];
+            int u = top[0], v = top[1], step = top[2];
 
-            if(a == n - 1 && b == m - 1) return dist + 1;
-
+            if(u == n - 1 && v == m - 1) return step;
+            
             for(int[] direction : DIRECTIONS){
-                int x = a + direction[0];
-                int y = b + direction[1];
+                int x = u + direction[0];
+                int y = v + direction[1];
 
-                if(x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 1|| visited[x][y]) continue;
+                if(x < 0 || x >= n || y < 0 || y >= m || visited[x][y] || grid[x][y] == 1) continue;
 
+                queue.offer(new int[]{x, y, step + 1});
                 visited[x][y] = true;
-                queue.offerLast(new int[]{x, y, dist + 1});
-            } 
+
+            }
         }
         return -1;
-    }
-
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        
-        return bfs(grid) ;
-
     }
 }
