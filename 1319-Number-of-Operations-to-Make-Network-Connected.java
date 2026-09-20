8bcede8a0f19e45a2301e76f262c2e144d512e46
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-20 12:28:55 +0530

    [1319] [Time Beats: 99.68%] [Memory Beats: 75.84%] - LeetPush

--- 1319-Number-of-Operations-to-Make-Network-Connected.java
+++ 1319-Number-of-Operations-to-Make-Network-Connected.java
@@ -1,61 +1,52 @@
 class Solution {
-
+    
     private int[] parent;
     private int[] rank;
-    private int connectedComponent; 
+    private int connectedComponents;
 
     private int find(int x){
-        if(parent[x] == x){
-            return x;
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
         }
-        parent[x] = find(parent[x]);
         return parent[x];
     }
 
     private boolean union(int x, int y){
-
         int rootX = find(x);
         int rootY = find(y);
 
-        if(rootX == rootY){
-            return false;
-        }
+        if(rootX == rootY) return false;
 
         if(rank[rootX] > rank[rootY]){
             parent[rootY] = rootX;
         }
-        else if(rank[rootX] < rank[rootY]){
+        if(rank[rootX] < rank[rootY]){
             parent[rootX] = rootY;
         }
-        else {
+        else{
             parent[rootY] = rootX;
             rank[rootX]++;
         }
-        connectedComponent--;
+        connectedComponents--;
         return true;
     }
 
     public int makeConnected(int n, int[][] connections) {
 
+        connectedComponents = n;
         parent = new int[n];
         rank = new int[n];
-        connectedComponent = n;
-        if(connections.length < n - 1) return -1;
 
+        if(connections.length < n - 1) return -1;
         for(int i = 0 ; i < n ; i++){
             parent[i] = i;
             rank[i] = 1;
         }
 
         for(int[] connection : connections){
-            int u = connection[0];
-            int v = connection[1];
-
-            union(u, v);
+            union(connection[0], connection[1]);
         }
-
-
-        return connectedComponent - 1;
         
+        return connectedComponents - 1;
     }
 }
