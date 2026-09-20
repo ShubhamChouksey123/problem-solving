7988746a435236a8564cc7aeaaf4ca5a6b594471
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-20 14:37:02 +0530

    [210] [Time Beats: 99.51%] [Memory Beats: 80.93%] - LeetPush

--- 210-Course-Schedule-II.java
+++ 210-Course-Schedule-II.java
@@ -1,35 +1,33 @@
 class Solution {
 
+    private boolean dfs(List<Integer>[] adj, Deque<Integer> stack, List<Integer> reverseAns, boolean[] addedInResult, boolean[] addedInStack,  int node){
 
-    private boolean dfs(List<Integer>[] adj, Deque<Integer> stack, boolean[] visited, boolean[] inStack, List<Integer> reverseOrderList, int node){
-
-        if(visited[node]) return true;
-        if(inStack[node]) return false;
-
-        inStack[node] = true;
+        stack.offerLast(node);
+        addedInStack[node] = true;
 
         for(int neighbour : adj[node]){
-            if(visited[neighbour]) continue;
-            if(!dfs(adj, stack, visited, inStack, reverseOrderList, neighbour)){
+            if(addedInResult[neighbour]) continue;
+            if(addedInStack[neighbour]) return false;
+
+            if(!dfs(adj, stack, reverseAns, addedInResult, addedInStack,  neighbour)){
                 return false;
             }
         }
 
-        inStack[node] = false;
-        visited[node] = true;
-        reverseOrderList.add(node);
+        stack.pollLast();
+        reverseAns.add(node);
+        addedInStack[node] = false;
+        addedInResult[node] = true;
         return true;
-    }
-    /**
-        DFS based topological sorting
-     */
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
+    } 
 
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        
         List<Integer>[] adj = new ArrayList[numCourses];
+
         for(int i = 0 ; i < numCourses ; i++){
             adj[i] = new ArrayList<>();
         }
-        
 
         for(int[] prerequisite : prerequisites){
             int u = prerequisite[1];
@@ -37,22 +35,25 @@ class Solution {
             adj[u].add(v);
         }
 
+
         Deque<Integer> stack = new ArrayDeque<>();
-        boolean[] visited = new boolean[numCourses];
-        boolean[] inStack = new boolean[numCourses];
-        List<Integer> reverseOrderList = new ArrayList<>();
+        List<Integer> reverseAns = new ArrayList<>();
+        boolean[] addedInResult = new boolean[numCourses];
+        boolean[] addedInStack = new boolean[numCourses];
+
 
         for(int i = 0 ; i < numCourses ; i++){
-           if(!visited[i]){
-                if(!dfs(adj, stack, visited, inStack, reverseOrderList, i)){
+            if(!addedInResult[i]){
+                if(!dfs(adj, stack, reverseAns, addedInResult, addedInStack, i)){
                     return new int[0];
                 }
-           }
+            }
         }
+
         int[] ans = new int[numCourses];
-        for(int i = 0 ; i < numCourses ; i++){
-            ans[i] = reverseOrderList.get(numCourses - i - 1);
-        } 
-        return ans;      
+        for(int i = 0 ; i <  numCourses ; i++){
+            ans[i] = reverseAns.get(numCourses - i - 1);
+        }
+        return ans;
     }
 }
