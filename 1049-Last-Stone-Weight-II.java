6f6a62912118b5f702bc483d6ead1c684e402607
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-20 22:53:51 +0530

    [1049] [Time Beats: 99.68%] [Memory Beats: 51.96%] - LeetPush

--- 1049-Last-Stone-Weight-II.java
+++ 1049-Last-Stone-Weight-II.java
@@ -1,38 +1,37 @@
 class Solution {
-    
-    public int maxSum(int[] stones, int target, int[][] memoCache, int index, int sum) {
-            
-        if(index == stones.length) return 0;
-        if(sum == target) return 0;
-        if(memoCache[index][sum] != -1) return memoCache[index][sum];
 
-        int notTake = maxSum(stones, target, memoCache, index + 1, sum);
+    public int lastStoneWeightII(int[] stones, int[][] memoCache, int index, int remainingWeight) {
+
+        if(index == stones.length) return 0;
+        if(remainingWeight == 0) return 0;
+        if(memoCache[index][remainingWeight] != -1) return memoCache[index][remainingWeight];
+
+        int notTake = lastStoneWeightII(stones, memoCache, index + 1, remainingWeight);
         int take = 0;
-        if(stones[index] + sum <= target){
-            take = stones[index] + maxSum(stones, target, memoCache, index + 1, sum + stones[index]);
+        if(remainingWeight >= stones[index]){
+            take = stones[index] + lastStoneWeightII(stones, memoCache, index + 1, remainingWeight - stones[index]);
         }
 
         int optimalValue = Math.max(take, notTake);
-        memoCache[index][sum] = optimalValue;
-        return optimalValue;
+        return memoCache[index][remainingWeight] = optimalValue;
     }
-    
+
     public int lastStoneWeightII(int[] stones) {
 
         int n = stones.length;
-        
         int totalSum = 0;
         for(int stone : stones){
             totalSum += stone;
         }
 
         int target = totalSum / 2;
+        
         int[][] memoCache = new int[n][target + 1];
         for(int[] row : memoCache){
             Arrays.fill(row, -1);
         }
-        int maxHalfSum = maxSum(stones, target, memoCache, 0, 0);
 
-        return totalSum - (maxHalfSum * 2);
+        int ans = lastStoneWeightII(stones, memoCache, 0, target);
+        return totalSum - (2 * ans);
     }
 }
