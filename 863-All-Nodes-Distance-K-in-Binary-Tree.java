09bf756fd8e8b0360fcf6e104925ff7ee2b3b268
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-21 17:04:28 +0530

    [863] [Time Beats: 82.48%] [Memory Beats: 61.42%] - LeetPush

--- 863-All-Nodes-Distance-K-in-Binary-Tree.java
+++ 863-All-Nodes-Distance-K-in-Binary-Tree.java
@@ -9,58 +9,64 @@
  */
 class Solution {
 
-    
-    private void preOrder(TreeNode root, Map<TreeNode, TreeNode> nodeToParent){
+    private void inOrder(TreeNode root, Map<TreeNode, TreeNode> nodeToParent) {
 
-        if(root == null) return;
+        if(root == null) return ;
 
         if(root.left != null) nodeToParent.put(root.left, root);
         if(root.right != null) nodeToParent.put(root.right, root);
-
-        preOrder(root.left, nodeToParent);
-        preOrder(root.right, nodeToParent);
-    }
-
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
+        inOrder(root.left, nodeToParent);
+        inOrder(root.right, nodeToParent);
+    }
+    
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
 
         Map<TreeNode, TreeNode> nodeToParent = new HashMap<>();
-        preOrder(root, nodeToParent);
+        inOrder(root, nodeToParent);
 
-
-        Deque<TreeNode> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>(); 
-
-        queue.offerLast(target);
-        visited.add(target.val);
-        int distance = 0;
         
+
+        Set<TreeNode> visited = new HashSet<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offerLast(target);
+        visited.add(target);
+
+        int distance = 0;
+
         while(!queue.isEmpty()){
+
             if(distance == k){
-                while(!queue.isEmpty()) ans.add(queue.pollFirst().val);
-                break;
+                while(!queue.isEmpty()){
+                    result.add(queue.pollFirst().val);
+                } break;
             }
+
             int size = queue.size();
             for(int i = 0 ; i < size ; i++){
                 TreeNode node = queue.pollFirst();
-
-                if(node.left != null && !visited.contains(node.left.val)){
+                if(node.left != null && !visited.contains(node.left)){
+                    visited.add(node.left);
                     queue.offerLast(node.left);
-                    visited.add(node.left.val);
                 }
-                if(node.right != null && !visited.contains(node.right.val)){
+                if(node.right != null && !visited.contains(node.right)){
+                    visited.add(node.right);
                     queue.offerLast(node.right);
-                    visited.add(node.right.val);
                 }
-                if(nodeToParent.containsKey(node) && !visited.contains(nodeToParent.get(node).val)){
-                    queue.offerLast(nodeToParent.get(node));
-                    visited.add(nodeToParent.get(node).val);
+                TreeNode parnetNode = nodeToParent.get(node);
+                if(parnetNode == null) continue;
+
+                if(!visited.contains(parnetNode) ){
+                    visited.add(parnetNode);
+                    queue.offerLast(parnetNode);
                 }
+
             }
             distance++;
         }
-        return ans;
+        return result;
     }
 }
