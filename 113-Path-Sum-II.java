585f08f991adfc95bcd476aa3f713a807ccf35b8
Shubham Chouksey <38021765+ShubhamChouksey123@users.noreply.github.com>
2026-09-21 19:56:40 +0530

    [113] [Time Beats: 99.98%] [Memory Beats: 86.28%] - LeetPush

--- 113-Path-Sum-II.java
+++ 113-Path-Sum-II.java
@@ -14,30 +14,38 @@
  * }
  */
 class Solution {
-
-    private void pathSumUtil(TreeNode root, int targetSum, List<List<Integer>> ans, List<Integer> cur, int ancestralSum){
-
+    
+    public void pathSum(TreeNode root, int targetSum, List<List<Integer>> result, List<Integer> cur, int sum) {
+        
         if(root == null) return;
 
-        cur.add(root.val);
-        ancestralSum += root.val;
-
         if(root.left == null && root.right == null){
-            if(ancestralSum == targetSum){
-                ans.add(new ArrayList<>(cur));
+
+            cur.add(root.val);
+            sum += root.val;
+
+            if(sum == targetSum) {
+                result.add(new ArrayList<>(cur));
             }
+
+            cur.remove(cur.size() - 1);
+            sum -= root.val;
         }
 
-        pathSumUtil(root.left, targetSum, ans, cur, ancestralSum);
-        pathSumUtil(root.right, targetSum, ans, cur, ancestralSum);
-        
-        cur.remove(cur.size() - 1);
-    }
+        cur.add(root.val);
+        sum += root.val;
 
+        pathSum(root.left, targetSum, result, cur, sum);
+        pathSum(root.right, targetSum, result, cur, sum);
+
+        cur.remove(cur.size() - 1);
+        sum -= root.val;
+    }
+    
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
 
-        List<List<Integer>> ans = new ArrayList<>();
-        pathSumUtil(root, targetSum, ans, new ArrayList<>(), 0);
-        return ans;
+        List<List<Integer>> result = new ArrayList<>();   
+        pathSum(root, targetSum, result, new ArrayList<>(), 0);
+        return result;
     }
 }
